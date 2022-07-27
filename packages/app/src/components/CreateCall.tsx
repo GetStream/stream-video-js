730f158bdae8c4a576220ef5bb27880bd6a5dcde
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2022-07-27 15:18:35 +0200

    feat: show call state indicators for call participants (wip)

--- packages/app/src/components/CreateCall.tsx
+++ packages/app/src/components/CreateCall.tsx
@@ -10,8 +10,10 @@ import {
   ListItemText,
   ListSubheader,
 } from '@mui/material';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import CallAccept from '@mui/icons-material/Call';
 import VideoCall from '@mui/icons-material/VideoCall';
+import { ParticipantJoined } from '@stream-io/video-client';
 import { useStreamVideoClient } from '@stream-io/video-components-react';
 import type { Participants } from '../App';
 
@@ -23,6 +25,8 @@ export const CreateCall = (props: {
   const [selectedParticipants, setSelectedParticipants] = useState<string[]>(
     Object.keys(participants),
   );
+
+  const [inCallParticipants, setInCallParticipants] = useState<string[]>([]);
 
   const toggleParticipant = useCallback(
     (name: string) => {
@@ -53,34 +57,55 @@ export const CreateCall = (props: {
     [client, selectedParticipants],
   );
 
+  useEffect(() => {
+    const handleParticipantJoined = (join: ParticipantJoined) => {
+      if (!join.participant) return;
+      const { user } = join.participant;
+      if (user && !inCallParticipants.includes(user.id)) {
+        setInCallParticipants((ps) => [...ps, user.id]);
+      }
+    };
+    // @ts-ignore
+    return client?.on('participantJoined', handleParticipantJoined);
+  }, [client, inCallParticipants]);
+
   return (
     <>
       <List
         subheader={<ListSubheader component="div">Participants</ListSubheader>}
       >
-        {Object.entries(participants).map(([name, token]) => (
-          <React.Fragment key={token}>
-            <ListItem
-              disablePadding
-              secondaryAction={
-                <Checkbox
-                  edge="end"
-                  disabled={!currentUser}
-                  onChange={() => toggleParticipant(name)}
-                  checked={selectedParticipants.indexOf(name) !== -1}
-                />
-              }
-            >
-              <ListItemButton onClick={() => toggleParticipant(name)}>
-                <ListItemAvatar>
-                  <Avatar>{name.charAt(0)}</Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={name} />
-              </ListItemButton>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-          </React.Fragment>
-        ))}
+        {Object.entries(participants).map(([name, token]) => {
+          const isInCall = inCallParticipants.includes(name);
+          return (
+            <React.Fragment key={token}>
+              <ListItem
+                disablePadding
+                secondaryAction={
+                  <Checkbox
+                    edge="end"
+                    disabled={!currentUser}
+                    onChange={() => toggleParticipant(name)}
+                    checked={selectedParticipants.indexOf(name) !== -1}
+                  />
+                }
+              >
+                <ListItemButton onClick={() => toggleParticipant(name)}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      {isInCall ? (
+                        <CallAccept fontSize="small" />
+                      ) : (
+                        name.charAt(0).toUpperCase()
+                      )}
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={name} />
+                </ListItemButton>
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          );
+        })}
       </List>
       <Button
         variant="contained"
