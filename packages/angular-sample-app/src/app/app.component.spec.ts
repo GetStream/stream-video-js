199aa15ae4e820edfa4e1ea745fd472047076b8e
Zita Szupera <szuperaz@gmail.com>
2022-10-12 10:50:01 +0200

    fix: lint errors

--- packages/angular-sample-app/src/app/app.component.spec.ts
+++ packages/angular-sample-app/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 });
--- packages/angular-sample-app/src/environments/environment.ts
+++ packages/angular-sample-app/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   coordinatorUrl: '/rpc',
-  wsUrl: 'ws://localhost:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
+  wsUrl:
+    'ws://localhost:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
   apiKey: 'key10',
   sfuRpcUrl: 'http://localhost:3031/twirp',
   user: {
@@ -15,7 +16,8 @@ export const environment = {
     imageUrl: '/profile.png',
     customJson: new Uint8Array(),
   },
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdHJlYW0tdmlkZW8tZ29AdjAuMS4wIiwic3ViIjoidXNlci9tYXJjZWxvIiwiaWF0IjoxNjYzNzc1MjA4LCJ1c2VyX2lkIjoibWFyY2VsbyJ9.1g7cO9RV4f89zeaRXa7ED2WyAKQ6DX3Pj1Qlbt5N8hg'
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdHJlYW0tdmlkZW8tZ29AdjAuMS4wIiwic3ViIjoidXNlci9tYXJjZWxvIiwiaWF0IjoxNjYzNzc1MjA4LCJ1c2VyX2lkIjoibWFyY2VsbyJ9.1g7cO9RV4f89zeaRXa7ED2WyAKQ6DX3Pj1Qlbt5N8hg',
 };
 
 /*
