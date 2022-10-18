7abdf2c9253bcfeb0bc6afcb43526e6d3418ba38
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2022-10-18 11:07:18 +0100

    fix: hardcoded userId

--- packages/react-sdk/src/hooks/useParticipantStreams.ts
+++ packages/react-sdk/src/hooks/useParticipantStreams.ts
@@ -24,9 +24,10 @@ export const useParticipantStreams = (call: Call) => {
       });
 
       const [primaryStream] = e.streams;
-      console.log(primaryStream,e.track)
+      console.log(primaryStream, e.track);
       // format: <user-id>:<session-id>:<kind>
-      const [name] = primaryStream.id.split(':');
+      // const [name] = primaryStream.id.split(':');
+      const name = 'test';
       if (e.track.kind === 'video') {
         setUserVideoStreams((s) => ({
           ...s,
