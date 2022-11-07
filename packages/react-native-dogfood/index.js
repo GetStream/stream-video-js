2561e76a5b48c6e5ba607c4ffc453aa1cde1c4ea
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2022-11-07 08:14:08 +0100

    patch rtcrtpsender and rtcrtpreceiver and add polyfill for uuid

--- packages/react-native-dogfood/index.js
+++ packages/react-native-dogfood/index.js
@@ -5,6 +5,14 @@
 import 'text-encoding-polyfill';
 /** Text decoder polyfill */
 import 'react-native-url-polyfill/auto';
+/** crypto.getRandomValues polyfill for uuid */
+import 'react-native-get-random-values';
+
+import { RTCRtpSender, RTCRtpReceiver } from 'react-native-webrtc';
+
+global.RTCRtpSender = RTCRtpSender;
+global.RTCRtpReceiver = RTCRtpReceiver;
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
