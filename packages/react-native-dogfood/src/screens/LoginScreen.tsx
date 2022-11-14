8f93ffbe1ebdcd77feef44f38f36248acf3fd7a6
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2022-11-14 21:30:29 +0100

    fix: auth did not work on android

--- packages/react-native-dogfood/src/screens/LoginScreen.tsx
+++ packages/react-native-dogfood/src/screens/LoginScreen.tsx
@@ -68,7 +68,7 @@ GoogleSignin.configure({
   // offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   hostedDomain: 'getstream.io', // specifies a hosted domain restriction
   // forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-  accountName: 'stream-video-dogfood', // [Android] specifies an account name on the device that should be used
+  // accountName: '', // [Android] specifies an account name on the device that should be used
   // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
   // googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
   // openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
