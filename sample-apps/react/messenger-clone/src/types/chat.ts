8855f3f3cf9363893fc3cbf21a6fa244a3dff247
MartinCupela <martin.cupela@gmail.com>
2022-11-16 15:49:44 +0100

    feat: add chat generic types

---
+++ sample-apps/react/messenger-clone/src/types/chat.ts
@@ -0,0 +1,19 @@
+import type { UR, LiteralStringForUnion } from 'stream-chat';
+
+export type AttachmentType = UR;
+export type ChannelType = UR;
+export type CommandType = LiteralStringForUnion;
+export type EventType = UR;
+export type MessageType = UR;
+export type ReactionType = UR;
+export type UserType = UR;
+
+export type StreamChatType = {
+  attachmentType: AttachmentType;
+  channelType: ChannelType;
+  commandType: CommandType;
+  eventType: EventType;
+  messageType: MessageType;
+  reactionType: ReactionType;
+  userType: UserType;
+};
