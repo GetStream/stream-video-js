3d5b2c5af69fcc1c1ce5a7be4252d00f551e498d
MartinCupela <martin.cupela@gmail.com>
2022-11-17 14:45:35 +0100

    feat: add channel subtitle to chat type

--- sample-apps/react/messenger-clone/src/types/chat.ts
+++ sample-apps/react/messenger-clone/src/types/chat.ts
@@ -1,7 +1,7 @@
 import type { UR, LiteralStringForUnion } from 'stream-chat';
 
 export type AttachmentType = UR;
-export type ChannelType = UR;
+export type ChannelType = UR & { subtitle?: string };
 export type CommandType = LiteralStringForUnion;
 export type EventType = UR;
 export type MessageType = UR;
