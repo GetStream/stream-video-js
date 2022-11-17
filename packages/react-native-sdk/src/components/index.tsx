83906b77c66a703cfe52a1d268764e169dbc35b8
vanGalilea <galiliziv@gmail.com>
2022-11-17 17:13:29 +0100

    add files

---
+++ packages/react-native-sdk/src/components/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const HelloWorld = () => (
+  <View style={{ width: 100, height: 100, backgroundColor: 'red' }}>
+    <Text>yolo</Text>
+  </View>
+);
