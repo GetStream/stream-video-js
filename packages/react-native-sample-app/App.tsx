3658455442968e30def847a912af1070c7fb4516
vanGalilea <galiliziv@gmail.com>
2022-11-21 15:28:14 +0100

    Initial SDK setup and Sample App

--- packages/react-native-sample-app/App.tsx
+++ packages/react-native-sample-app/App.tsx
@@ -8,23 +8,11 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from "react";
+import { SafeAreaView, StatusBar, Text, useColorScheme } from "react-native";
 
-import {
-  Colors,
-  Header,
-  LearnMoreLinks,
-} from 'react-native/Libraries/NewAppScreen';
-import {HelloWorld} from '@stream-io/react-native-sdk';
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { HelloWorld } from "@stream-io/video-react-native-sdk";
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -39,40 +27,10 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Text>Hello</Text>
-          <HelloWorld />
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text>Hello</Text>
+      <HelloWorld />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- packages/react-native-sdk/src/components/index.tsx
+++ packages/react-native-sdk/src/components/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export const HelloWorld = () => (
   <View style={{ width: 100, height: 100, backgroundColor: 'red' }}>
-    <Text>yolo</Text>
+    <Pressable onPress={() => console.log('hello')}>
+      <Text>Hello World!</Text>
+    </Pressable>
   </View>
 );
