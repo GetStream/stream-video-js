2f8cf62bedc962d85cf2cc22fade28332049d088
vanGalilea <galiliziv@gmail.com>
2022-11-22 16:44:21 +0100

    app name changes

--- packages/react-native-sample-app/android/app/src/main/jni/MainComponentsRegistry.h
+++ packages/react-native-sample-app/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/reactnativesampleapp/newarchitecture/components/MainComponentsRegistry;";
+      "Lio/getstream/video/rn/nativesample/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
