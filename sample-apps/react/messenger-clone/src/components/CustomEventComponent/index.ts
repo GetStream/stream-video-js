3621c0c1141629e681eeb280cb516a8b84b94099
Anton Arnautov <arnautov.anton@gmail.com>
2022-11-23 23:34:46 +0100

    feat: added CustomEventComponent

---
+++ sample-apps/react/messenger-clone/src/components/CustomEventComponent/index.ts
@@ -0,0 +1,1 @@
+export * from './CustomEventComponent';
--- sample-apps/react/messenger-clone/src/utils/userFromToken.ts
+++ sample-apps/react/messenger-clone/src/utils/userFromToken.ts
@@ -2,7 +2,10 @@ import { decodeBase64, UserResponse } from 'stream-chat';
 
 import type { StreamChatType } from '../types/chat';
 
-export const userFromToken = (token: string): UserResponse<StreamChatType> => {
+export const userFromToken = (
+  token?: string,
+): UserResponse<StreamChatType> | null => {
+  if (!token) return null;
   const fragments = token.split('.');
   if (fragments.length !== 3) {
     return null;
@@ -11,5 +14,9 @@ export const userFromToken = (token: string): UserResponse<StreamChatType> => {
   const payload = decodeBase64(b64Payload);
   const { user_id, ...userData } = JSON.parse(payload);
 
-  return { ...userData, id: user_id, teams: [] };
+  return {
+    ...userData,
+    id: user_id,
+    teams: [],
+  };
 };
