ab2633c325dcc0de40571d87c85d9bb48c53727d
Anton Arnautov <arnautov.anton@gmail.com>
2022-11-23 23:33:49 +0100

    feat: added UserList, moved other stuff around

--- sample-apps/react/messenger-clone/src/App.tsx
+++ sample-apps/react/messenger-clone/src/App.tsx
@@ -1,4 +1,9 @@
-import type { ChannelFilters, ChannelOptions, ChannelSort } from 'stream-chat';
+import type {
+  ChannelFilters,
+  ChannelOptions,
+  ChannelSort,
+  UserResponse,
+} from 'stream-chat';
 import {
   Channel,
   ChannelList,
@@ -12,6 +17,8 @@ import {
 import { ChannelHeader } from './components/ChannelHeader';
 import Video from './components/Video/Video';
 import { CustomChannelSearch } from './components/CustomChannelSearch';
+import { CustomEventComponent } from './components/CustomEventComponent';
+import { UserList } from './components/UserList';
 
 import { CallController } from './context';
 import { useClient } from './hooks';
@@ -19,43 +26,48 @@ import { userFromToken } from './utils/userFromToken';
 
 import type { StreamChatType } from './types/chat';
 
-import users from '../data/users.json';
-
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, property) => searchParams.get(property as string),
-}) as unknown as Record<string, string | null>;
+import { useMemo } from 'react';
 
 const apiKey = import.meta.env.VITE_STREAM_KEY as string;
-const userId = params.uid ?? (import.meta.env.VITE_USER_ID as string);
-const userToken = params.ut ?? (import.meta.env.VITE_USER_TOKEN as string);
 
-const filters: ChannelFilters = {
-  members: { $in: [userId] },
-  type: 'messaging',
+const Root = () => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, property) => searchParams.get(property as string),
+  }) as unknown as Record<string, string | null>;
+
+  const userToken = params.ut ?? (import.meta.env.VITE_USER_TOKEN as string);
+
+  const user = useMemo(() => userFromToken(userToken), []);
+
+  if (!user?.id) return <UserList />;
+
+  return <App user={user} userToken={userToken} />;
 };
-const options: ChannelOptions = { limit: 10, presence: true, state: true };
-const sort: ChannelSort = { last_message_at: -1, updated_at: -1 };
 
-const App = () => {
-  const user = userFromToken(userToken);
+const App = ({
+  user,
+  userToken,
+}: {
+  user: UserResponse<StreamChatType>;
+  userToken: string;
+}) => {
   const client = useClient<StreamChatType>({
     apiKey,
     userData: user,
     tokenOrProvider: userToken,
   });
 
+  const filters: ChannelFilters = {
+    members: { $in: [user.id] },
+    type: 'messaging',
+  };
+  const options: ChannelOptions = { limit: 10, presence: true, state: true };
+  const sort: ChannelSort = { last_message_at: -1, updated_at: -1 };
+
   if (!client) return null;
 
   return (
     <CallController>
-      {/* TODO: add user-list logic */}
-      <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
-        {users.map((u) => (
-          <a href={`http://localhost:5174?ut=${u.token}&uid=${u.id}`}>
-            {u.name}
-          </a>
-        ))}
-      </div>
       <Chat client={client}>
         <ChannelList
           filters={filters}
@@ -64,7 +76,7 @@ const App = () => {
           sort={sort}
           ChannelSearch={CustomChannelSearch}
         />
-        <Channel>
+        <Channel MessageSystem={CustomEventComponent}>
           <Window>
             <ChannelHeader />
             <MessageList />
@@ -78,4 +90,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Root;
