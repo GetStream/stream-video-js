d126023da76a46a7b802e14e75b00da604059311
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-11-24 13:40:39 +0530

    feat: add ring call hooks to react bindings (#88)

--- packages/react-bindings/src/hooks/call.ts
+++ packages/react-bindings/src/hooks/call.ts
@@ -10,3 +10,28 @@ export const useDominantSpeaker = () => {
   const { dominantSpeaker$ } = useStore();
   return useObservableValue(dominantSpeaker$);
 };
+
+export const useActiveCall = () => {
+  const { activeCall$ } = useStore();
+  return useObservableValue(activeCall$);
+};
+
+export const useActiveRingCall = () => {
+  const { activeRingCallMeta$ } = useStore();
+  return useObservableValue(activeRingCallMeta$);
+};
+
+export const useTerminatedRingCall = () => {
+  const { terminatedRingCallMeta$ } = useStore();
+  return useObservableValue(terminatedRingCallMeta$);
+};
+
+export const useActiveRingCallDetails = () => {
+  const { activeRingCallDetails$ } = useStore();
+  return useObservableValue(activeRingCallDetails$);
+};
+
+export const useIncomingRingCalls = () => {
+  const { incomingRingCalls$ } = useStore();
+  return useObservableValue(incomingRingCalls$);
+};
