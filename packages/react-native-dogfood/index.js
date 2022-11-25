8f59003086caccdf9bebbb2ffb9bee00b26a07c2
khushal87 <khushal.agarwal987@gmail.com>
2022-11-25 12:49:23 +0530

    chore: add back react-native-get-random-values for uuid

--- packages/react-native-dogfood/index.js
+++ packages/react-native-dogfood/index.js
@@ -3,6 +3,7 @@ import 'text-encoding-polyfill';
 /** Text decoder polyfill */
 import 'react-native-url-polyfill/auto';
 /** crypto.getRandomValues polyfill for uuid */
+import 'react-native-get-random-values';
 
 import { registerGlobals } from 'react-native-webrtc';
 
