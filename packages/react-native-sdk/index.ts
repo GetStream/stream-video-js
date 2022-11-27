c934a09a034595273434f391ac78868ffe59898b
Steve Galili <galiliziv@gmail.com>
2022-11-27 16:20:21 +0100

    Update packages/react-native-sdk/index.ts

--- packages/react-native-sdk/index.ts
+++ packages/react-native-sdk/index.ts
@@ -3,5 +3,5 @@ export * from './src/components';
 export * from './src/contexts';
 
 // Overriding 'StreamVideo' from '@stream-io/video-react-bindings'
-// Explicitly re-exporting to resolve the ambiguity.
+// Explicitly re-exporting to resolve ambiguity.
 export { StreamVideo } from './src/contexts/StreamVideoContext';
