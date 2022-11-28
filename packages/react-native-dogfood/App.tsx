3fc5fe13f05a8fdb79d324516a43dd530930c50b
khushal87 <khushal.agarwal987@gmail.com>
2022-11-28 18:53:45 +0530

    fix: App.tsx conditional logic

--- packages/react-native-dogfood/App.tsx
+++ packages/react-native-dogfood/App.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ActiveCallScreen from './src/screens/ActiveCall';
+import {
+  AppGlobalContextProvider,
+  useAppGlobalStoreValue,
+} from './src/contexts/AppContext';
+
 import { RootStackParamList } from './types';
 import LoginScreen from './src/screens/LoginScreen';
 import { NavigationHeader } from './src/components/NavigationHeader';
@@ -12,10 +17,6 @@ import AuthenticatingProgressScreen from './src/screens/AuthenticatingProgress';
 import { useProntoLinkEffect } from './src/hooks/useProntoLinkEffect';
 import OutgoingCallScreen from './src/screens/OutgoingCallScreen';
 import { StreamVideo } from '@stream-io/video-react-native-sdk';
-import {
-  AppGlobalContextProvider,
-  useAppGlobalStoreValue,
-} from './src/contexts/AppContext';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -27,38 +28,36 @@ const StackNavigator = () => {
 
   if (authenticationInProgress) {
     return <AuthenticatingProgressScreen />;
-  } else {
-    if (videoClient) {
-      return (
-        <StreamVideo client={videoClient}>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="HomeScreen"
-              component={HomeScreen}
-              options={{ header: NavigationHeader }}
-            />
-            <Stack.Screen
-              name="ActiveCall"
-              component={ActiveCallScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="IncomingCallScreen"
-              component={IncomingCallScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="OutgoingCallScreen"
-              component={OutgoingCallScreen}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        </StreamVideo>
-      );
-    } else {
-      return <LoginScreen />;
-    }
   }
+  if (!videoClient) {
+    return <LoginScreen />;
+  }
+  return (
+    <StreamVideo client={videoClient}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ header: NavigationHeader }}
+        />
+        <Stack.Screen
+          name="ActiveCall"
+          component={ActiveCallScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="IncomingCallScreen"
+          component={IncomingCallScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="OutgoingCallScreen"
+          component={OutgoingCallScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </StreamVideo>
+  );
 };
 
 export default function App() {
