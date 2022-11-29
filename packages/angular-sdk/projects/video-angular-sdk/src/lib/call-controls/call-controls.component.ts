e38b2864bb40789f3c3357da081527431414032f
Zita Szupera <zita.szupera@getstream.io>
2022-11-29 13:07:37 +0100

    Update angular sdk (#101)
    - Call recording -> not yet working because the state doesn't emit call
    metadata at the moment, will update when the store problem is fixed
    - Stat screen

--- packages/angular-sdk/projects/video-angular-sdk/src/lib/call-controls/call-controls.component.ts
+++ packages/angular-sdk/projects/video-angular-sdk/src/lib/call-controls/call-controls.component.ts
@@ -1,24 +1,57 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StreamVideoParticipant, Call } from '@stream-io/video-client';
+import { NgxPopperjsTriggers } from 'ngx-popperjs';
+import { Subscription } from 'rxjs';
+import { StreamVideoService } from '../video.service';
 
 @Component({
   selector: 'stream-call-controls',
   templateUrl: './call-controls.component.html',
   styles: [],
 })
-export class CallControlsComponent implements OnInit {
-  @Input() localParticipant?: StreamVideoParticipant;
-  @Input() call?: Call;
+export class CallControlsComponent implements OnInit, OnDestroy {
+  localParticipant?: StreamVideoParticipant;
+  call?: Call;
+  isCallRecordingInProgress: boolean = false;
+  popperTrigger = NgxPopperjsTriggers.click;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private streamVideoService: StreamVideoService) {
+    this.subscriptions.push(
+      this.streamVideoService.callRecordingInProgress$.subscribe(
+        (inProgress) => (this.isCallRecordingInProgress = inProgress),
+      ),
+    );
+    this.subscriptions.push(
+      this.streamVideoService.activeCall$.subscribe((c) => (this.call = c)),
+    );
+    this.subscriptions.push(
+      this.streamVideoService.activeCallLocalParticipant$.subscribe(
+        (p) => (this.localParticipant = p),
+      ),
+    );
+  }
 
   ngOnInit(): void {}
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+
   updateAudioMutaState() {
-    console.warn(this.call, !this.localParticipant?.audio);
     this.call?.updateMuteState('audio', !!this.localParticipant?.audio);
   }
 
   updateVideoMutaState() {
     this.call?.updateMuteState('video', !!this.localParticipant?.video);
+  }
+
+  toggleRecording() {
+    alert('Not yet implemented');
+    // TODO: call meta should be part of the store
+    // this.isCallRecordingInProgress
+    //   ? this.streamVideoService.videoClient?.stopRecording('', '')
+    //   : this.streamVideoService.videoClient?.startRecording('', '');
   }
 
   endCall() {
--- packages/angular-sdk/projects/video-angular-sdk/src/lib/stream-video.module.ts
+++ packages/angular-sdk/projects/video-angular-sdk/src/lib/stream-video.module.ts
@@ -6,6 +6,8 @@ import { CallControlsComponent } from './call-controls/call-controls.component';
 import { StageComponent } from './stage/stage.component';
 import { DeviceSettingsComponent } from './device-settings/device-settings.component';
 import { NgxPopperjsModule } from 'ngx-popperjs';
+import { CallStatisticsComponent } from './call-statistics/call-statistics.component';
+import { NgChartsModule } from 'ng2-charts';
 
 /**
  * This Angular Module is the entry point of the SDK, import this Angular module in your application, all services defined by the SDK are created by importing this module
@@ -17,8 +19,9 @@ import { NgxPopperjsModule } from 'ngx-popperjs';
     CallControlsComponent,
     StageComponent,
     DeviceSettingsComponent,
+    CallStatisticsComponent,
   ],
-  imports: [CommonModule, NgxPopperjsModule],
+  imports: [CommonModule, NgxPopperjsModule, NgChartsModule],
   exports: [
     CallComponent,
     ParticipantComponent,
