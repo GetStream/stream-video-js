19e54b6bc397be4b84acd3e1d93806df8b027d05
MartinCupela <martin.cupela@gmail.com>
2022-11-30 11:19:29 +0100

    feat(client): change call workflow to publish WS events to app state store

--- packages/client/src/StreamVideoClient.ts
+++ packages/client/src/StreamVideoClient.ts
@@ -1,7 +1,9 @@
 import {
+  StreamVideoReadableStateStore2,
+  StreamVideoWriteableStateStore2,
   StreamVideoReadOnlyStateStore,
   StreamVideoWriteableStateStore,
-} from './stateStore';
+} from './store';
 import type { Call as CallMeta } from './gen/video/coordinator/call_v1/call';
 import type {
   CreateCallRequest,
@@ -33,11 +35,16 @@ import {
 } from './ws';
 import { StreamSfuClient } from './StreamSfuClient';
 import { Call } from './rtc/Call';
-import { registerWSEventHandlers } from './ws/callUserEventHandlers';
 import {
   WebsocketClientEvent,
   WebsocketHealthcheck,
 } from './gen/video/coordinator/client_v1_rpc/websocket';
+import {
+  CallAccepted,
+  CallCancelled,
+  CallCreated,
+  CallRejected,
+} from './gen/video/coordinator/event_v1/event';
 
 const defaultOptions: Partial<StreamVideoClientOptions> = {
   coordinatorRpcUrl:
@@ -57,8 +64,10 @@ export class StreamVideoClient {
    * @angular If you're using our Angular SDK, you shouldn't be interacting with the state store directly, instead, you should be using the [`StreamVideoService`](./StreamVideoService.md).
    */
   readonly readOnlyStateStore: StreamVideoReadOnlyStateStore;
+  readonly readOnlyStateStore2: StreamVideoReadableStateStore2;
   // Make it public temporary to ease SDK transition
   readonly writeableStateStore: StreamVideoWriteableStateStore;
+  private writeableStateStore2: StreamVideoWriteableStateStore2;
   private client: ClientRPCClient;
   private options: StreamVideoClientOptions;
   private ws: StreamWSClient | undefined;
@@ -87,6 +96,10 @@ export class StreamVideoClient {
         }),
       ],
     });
+    this.writeableStateStore2 = new StreamVideoWriteableStateStore2();
+    this.readOnlyStateStore2 = new StreamVideoReadableStateStore2(
+      this.writeableStateStore2,
+    );
 
     this.writeableStateStore = new StreamVideoWriteableStateStore();
     this.readOnlyStateStore = this.writeableStateStore.asReadOnlyStore();
@@ -110,8 +123,13 @@ export class StreamVideoClient {
       user,
     );
     if (this.ws) {
-      registerWSEventHandlers(this, this.writeableStateStore);
+      this.registerWSEventHandlers();
     }
+    this.writeableStateStore2.setCurrentValue(
+      this.writeableStateStore.connectedUserSubject,
+      user,
+    );
+    // todo: MC: remove stateStore
     this.writeableStateStore.setCurrentValue(
       this.writeableStateStore.connectedUserSubject,
       user,
@@ -172,9 +190,17 @@ export class StreamVideoClient {
     );
   };
 
+  registerWSEventHandlers = () => {
+    this.on('callCreated', this.onCallCreated);
+    this.on('callAccepted', this.onCallAccepted);
+    this.on('callRejected', this.onCallRejected);
+    this.on('callCancelled', this.onCallCancelled);
+  };
+
   /**
-   * Allows you to create new calls with the given parameters. If a call with the same combination of type and id already exists, it will return the existing call.
-   * @param data
+   * Allows you to create new calls with the given parameters. If a call with the same combination of type and id already exists, this will return an error.
+   * Causes the CallCreated event to be emitted to all the call members.
+   * @param data CreateCallRequest payload object
    * @returns A call metadata with information about the call.
    */
   getOrCreateCall = async (data: GetOrCreateCallRequest) => {
@@ -187,17 +213,38 @@ export class StreamVideoClient {
     }
   };
 
-  /**
-   * Allows you to create new calls with the given parameters. If a call with the same combination of type and id already exists, this will return an error.
-   * @param data
-   * @returns A call metadata with information about the call.
-   */
   createCall = async (data: CreateCallRequest) => {
     const callToCreate = await this.client.createCall(data);
     const { call: callEnvelope } = callToCreate.response;
     return callEnvelope;
   };
 
+  /**
+   * Event handler invoked upon delivery of CallCreated Websocket event
+   * Updates the state store and notifies its subscribers that
+   * a new pending call has been initiated.
+   * @param event received CallCreated Websocket event
+   * @returns
+   */
+  onCallCreated = (event: CallCreated) => {
+    const { call } = event;
+    if (!call) {
+      console.warn("Can't find call in CallCreated event");
+      return;
+    }
+
+    this.writeableStateStore2.addCall(
+      this.writeableStateStore2.pendingCallsSubject,
+      event,
+    );
+  };
+
+  /**
+   * Signals other users that I have accepted the incoming call.
+   * Causes the CallAccepted event to be emitted to all the call members.
+   * @param callCid config ID of the rejected call
+   * @returns
+   */
   acceptCall = async (callCid: string) => {
     await this.client.sendEvent({
       callCid,
@@ -205,6 +252,35 @@ export class StreamVideoClient {
     });
   };
 
+  /**
+   * Event handler invoked upon delivery of CallAccepted Websocket event
+   * Updates the state store and notifies its subscribers that
+   * the call is now considered active.
+   * @param event received CallAccepted Websocket event
+   * @returns
+   */
+  onCallAccepted = (event: CallAccepted) => {
+    const { call } = event;
+    if (!call) {
+      console.warn("Can't find call in CallCreated event");
+      return;
+    }
+    this.writeableStateStore2.setCurrentValue(
+      this.writeableStateStore2.activeCallSubject,
+      { data: event },
+    );
+    this.writeableStateStore2.removeCall(
+      this.writeableStateStore2.pendingCallsSubject,
+      event,
+    );
+  };
+
+  /**
+   * Signals other users that I have rejected the incoming call.
+   * Causes the CallRejected event to be emitted to all the call members.
+   * @param callCid config ID of the rejected call
+   * @returns
+   */
   rejectCall = async (callCid: string) => {
     await this.client.sendEvent({
       callCid,
@@ -212,11 +288,55 @@ export class StreamVideoClient {
     });
   };
 
+  /**
+   * Event handler invoked upon delivery of CallRejected Websocket event.
+   * Updates the state store and notifies its subscribers that
+   * the call is now considered terminated.
+   * @param event received CallRejected Websocket event
+   * @returns
+   */
+  onCallRejected = (event: CallRejected) => {
+    const { call } = event;
+    if (!call) {
+      console.warn("Can't find call in CallRejected event");
+      return;
+    }
+    this.writeableStateStore2.removeCall(
+      this.writeableStateStore2.pendingCallsSubject,
+      event,
+    );
+  };
+
+  /**
+   * Signals other users that I have cancelled my call to them before they accepted it.
+   * Causes the CallCancelled event to be emitted to all the call members.
+   * @param callCid config ID of the cancelled call
+   * @returns
+   */
   cancelCall = async (callCid: string) => {
     await this.client.sendEvent({
       callCid,
       eventType: UserEventType.CANCELLED_CALL,
     });
+  };
+
+  /**
+   * Event handler invoked upon delivery of CallCancelled Websocket event
+   * Updates the state store and notifies its subscribers that
+   * the call is now considered terminated.
+   * @param event received CallCancelled Websocket event
+   * @returns
+   */
+  onCallCancelled = (event: CallCancelled) => {
+    const { call } = event;
+    if (!call) {
+      console.log("Can't find call in CallCancelled event");
+      return;
+    }
+    this.writeableStateStore2.removeCall(
+      this.writeableStateStore2.pendingCallsSubject,
+      event,
+    );
   };
 
   /**
@@ -232,6 +352,7 @@ export class StreamVideoClient {
         response.call.call,
         response.edges,
       );
+      // todo: MC: remove stateStore
       if (data.input?.ring) {
         this.writeableStateStore.setCurrentValue(
           this.writeableStateStore.activeRingCallMetaSubject,
@@ -247,7 +368,8 @@ export class StreamVideoClient {
         const selectedEdge = response.edges.find((e) => e.name === edgeName);
         const { server, iceServers, token } = edge.credentials;
         const sfuClient = new StreamSfuClient(server.url, token, sessionId);
-        return new Call(
+        const callController = new Call(
+          response.call.call.id,
           sfuClient,
           {
             connectionConfig:
@@ -256,8 +378,19 @@ export class StreamVideoClient {
             latencyCheckUrl: selectedEdge?.latencyUrl,
             edgeName,
           },
+          // todo: MC: remove stateStore
           this.writeableStateStore,
+          this.writeableStateStore2,
         );
+        await callController.join();
+        const activeCall = this.writeableStateStore2.getCurrentValue(
+          this.writeableStateStore2.activeCallSubject,
+        );
+        this.writeableStateStore2.setCurrentValue(
+          this.writeableStateStore2.activeCallSubject,
+          { data: activeCall?.data, connection: callController },
+        );
+        return callController;
       } else {
         // TODO: handle error?
         return undefined;
