c90422be8cacd9b4e7984867bc241c803663844d
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2022-12-01 05:27:54 +0530

    feat(react-native): init sentry

--- packages/react-native-dogfood/index.js
+++ packages/react-native-dogfood/index.js
@@ -7,13 +7,14 @@ import 'react-native-get-random-values';
 
 import { registerGlobals } from 'react-native-webrtc';
 
+import { AppRegistry, Platform } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
 if (Platform.OS !== 'web') {
   registerGlobals();
 }
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
 
 AppRegistry.registerHeadlessTask(
   'RNCallKeepBackgroundMessage',
@@ -25,4 +26,9 @@ AppRegistry.registerHeadlessTask(
     },
 );
 
-AppRegistry.registerComponent(appName, () => App);
+Sentry.init({
+  dsn: 'https://3f853df83a5843bfa09a1e0dc785dbf6@o14368.ingest.sentry.io/4504248362926080',
+});
+
+const SentryApp = Sentry.wrap(App);
+AppRegistry.registerComponent(appName, () => SentryApp);
