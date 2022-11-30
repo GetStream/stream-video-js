8f531a6ac9f4f9a3b860a95ece31de44e66f038b
khushal87 <khushal.agarwal987@gmail.com>
2022-11-30 11:32:24 +0530

    fix: incoming call view video mute status

--- packages/react-native-dogfood/src/components/CallControls/VideoButton.tsx
+++ packages/react-native-dogfood/src/components/CallControls/VideoButton.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ButtonContainer from './ButtonContainer';
 import VideoSlash from '../../icons/VideoSlash';
 import Video from '../../icons/Video';
 import {
   useActiveCall,
   useLocalParticipant,
+  useStreamVideoStoreSetState,
+  useStreamVideoStoreValue,
 } from '@stream-io/video-react-native-sdk';
 
 const VideoButton = () => {
   const localParticipant = useLocalParticipant();
-  const isVideoMuted = !localParticipant?.video;
+  const videoMuted = useStreamVideoStoreValue((store) => store.isVideoMuted);
+  const streamSetState = useStreamVideoStoreSetState();
+  const isVideoMuted = !localParticipant?.video || videoMuted;
   const call = useActiveCall();
+
+  useEffect(() => {
+    if (videoMuted) {
+      call?.updateMuteState('video', videoMuted);
+    }
+  }, [call, videoMuted]);
 
   const toggleVideoState = async () => {
     call?.updateMuteState('video', !isVideoMuted);
+    streamSetState({ isVideoMuted: false });
   };
 
   return (
