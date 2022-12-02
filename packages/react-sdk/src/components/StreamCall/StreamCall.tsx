3482321dbd1bc80895510da763f6ac8782edeace
MartinCupela <martin.cupela@gmail.com>
2022-12-02 13:22:15 +0100

    feat(react-messenger-clone): leave call if all remote participants left

--- packages/react-sdk/src/components/StreamCall/StreamCall.tsx
+++ packages/react-sdk/src/components/StreamCall/StreamCall.tsx
@@ -3,6 +3,8 @@ import {
   useAcceptedCall,
   useActiveCall,
   useOutgoingCalls,
+  useRejectedCallNotifications,
+  useRemoteParticipants,
   useStreamVideoClient,
 } from '@stream-io/video-react-bindings';
 import { MediaDevicesProvider } from '../../contexts';
@@ -12,9 +14,15 @@ export const StreamCall = ({ children }: { children: ReactNode }) => {
   const activeCall = useActiveCall();
   const outgoingCalls = useOutgoingCalls();
   const acceptedCall = useAcceptedCall();
+  const remoteParticipants = useRemoteParticipants();
+  const rejectedCallNotifications = useRejectedCallNotifications();
 
   useEffect(() => {
     if (!videoClient) return;
+    const activeCallRejections = rejectedCallNotifications.filter(
+      (notification) =>
+        notification.call?.callCid === activeCall?.data.call?.callCid,
+    );
 
     const acceptedOrInitiatedCall =
       acceptedCall?.call || outgoingCalls[0]?.call;
@@ -25,11 +33,24 @@ export const StreamCall = ({ children }: { children: ReactNode }) => {
         // FIXME: OL optional, but it is marked as required in proto
         datacenterId: '',
       });
+    } else if (
+      activeCall &&
+      activeCallRejections.length > 0 &&
+      remoteParticipants.length === 0
+    ) {
+      activeCall?.leave();
     }
     return () => {
-      activeCall?.leave();
+      // activeCall?.leave();
     };
-  }, [activeCall, videoClient, outgoingCalls, acceptedCall]);
+  }, [
+    activeCall,
+    videoClient,
+    outgoingCalls,
+    acceptedCall,
+    remoteParticipants,
+    rejectedCallNotifications,
+  ]);
 
   if (!videoClient) return null;
 
