b03f7d13fe43827114962c732be4c2d87b1e2cd1
khushal87 <khushal.agarwal987@gmail.com>
2022-12-02 10:40:04 +0530

    fix: added improvements and suggestions

--- packages/react-native-sdk/src/utils/index.ts
+++ packages/react-native-sdk/src/utils/index.ts
@@ -2,12 +2,15 @@ import { MAX_AVATARS_IN_VIEW } from '../constants';
 
 // Utility to join strings with commas and 'and'
 export const generateCallTitle = (memberUserIds: string[]) => {
-  const members = memberUserIds.slice(0, MAX_AVATARS_IN_VIEW);
-  if (members.length < MAX_AVATARS_IN_VIEW) {
-    return members.join(' and ');
+  const supportedAmountOfMemberUserIds = memberUserIds.slice(
+    0,
+    MAX_AVATARS_IN_VIEW,
+  );
+  if (supportedAmountOfMemberUserIds.length < MAX_AVATARS_IN_VIEW) {
+    return supportedAmountOfMemberUserIds.join(' and ');
   }
 
-  const allMemebersExceptLast = members.slice(0, -1);
-  const lastMember = members.slice(-1)[0];
+  const allMemebersExceptLast = supportedAmountOfMemberUserIds.slice(0, -1);
+  const lastMember = supportedAmountOfMemberUserIds.slice(-1)[0];
   return `${allMemebersExceptLast.join(', ')}, and ${lastMember}`;
 };
