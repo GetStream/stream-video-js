7aaf17573a8a6cdadbabdddec68542e8ab5a1e62
MartinCupela <martin.cupela@gmail.com>
2022-12-05 16:40:06 +0100

    refactor(react-sdk): separate call joining and leaving into different effects

--- packages/react-sdk/src/components/StreamCall/StreamCall.tsx
+++ packages/react-sdk/src/components/StreamCall/StreamCall.tsx
@@ -6,38 +6,67 @@ import {
   useHangUpNotifications,
   useRemoteParticipants,
   useStreamVideoClient,
+  useIncomingCalls,
 } from '@stream-io/video-react-bindings';
-import { MediaDevicesProvider } from '../../contexts';
+import {
+  LocalMediaStreamsContextProvider,
+  MediaDevicesProvider,
+} from '../../contexts';
 
 export const StreamCall = ({ children }: { children: ReactNode }) => {
   const videoClient = useStreamVideoClient();
   const activeCall = useActiveCall();
+  const incomingCalls = useIncomingCalls();
   const outgoingCalls = useOutgoingCalls();
   const acceptedCall = useAcceptedCall();
   const remoteParticipants = useRemoteParticipants();
   const hangupNotifications = useHangUpNotifications();
 
   useEffect(() => {
-    if (!videoClient) return;
+    if (!(videoClient && acceptedCall)) return;
+
+    const callToJoin =
+      outgoingCalls.length > 0
+        ? outgoingCalls.find(
+            (c) => c.call?.callCid === acceptedCall?.call?.callCid,
+          )
+        : incomingCalls.length > 0
+        ? incomingCalls.find(
+            (c) => c.call?.callCid === acceptedCall?.call?.callCid,
+          )
+        : undefined;
+
+    if (callToJoin?.call) {
+      videoClient
+        ?.joinCall({
+          id: callToJoin.call.id,
+          type: callToJoin.call.type,
+          // FIXME: OL optional, but it is marked as required in proto
+          datacenterId: '',
+        })
+        .then((call) => call?.join());
+    }
+    return () => {
+      // activeCall?.leave();
+    };
+  }, [
+    activeCall,
+    videoClient,
+    outgoingCalls,
+    acceptedCall,
+    remoteParticipants,
+    hangupNotifications,
+    incomingCalls,
+  ]);
+
+  useEffect(() => {
+    if (!(videoClient && hangupNotifications.length > 0)) return;
     const hangups = hangupNotifications.filter(
       (notification) =>
         notification.call?.callCid === activeCall?.data.call?.callCid,
     );
 
-    const acceptedOrInitiatedCall =
-      acceptedCall?.call || outgoingCalls[0]?.call;
-    if (!activeCall && acceptedOrInitiatedCall) {
-      videoClient?.joinCall({
-        id: acceptedOrInitiatedCall.id,
-        type: acceptedOrInitiatedCall.type,
-        // FIXME: OL optional, but it is marked as required in proto
-        datacenterId: '',
-      });
-    } else if (
-      activeCall &&
-      hangups.length > 0 &&
-      remoteParticipants.length === 0
-    ) {
+    if (hangups.length > 0 && remoteParticipants.length === 0) {
       activeCall?.leave();
     }
     return () => {
@@ -50,9 +79,16 @@ export const StreamCall = ({ children }: { children: ReactNode }) => {
     acceptedCall,
     remoteParticipants,
     hangupNotifications,
+    incomingCalls,
   ]);
 
   if (!videoClient) return null;
 
-  return <MediaDevicesProvider>{children}</MediaDevicesProvider>;
+  return (
+    <MediaDevicesProvider>
+      <LocalMediaStreamsContextProvider>
+        {children}
+      </LocalMediaStreamsContextProvider>
+    </MediaDevicesProvider>
+  );
 };
