327f412ff190de2234779ef35435d6801b6efcc0
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2022-12-05 17:18:23 +0530

    fix: change doc entry point to use index

--- packages/react-native-sdk/scripts/generate-docs.sh
+++ packages/react-native-sdk/scripts/generate-docs.sh
@@ -8,19 +8,16 @@ yarn generate-docs:react:bindings > /dev/null
 
 echo "Generating docs from React Native SDK..."
 # clean up old docs
-npx rimraf generated-docs
+rm -rf generated-docs
 # generate new docs
 npx typedoc --options typedoc.json
 
 # preprocess the docs to our specific needs
-npx replace-in-file '# Interface: ' '# ' 'temp-docs/**' > /dev/null
-npx replace-in-file '# Module: ' '# ' 'temp-docs/**' > /dev/null
-npx replace-in-file '.md' '' 'temp-docs/**' > /dev/null
+npx replace-in-file '# @stream-io/video-react-native-sdk' '# Components' 'temp-docs/**' > /dev/null
 
 # copy from the temp-docs to the structure we want in docusaurus
 mkdir generated-docs
-cp -r temp-docs/interfaces/ generated-docs/Interfaces
-cp -r temp-docs/modules/ generated-docs/Modules
+cp temp-docs/modules.md generated-docs/components.md
 rm -rf temp-docs
 
 # copy the docs to react-native docusaurus
