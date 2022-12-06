2b5a9551cbe1f49b23eb4c3266ca40b043f2747b
Anton Arnautov <arnautov.anton@gmail.com>
2022-12-06 20:23:03 +0100

    feat(react-sdk): add useLocalMediaStreamSetup hook

--- packages/react-sdk/src/contexts/LocalMediaStreamsContext.tsx
+++ packages/react-sdk/src/contexts/LocalMediaStreamsContext.tsx
@@ -1,8 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import {
-  useIncomingCalls,
-  useOutgoingCalls,
-} from '@stream-io/video-react-bindings';
+import { usePendingCalls } from '@stream-io/video-react-bindings';
 import { getAudioStream, getVideoStream } from '@stream-io/video-client';
 import { useMediaDevices } from './MediaDevicesContext';
 
@@ -15,63 +12,85 @@ const LocalMediaStreamsContext = createContext<MediaStreamsContextType>({
   localAudioStream: undefined,
   localVideoStream: undefined,
 });
+
+const getDeviceId = (mediaStream: MediaStream) => {
+  // considering the fact that we create separate MediaStreams for
+  // audio inputs and video inputs meaning one track per stream (one deviceId)
+  const [deviceId] =
+    mediaStream.getTracks().map((track) => track.getSettings().deviceId) ?? [];
+  return deviceId;
+};
+
+const getStream = (type: 'audioinput' | 'videoinput', deviceId?: string) =>
+  type === 'audioinput' ? getAudioStream(deviceId) : getVideoStream(deviceId);
+
+const useLocalMediaStreamSetup = ({
+  localStream,
+  selectedDeviceId,
+  setLocalStream,
+  type,
+}: {
+  localStream?: MediaStream;
+  setLocalStream: React.Dispatch<React.SetStateAction<MediaStream | undefined>>;
+  selectedDeviceId?: string;
+  type: 'audioinput' | 'videoinput';
+}) => {
+  const pendingCalls = usePendingCalls();
+  const { switchDevice } = useMediaDevices();
+
+  useEffect(() => {
+    let effectInterrupted = false;
+
+    const cleanup = () => {
+      effectInterrupted = true;
+      localStream?.getTracks().forEach((t) => {
+        console.log(t);
+        t.stop();
+      });
+    };
+
+    if (!pendingCalls.length) return cleanup;
+
+    if (localStream?.active) {
+      const deviceId = getDeviceId(localStream);
+      if (deviceId === selectedDeviceId) return;
+    }
+
+    getStream(type, selectedDeviceId).then((stream) => {
+      if (effectInterrupted) return;
+      setLocalStream(stream);
+      if (selectedDeviceId) return;
+      const deviceId = getDeviceId(stream);
+      // @ts-ignore
+      switchDevice(type, deviceId);
+    });
+
+    return cleanup;
+  }, [localStream, pendingCalls, selectedDeviceId]);
+};
+
 export const LocalMediaStreamsContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const incomingCalls = useIncomingCalls();
-  const outgoingCalls = useOutgoingCalls();
-  const { selectedAudioDeviceId, selectedVideoDeviceId, switchDevice } =
-    useMediaDevices();
+  const { selectedAudioDeviceId, selectedVideoDeviceId } = useMediaDevices();
   const [localAudioStream, setLocalAudioStream] = useState<MediaStream>();
   const [localVideoStream, setLocalVideoStream] = useState<MediaStream>();
 
-  useEffect(() => {
-    if (!(incomingCalls.length + outgoingCalls.length)) return;
+  useLocalMediaStreamSetup({
+    localStream: localAudioStream,
+    setLocalStream: setLocalAudioStream,
+    selectedDeviceId: selectedAudioDeviceId,
+    type: 'audioinput',
+  });
 
-    if (localAudioStream?.active) {
-      const [t] =
-        localAudioStream
-          .getAudioTracks()
-          .map((track) => track.getSettings().deviceId) ?? [];
-
-      if (t === selectedAudioDeviceId) return;
-    }
-
-    getAudioStream(selectedAudioDeviceId).then((stream) => {
-      setLocalAudioStream(stream);
-      if (selectedAudioDeviceId) return;
-      const [deviceId] = stream
-        .getAudioTracks()
-        .map((v) => v.getSettings().deviceId);
-      // @ts-ignore
-      switchDevice('audioinput', deviceId);
-    });
-  }, [localAudioStream, incomingCalls, outgoingCalls, selectedAudioDeviceId]);
-
-  useEffect(() => {
-    if (!(incomingCalls.length + outgoingCalls.length)) return;
-
-    if (localVideoStream?.active) {
-      const [deviceId] =
-        localVideoStream
-          .getVideoTracks()
-          .map((track) => track.getSettings().deviceId) ?? [];
-
-      if (deviceId === selectedVideoDeviceId) return;
-    }
-
-    getVideoStream(selectedVideoDeviceId).then((stream) => {
-      setLocalVideoStream(stream);
-      if (selectedVideoDeviceId) return;
-      const [deviceId] = stream
-        .getVideoTracks()
-        .map((v) => v.getSettings().deviceId);
-      // @ts-ignore
-      switchDevice('videoinput', deviceId);
-    });
-  }, [localVideoStream, incomingCalls, outgoingCalls, selectedVideoDeviceId]);
+  useLocalMediaStreamSetup({
+    localStream: localVideoStream,
+    setLocalStream: setLocalVideoStream,
+    selectedDeviceId: selectedVideoDeviceId,
+    type: 'videoinput',
+  });
 
   return (
     <LocalMediaStreamsContext.Provider
