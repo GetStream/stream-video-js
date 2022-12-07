44fc5e894296f53da5cb6b02d705ffc361e045a1
Steve Galili <galiliziv@gmail.com>
2022-12-07 12:25:15 +0100

    Sonar and test config (#112)
    ## Goal
    - Code analysis addition
    - Being more code coverage aware
    - Making writing testing easier and maintainable
    
    ## Implementation
    - Code analysis via
    [Sonarcloud](https://sonarcloud.io/organizations/getstream/projects)
    - Code coverage is being generated in CI/CD step and is a part of
    [Sonar's
    quality](https://docs.sonarcloud.io/improving/quality-gates/#:~:text=A%20quality%20gate%20is%20an,the%20results%20of%20each%20analysis.).
    - Mocking and data faking made easier via
    [vitest-mock-extended](https://github.com/eratio08/vitest-mock-extended).
    
    ## What's Next and Discussions
    - [Loom video learn about Sonar, and how we use
    it](https://www.loom.com/share/ffabc1aaa9424737854341f4c41ee679)
    - Should coverage of 80% be mandatory in the client package?
    - Testing and increasing code coverage.
    - A short Loom about Sonar and trick and tips for PR reviewers and
    authors.

---
+++ packages/client/src/__tests__/StreamVideoClient.test.ts
@@ -0,0 +1,86 @@
+import { describe, it, vi, beforeEach, expect } from 'vitest';
+import { StreamVideoClient } from '../StreamVideoClient';
+import { createSocketConnection } from '../ws';
+import { StreamVideoParticipant } from '../rtc/types';
+import { mock } from 'vitest-mock-extended';
+
+describe('StreamVideoClient', () => {
+  let client: StreamVideoClient;
+  const getPinnedParticipants = () =>
+    client.writeableStateStore.getCurrentValue(
+      client.writeableStateStore.pinnedParticipants$,
+    );
+
+  beforeEach(() => {
+    vi.mock('../rpc/createClient', () => {
+      return {
+        createCoordinatorClient: vi.fn(),
+        withHeaders: vi.fn(),
+      };
+    });
+    vi.mock('../ws/connection', () => {
+      return {
+        createSocketConnection: vi.fn(),
+      };
+    });
+    vi.mock('../stats/coordinator-stats-reporter');
+    client = new StreamVideoClient('123', {
+      token: 'abc',
+    });
+  });
+
+  it('should connect', async () => {
+    const user = {
+      id: 'marcelo',
+      name: 'marcelo',
+      role: 'admin',
+      teams: ['team-1, team-2'],
+      imageUrl: '/profile.png',
+      customJson: new Uint8Array(),
+    };
+    const apiKey = '123';
+    const token = 'abc';
+    await client.connect(apiKey, token, user);
+
+    expect(createSocketConnection).toHaveBeenCalledWith(
+      expect.anything(),
+      apiKey,
+      token,
+      user,
+    );
+  });
+
+  it('does not pin a participant ', async () => {
+    client.setParticipantIsPinned('non-existing-essionid', true);
+
+    const participantsInStore = client.writeableStateStore.getCurrentValue(
+      client.writeableStateStore.pinnedParticipants$,
+    );
+    expect(participantsInStore).toHaveLength(0);
+  });
+
+  it('does pin one participant which populates pinnedParticipants', async () => {
+    const p1 = mock<StreamVideoParticipant>({
+      isPinned: false,
+      sessionId: '123abc',
+      publishedTracks: [],
+    });
+    const p2 = mock<StreamVideoParticipant>({
+      isPinned: false,
+      sessionId: '456def',
+    });
+    let pinnedParticipants = getPinnedParticipants();
+    expect(pinnedParticipants).toHaveLength(0);
+
+    client.writeableStateStore.setCurrentValue(
+      client.writeableStateStore.activeCallAllParticipantsSubject,
+      [p1, p2],
+    );
+
+    client.setParticipantIsPinned(p2.sessionId, true);
+
+    pinnedParticipants = getPinnedParticipants();
+    expect(pinnedParticipants).toHaveLength(1);
+    expect(pinnedParticipants[0].sessionId).toEqual(p2.sessionId);
+  });
+});
