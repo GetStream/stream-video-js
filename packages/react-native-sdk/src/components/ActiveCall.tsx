5c9931aed4d937bca80311f88ec530eabf32ae64
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-12-08 11:53:30 +0530

    feat: added ActiveCall view to the React Native SDK (#125)
    This PR deals with adding ActiveCall to the RN SDK.

---
+++ packages/react-native-sdk/src/components/ActiveCall.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import {
+  useActiveCall,
+  useTerminatedRingCall,
+} from '@stream-io/video-react-bindings';
+import { StyleSheet, View } from 'react-native';
+import { useCallKeep } from '../hooks';
+import { CallControlsView } from './CallControlsView';
+import { CallParticipantsView } from './CallParticipantsView';
+
+export type ActiveCallProps = {
+  /**
+   * Handler called when the call is hanged up by the caller. Mostly used for navigation and related actions.
+   */
+  onHangupCall: () => void;
+};
+
+export const ActiveCall = (props: ActiveCallProps) => {
+  const activeCall = useActiveCall();
+  const terminatedRingCall = useTerminatedRingCall();
+  const { startCall, endCall } = useCallKeep();
+  const { onHangupCall } = props;
+
+  useEffect(() => {
+    startCall();
+    if (!activeCall || terminatedRingCall) {
+      endCall();
+      onHangupCall();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeCall, terminatedRingCall]);
+
+  return (
+    <>
+      <View style={styles.callParticipantsWrapper}>
+        <CallParticipantsView />
+      </View>
+      <CallControlsView />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  callParticipantsWrapper: { flex: 1, marginBottom: -20 },
+});
