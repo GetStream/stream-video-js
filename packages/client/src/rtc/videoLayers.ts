fd61c55ed65c544ed2d7f7d627009881d2b050fd
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2022-12-08 18:03:45 +0100

    fix: simulcast tracks need to be ordered from low to high (#129)
    In case there are CPU/Bandwidth constraints, most "expensive" layer
    should be disabled. The browsers do this automatically, and they look
    into Simulcast track indices...

--- packages/client/src/rtc/videoLayers.ts
+++ packages/client/src/rtc/videoLayers.ts
@@ -8,7 +8,6 @@ export type OptimalVideoLayer = RTCRtpEncodingParameters & {
 };
 
 export const findOptimalVideoLayers = (videoTrack: MediaStreamTrack) => {
-  // https://chromium.googlesource.com/external/webrtc/+/refs/heads/main/media/engine/simulcast.cc#90
   const steps: [number, number, number][] = [
     [1920, 1080, 3000000],
     [1280, 720, 1250000],
@@ -26,7 +25,10 @@ export const findOptimalVideoLayers = (videoTrack: MediaStreamTrack) => {
     if (w === settings.width && h === settings.height) {
       let scaleFactor: number = 1;
       ['f', 'h', 'q'].forEach((rid) => {
-        optimalVideoLayers.push({
+        // Reversing the order [f, h, q] to [q, h, f] as Chrome uses encoding index
+        // when deciding which layer to disable when CPU or bandwidth is constrained.
+        // Encodings should be ordered in increasing spatial resolution order.
+        optimalVideoLayers.unshift({
           active: true,
           rid,
           width: w / scaleFactor,
@@ -45,7 +47,42 @@ export const findOptimalVideoLayers = (videoTrack: MediaStreamTrack) => {
       break;
     }
   }
-  return optimalVideoLayers;
+  // for simplicity, we start with all layers enabled, then this function
+  // will clear/reassign the layers that are not needed
+  return withSimulcastConstraints(settings, optimalVideoLayers);
+};
+
+/**
+ * Browsers have different simulcast constraints for different video resolutions.
+ *
+ * This function modifies the provided list of video layers according to the
+ * current implementation of simulcast constraints in the Chromium based browsers.
+ *
+ * https://chromium.googlesource.com/external/webrtc/+/refs/heads/main/media/engine/simulcast.cc#90
+ */
+const withSimulcastConstraints = (
+  settings: MediaTrackSettings,
+  optimalVideoLayers: OptimalVideoLayer[],
+) => {
+  let layers;
+
+  const size = Math.max(settings.width || 0, settings.height || 0);
+  if (size <= 320) {
+    // provide only one layer 320x240 (q), the one with the highest quality
+    layers = optimalVideoLayers.filter((layer) => layer.rid === 'f');
+  } else if (size <= 640) {
+    // provide two layers, 160x120 (q) and 640x480 (h)
+    layers = optimalVideoLayers.filter((layer) => layer.rid !== 'h');
+  } else {
+    // provide three layers for sizes > 640x480
+    layers = optimalVideoLayers;
+  }
+
+  const ridMapping = ['q', 'h', 'f'];
+  return layers.map((layer, index) => ({
+    ...layer,
+    rid: ridMapping[index], // reassign rid
+  }));
 };
 
 export const findOptimalScreenSharingLayers = (
@@ -55,7 +92,7 @@ export const findOptimalScreenSharingLayers = (
   return [
     {
       active: true,
-      rid: 'f',
+      rid: 'q', // single track, start from 'q'
       width: settings.width || 0,
       height: settings.height || 0,
       maxBitrate: 3000000,
