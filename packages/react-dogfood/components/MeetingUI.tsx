3e7f54e9c1d41f29fca3f3a60b6460d249df4959
MartinCupela <martin.cupela@gmail.com>
2022-12-16 11:17:49 +0100

    feat(react-sdk): allow for executing an onLeave callback in CallControls

--- packages/react-dogfood/components/MeetingUI.tsx
+++ packages/react-dogfood/components/MeetingUI.tsx
@@ -1,31 +1,21 @@
-import {
-  useActiveCall,
-  useLocalHangUpNotifications,
-} from '@stream-io/video-react-bindings';
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+import { useActiveCall } from '@stream-io/video-react-bindings';
 import {
   CallControls,
   DeviceSettings,
   Stage,
 } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/router';
 
-type MeetingUIProps = {
-  callId: string;
-};
-
-export const MeetingUI = ({ callId }: MeetingUIProps) => {
+export const MeetingUI = () => {
   const router = useRouter();
   const activeCall = useActiveCall();
-  const hangups = useLocalHangUpNotifications();
+
+  const onLeave = useCallback(() => {
+    router.push('/');
+  }, [router]);
 
   if (!activeCall) return <div>loading</div>;
-
-  const shouldLeave = hangups.find((hangup) => hangup.call.id === callId);
-
-  if (shouldLeave) {
-    activeCall.leave();
-    router.push('/');
-  }
 
   const { type, id } = activeCall.data.call;
 
@@ -38,7 +28,7 @@ export const MeetingUI = ({ callId }: MeetingUIProps) => {
         <DeviceSettings activeCall={activeCall} />
       </div>
       <Stage call={activeCall} />
-      <CallControls call={activeCall} />
+      <CallControls call={activeCall} onLeave={onLeave} />
     </div>
   );
 };
