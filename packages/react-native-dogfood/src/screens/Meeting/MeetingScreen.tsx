ddf6dc141d7a39f7c579bfa80de02e1439b55acb
khushal87 <khushal.agarwal987@gmail.com>
2022-12-16 17:22:51 +0530

    fix: navigation in meeting flow

--- packages/react-native-dogfood/src/screens/Meeting/MeetingScreen.tsx
+++ packages/react-native-dogfood/src/screens/Meeting/MeetingScreen.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { ActiveCall, useActiveCall } from '@stream-io/video-react-native-sdk';
+import { ActiveCall } from '@stream-io/video-react-native-sdk';
 import { MeetingStackParamList } from '../../../types';
-import { ActivityIndicator, StyleSheet } from 'react-native';
 
 type Props = NativeStackScreenProps<MeetingStackParamList, 'MeetingScreen'>;
 
 export const MeetingScreen = ({ navigation }: Props) => {
-  const activeCall = useActiveCall();
-
-  return activeCall ? (
+  return (
     <ActiveCall onHangupCall={() => navigation.navigate('JoinMeetingScreen')} />
-  ) : (
-    <ActivityIndicator size={'large'} style={StyleSheet.absoluteFill} />
   );
 };
