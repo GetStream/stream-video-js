2d5eb6ce950ad32a14c484c27425ecbdac2f7156
Khushal Agarwal <khushal.agarwal987@gmail.com>
2022-12-22 22:03:19 +0530

    feat: add ParticipantsInfoView component to RN SDK (#151)
    This PR deals with adding CallParticipantsInfoView to the RN SDK.
    
    * Android:
    
    
    https://user-images.githubusercontent.com/39884168/208828222-efbfe6d3-29ae-4110-b7cb-e8183e8798ac.mov
    
    
    * iOS
    
    
    https://user-images.githubusercontent.com/39884168/208828207-70610787-92d2-4575-84b5-9e946737a0bb.mp4
    
    Co-authored-by: Anton Arnautov <arnautov.anton@gmail.com>
    Co-authored-by: MartinCupela <martin.cupela@gmail.com>
    Co-authored-by: Santhosh Vaiyapuri <3846977+santhoshvai@users.noreply.github.com>
    Co-authored-by: Santhosh Vaiyapuri <santhoshvai@gmail.com>
    Co-authored-by: Oliver Lazoroski <oliver.lazoroski@gmail.com>
    Co-authored-by: Sam Jeeves <jeevcat@users.noreply.github.com>
    Co-authored-by: MartinCupela <32706194+MartinCupela@users.noreply.github.com>

---
+++ packages/react-native-sdk/src/icons/Spotlight.tsx
@@ -0,0 +1,16 @@
+import { Svg, Path } from 'react-native-svg';
+
+type Props = {
+  color: string;
+};
+
+export const SpotLight = ({ color }: Props) => (
+  <Svg viewBox="0 0 24 24">
+    <Path
+      fill={color}
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M 12 22 C 17.5228 22 22 17.5228 22 12 C 22 6.47715 17.5228 2 12 2 C 6.47715 2 2 6.47715 2 12 C 2 17.5228 6.47715 22 12 22 Z M 12 24 C 18.6274 24 24 18.6274 24 12 C 24 5.37258 18.6274 0 12 0 C 5.37258 0 0 5.37258 0 12 C 0 18.6274 5.37258 24 12 24 Z"
+    />
+  </Svg>
+);
