15b0ca0e25c47a22f9ba6b710e5920858452d056
vanGalilea <galiliziv@gmail.com>
2022-12-24 18:00:53 +0100

    feat: tutorial step 1

--- packages/react-native-meeting-starter-kit/src/hooks/useVideoClient.tsx
+++ packages/react-native-meeting-starter-kit/src/hooks/useVideoClient.tsx
@@ -1,10 +1,15 @@
 import {StreamVideoClient, User} from '@stream-io/video-client';
 import {useEffect, useState} from 'react';
 
-const APIParams = {
-  apiKey: '5mxvmc2t4qys', // see <video>/data/fixtures/apps.yaml for API key/secret
-  apiSecret: 'u54nds9v328s4b6g56juvsmj5j9nevetdqjszwdt2qr5ubfkswh5rjhmzuw9rvd4',
+const DFA_API_PARAMS = {
+  apiKey: 'us83cfwuhy8n', // see <video>/data/fixtures/apps.yaml for API key/secret
+  apiSecret: '4rvyp4jv4da46f36ep96ck4yshjmrtweme2xujv4cftepprckqn9r3w8sn7aywcx',
 };
+
+// const MC_API_PARAMS = {
+//   apiKey: '5mxvmc2t4qys', // see <video>/data/fixtures/apps.yaml for API key/secret
+//   apiSecret: 'u54nds9v328s4b6g56juvsmj5j9nevetdqjszwdt2qr5ubfkswh5rjhmzuw9rvd4',
+// };
 type VideoProps = {
   user: User;
   token: string;
@@ -27,13 +32,13 @@ export const useVideoClient = ({user, token}: VideoProps) => {
       };
 
       try {
-        const _videoClient = new StreamVideoClient(APIParams.apiKey, {
+        const _videoClient = new StreamVideoClient(DFA_API_PARAMS.apiKey, {
           coordinatorWsUrl: clientParams.coordinatorWsUrl,
           coordinatorRpcUrl: clientParams.coordinatorRpcUrl,
           sendJson: true,
           token,
         });
-        await _videoClient.connect(APIParams.apiKey, token, user);
+        await _videoClient.connect(DFA_API_PARAMS.apiKey, token, user);
         setVideoClient(_videoClient);
       } catch (err) {
         console.error('Failed to establish connection', err);
