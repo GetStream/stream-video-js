ac1f56bb8c89172a1815a166e60a180f34a93b65
Zita Szupera <szuperaz@gmail.com>
2023-01-02 16:26:07 +0100

    fix: Angular documentation generation problem

--- packages/angular-sdk/projects/video-angular-sdk/src/lib/call-participants-screenshare/call-participants-screenshare.component.spec.ts
+++ packages/angular-sdk/projects/video-angular-sdk/src/lib/call-participants-screenshare/call-participants-screenshare.component.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CallParticipantsScreenshareViewComponent } from './call-participants-screenshare.component';
+import { CallParticipantsScreenshareComponent } from './call-participants-screenshare.component';
 
 describe('CallParticipantsScreenshareViewComponent', () => {
-  let component: CallParticipantsScreenshareViewComponent;
-  let fixture: ComponentFixture<CallParticipantsScreenshareViewComponent>;
+  let component: CallParticipantsScreenshareComponent;
+  let fixture: ComponentFixture<CallParticipantsScreenshareComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CallParticipantsScreenshareViewComponent],
+      declarations: [CallParticipantsScreenshareComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CallParticipantsScreenshareViewComponent);
+    fixture = TestBed.createComponent(CallParticipantsScreenshareComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- packages/angular-sdk/projects/video-angular-sdk/src/lib/call-participants/call-participants.component.spec.ts
+++ packages/angular-sdk/projects/video-angular-sdk/src/lib/call-participants/call-participants.component.spec.ts
@@ -1,17 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CallParticipantsViewComponent } from './call-participants.component';
+import { CallParticipantsComponent } from './call-participants.component';
 
 describe('CallParticipantsViewComponent', () => {
-  let component: CallParticipantsViewComponent;
-  let fixture: ComponentFixture<CallParticipantsViewComponent>;
+  let component: CallParticipantsComponent;
+  let fixture: ComponentFixture<CallParticipantsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CallParticipantsViewComponent],
+      declarations: [CallParticipantsComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CallParticipantsViewComponent);
+    fixture = TestBed.createComponent(CallParticipantsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
