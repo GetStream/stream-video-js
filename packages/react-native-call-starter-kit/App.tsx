f2868c89eed4acbfcc4708feb3decd19946a193f
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-01-10 22:23:22 +0530

    feat: added react native call starter kit (#154)
    This PR deals with adding React Native Messenger Starter Kit to the
    project.
    
    Co-authored-by: vanGalilea <galiliziv@gmail.com>

---
+++ packages/react-native-call-starter-kit/App.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import {UserList} from './src/components/UserList';
+import {NavigationStackParamsList} from './src/types';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {useStreamChatTheme} from './useStreamChatTheme';
+import {
+  NativeStackHeaderProps,
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import {ChannelListScreen} from './src/screens/ChannelListScreen';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {ChannelScreen} from './src/screens/ChannelScreen';
+import {ThreadScreen} from './src/screens/ThreadScreen';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {NavigationHeader} from './src/components/NavigationHeader';
+import {MessengerWrapper} from './src/components/MessengerWrapper';
+import {ChannelHeader} from './src/components/ChannelHeader';
+import IncomingCallScreen from './src/screens/IncomingCallScreen';
+import {ActiveCallScreen} from './src/screens/ActiveCallScreen';
+import OutgoingCallScreen from './src/screens/OutgoingCallScreen';
+import {CallParticipansInfoScreen} from './src/screens/CallParticipantsInfoScreen';
+import {AppProvider, useAppContext} from './src/context/AppContext';
+
+const Stack = createNativeStackNavigator<NavigationStackParamsList>();
+
+function ChannelHeaderComponent(props: NativeStackHeaderProps) {
+  return <ChannelHeader {...props} />;
+}
+
+const Messenger = () => {
+  const {userId, userToken} = useAppContext();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<NavigationStackParamsList>>();
+
+  if (!(userId && userToken)) {
+    return (
+      <SafeAreaView>
+        <UserList />
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <MessengerWrapper navigation={navigation}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ChannelListScreen"
+          component={ChannelListScreen}
+          options={{header: NavigationHeader}}
+        />
+        <Stack.Screen
+          name="ChannelScreen"
+          component={ChannelScreen}
+          options={{
+            header: ChannelHeaderComponent,
+          }}
+        />
+        <Stack.Screen name="ThreadScreen" component={ThreadScreen} />
+        <Stack.Screen
+          name="IncomingCallScreen"
+          component={IncomingCallScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ActiveCallScreen"
+          component={ActiveCallScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="OutgoingCallScreen"
+          component={OutgoingCallScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="CallParticipantsInfoScreen"
+          component={CallParticipansInfoScreen}
+        />
+      </Stack.Navigator>
+    </MessengerWrapper>
+  );
+};
+
+const App = () => {
+  const theme = useStreamChatTheme();
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider
+        style={[
+          styles.container,
+          {backgroundColor: theme.colors?.white_snow || '#FCFCFC'},
+        ]}>
+        <NavigationContainer>
+          <AppProvider>
+            <Messenger />
+          </AppProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ packages/react-native-call-starter-kit/src/components/MessengerWrapper.tsx
@@ -0,0 +1,109 @@
+import {
+  Chat,
+  OverlayProvider,
+  Streami18n,
+  useChatContext,
+} from 'stream-chat-react-native';
+import React, {PropsWithChildren, useCallback, useMemo} from 'react';
+import {useVideoClient} from '../hooks/useVideoClient';
+import {StreamCall, StreamVideo} from '@stream-io/video-react-native-sdk';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {userFromToken} from '../utils/userFromToken';
+import {useChatClient} from '../hooks/useChatClient';
+import {useStreamChatTheme} from '../../useStreamChatTheme';
+import {AuthProgressLoader} from './AuthProgressLoader';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import type {
+  NavigationStackParamsList,
+  StreamChatGenerics,
+  VideoProps,
+} from '../types';
+import {STREAM_API_KEY} from 'react-native-dotenv';
+import {useAppContext} from '../context/AppContext';
+console.log('STREAM_API_KEY', STREAM_API_KEY);
+export const VideoWrapper = ({
+  children,
+  navigation,
+}: PropsWithChildren<{
+  navigation: NativeStackNavigationProp<NavigationStackParamsList>;
+}>) => {
+  const {client} = useChatContext<StreamChatGenerics>();
+
+  const user = useMemo<VideoProps['user']>(
+    () => ({
+      id: client.user?.id as string,
+      name: client.user?.name as string,
+      role: client.user?.role as string,
+      imageUrl: client.user?.image as string,
+      teams: [],
+      customJson: new Uint8Array(),
+    }),
+    [client.user],
+  );
+  const token = client._getToken() || '';
+
+  const {videoClient} = useVideoClient({user, token});
+
+  const acceptCallHandler = useCallback(() => {
+    navigation.navigate('ActiveCallScreen');
+  }, [navigation]);
+
+  const outgoingCallHandler = useCallback(() => {
+    navigation.navigate('OutgoingCallScreen');
+  }, [navigation]);
+
+  const incomingCallHandler = useCallback(() => {
+    navigation.navigate('IncomingCallScreen');
+  }, [navigation]);
+
+  if (!videoClient) {
+    return <AuthProgressLoader />;
+  }
+
+  return (
+    <StreamVideo client={videoClient}>
+      <StreamCall
+        onAcceptCall={acceptCallHandler}
+        onOutgoingCall={outgoingCallHandler}
+        onIncomingCall={incomingCallHandler}>
+        {children}
+      </StreamCall>
+    </StreamVideo>
+  );
+};
+
+const streami18n = new Streami18n({
+  language: 'en',
+});
+
+export const MessengerWrapper = ({
+  children,
+  navigation,
+}: PropsWithChildren<{
+  navigation: NativeStackNavigationProp<NavigationStackParamsList>;
+}>) => {
+  const {userToken} = useAppContext();
+  const user = useMemo(() => userFromToken(userToken), [userToken]);
+  const chatClient = useChatClient({
+    apiKey: STREAM_API_KEY,
+    userData: user,
+    tokenOrProvider: userToken,
+  });
+  const {bottom} = useSafeAreaInsets();
+  const theme = useStreamChatTheme();
+
+  if (!chatClient) {
+    return <AuthProgressLoader />;
+  }
+
+  return (
+    <OverlayProvider<StreamChatGenerics>
+      bottomInset={bottom}
+      i18nInstance={streami18n}
+      value={{style: theme}}>
+      <Chat client={chatClient} i18nInstance={streami18n}>
+        <VideoWrapper navigation={navigation}>{children}</VideoWrapper>
+      </Chat>
+    </OverlayProvider>
+  );
+};
