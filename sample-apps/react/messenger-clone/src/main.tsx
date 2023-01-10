ded0b7db708efa2b7ab41fbf3f23ed9c88d31d09
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-01-10 15:08:56 +0100

    fix: video styles didn't load

--- sample-apps/react/messenger-clone/src/main.tsx
+++ sample-apps/react/messenger-clone/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import '@stream-io/video-styling/dist/css/styles.css';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
