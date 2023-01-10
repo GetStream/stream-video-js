9241f9c8752ff2f2405ecac63d9df9d45e6d2ab9
Zita Szupera <szuperaz@gmail.com>
2023-01-10 15:32:02 +0100

    feat(angular-zoom-clone): chat integration

--- packages/angular-sdk/projects/zoom-clone/src/environments/environment.ts
+++ packages/angular-sdk/projects/zoom-clone/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
     'ws://localhost:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
   apiKey: 'us83cfwuhy8n',
   sfuRpcUrl: 'http://localhost:3031/twirp',
+  chatApiKey: 'qygw7k5cvvkx',
 };
 
 /*
