1b0426b8ea900a6112a802197de6b3d27ba71b29
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-01-19 11:42:02 +0530

    fix(react-native): copy call keep options type for exporting

--- packages/react-native-sdk/src/types/CallKeepOptions.ts
+++ packages/react-native-sdk/src/types/CallKeepOptions.ts
@@ -1,2 +1,27 @@
-type RNCallKeepType = typeof import('react-native-callkeep').default;
-export type CallKeepOptions = Parameters<RNCallKeepType['setup']>[0];
+// copied this snippet from IOptions in react-native-callkeep/index.d.ts:
+export interface CallKeepOptions {
+  ios: {
+    appName: string;
+    imageName?: string;
+    supportsVideo?: boolean;
+    maximumCallGroups?: string;
+    maximumCallsPerCallGroup?: string;
+    ringtoneSound?: string;
+    includesCallsInRecents?: boolean;
+  };
+  android: {
+    alertTitle: string;
+    alertDescription: string;
+    cancelButton: string;
+    okButton: string;
+    imageName?: string;
+    additionalPermissions: string[];
+    selfManaged?: boolean;
+    foregroundService?: {
+      channelId: string;
+      channelName: string;
+      notificationTitle: string;
+      notificationIcon?: string;
+    };
+  };
+}
