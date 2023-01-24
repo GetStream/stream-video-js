0ac356c303bfe8e2e313f877a44b81d656c45e6c
zwaardje <niels@seiton.io>
2023-01-24 10:43:30 +0100

    feat: LatencyPopup add popup
    Popup that will be displayed on the map

---
+++ sample-apps/react-video-demo/src/components/LatencyMapPopup/LatencyMapPopup.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+
+export type Props = {
+  abbriviation: string;
+  city: string;
+  countryCode: string;
+};
+
+export const LatencyMapPopup: FC<Props> = ({
+  abbriviation,
+  city,
+  countryCode,
+}) => (
+  <div className="relative">
+    <div className="bg-white rounded p-3">
+      <h3 className="font-bold flex items-center">
+        <span className="bg-green block rounded-full w-2 h-2 mr-2"></span>
+        {abbriviation}
+      </h3>
+      <p className="font-inter text-sm text-map-popup-description">
+        {city}, {countryCode}
+      </p>
+    </div>
+    <div className="w-8 overflow-hidden inline-block absolute top-18  ml-2">
+      <div className="h-3 w-3 bg-white -rotate-45 transform origin-top-left"></div>
+    </div>
+  </div>
+);
