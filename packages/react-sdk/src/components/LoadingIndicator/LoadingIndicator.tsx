51d42e3cf268137809594110e2a1c771eae91784
martincupela <martin.cupela@gmail.com>
2023-01-25 16:56:15 +0100

    fix(react-sdk): show text under the LoadingIndicator icon if set

--- packages/react-sdk/src/components/LoadingIndicator/LoadingIndicator.tsx
+++ packages/react-sdk/src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -11,7 +11,8 @@ export const LoadingIndicator = ({
   text,
 }: LoadingIndicatorProps) => {
   return (
-    <div className={clsx('str-video__loading-indicator', type)}>
+    <div className="str-video__loading-indicator">
+      <div className={clsx('str-video__loading-indicator__icon', type)}></div>
       {text && <p className="str-video__loading-indicator-text">{text}</p>}
     </div>
   );
