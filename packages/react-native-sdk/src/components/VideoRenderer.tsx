e7477707fd62889505ed1f806d00cd442b3025f3
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-01-26 14:57:51 +0530

    feat(react-native): bring hidden participants to view if isSpeaking and dominant speaker true (#191)
    ### Goal
    
    This PR deals with bringing dominant and speaking participants to the
    visible participant's area.
    The local participant always occupies the first position, as handled in
    #183. The 2nd position will be taken by the dominant speaker, followed
    by the speaking participants, and the rest are the non-speaking
    participants. The dominant speaker still maintains their position
    whether or not he/she is speaking.
    
    This PR is built with the best possible parameters available to us by
    the clients and the BE. There can be flaws in bringing participants in
    view in a call of 8+ participants if everyone is speaking
    simultaneously. This is planned to be dealt with by the BE using the
    cascading feature. So we need to wait for them.
    
    ### Extra Side Quests
    - Increased readability by creating isVideoAvailable and
    isAudioAudioAvailable
    - Show Avatar when video is muted or video stream not available
    
    Co-authored-by: vanGalilea <galiliziv@gmail.com>

--- packages/react-native-sdk/src/components/VideoRenderer.tsx
+++ packages/react-native-sdk/src/components/VideoRenderer.tsx
@@ -71,8 +71,7 @@ export const VideoRenderer = (props: VideoRendererProps) => {
   } = props;
   return (
     <RTCView
-      // Since we stream the audio/video on mute/unmute, the `mediaStream` might not be available for a split second hence check for `mediaStream` is added
-      streamURL={mediaStream?.toURL()}
+      streamURL={mediaStream.toURL()}
       mirror={mirror}
       style={style}
       objectFit={objectFit}
