6248ffdb8cb83f489cfdb09a75bc14de850799f6
zwaardje <niels@seiton.io>
2023-01-30 21:10:51 +0100

    chore: configs
    Update configs to play nice with editor

--- sample-apps/react-video-demo/vite.config.ts
+++ sample-apps/react-video-demo/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: '.',
+  plugins: [
+    react(),
+    // tsconfigPaths({
+    //   root: '.',
+    // }),
+  ],
   resolve: {
     preserveSymlinks: true,
   },
