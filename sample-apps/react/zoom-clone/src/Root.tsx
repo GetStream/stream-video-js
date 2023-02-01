c25a272c92a9d82cc41571a2a4031d46ac6ae526
Anton Arnautov <arnautov.anton@gmail.com>
2023-02-01 14:35:32 +0100

    fix: load video theme variables

--- sample-apps/react/zoom-clone/src/Root.tsx
+++ sample-apps/react/zoom-clone/src/Root.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 
 export const Root = () => (
-  <div className="h-full w-full flex flex-col bg-zinc-50">
+  <div className="h-full w-full flex flex-col bg-zinc-50 str-video__theme-variables">
     <Header />
     <Outlet />
   </div>
