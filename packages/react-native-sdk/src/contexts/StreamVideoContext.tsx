1192d2d66d8647bf7f41ebd2945ba28e59a60916
khushal87 <khushal.agarwal987@gmail.com>
2023-02-03 17:16:19 +0530

    feat(react-native): add call lobby UI component in the RN SDK

--- packages/react-native-sdk/src/contexts/StreamVideoContext.tsx
+++ packages/react-native-sdk/src/contexts/StreamVideoContext.tsx
@@ -3,10 +3,12 @@ import createStoreContext from './createStoreContext';
 export interface SDKStreamVideoStore {
   cameraBackFacingMode: boolean;
   isVideoMuted: boolean;
+  isAudioMuted: boolean;
 }
 
 export const { Provider, useStoreValue, useStoreSetState } =
   createStoreContext<SDKStreamVideoStore>({
     cameraBackFacingMode: false,
     isVideoMuted: false,
+    isAudioMuted: false,
   });
