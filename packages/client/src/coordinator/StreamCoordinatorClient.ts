32fa09a1ff81b138cc9749e52b9d5cfcda7dad69
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-02-09 11:19:31 +0100

    chore: trigger build

--- packages/client/src/coordinator/StreamCoordinatorClient.ts
+++ packages/client/src/coordinator/StreamCoordinatorClient.ts
@@ -78,6 +78,7 @@ export class StreamCoordinatorClient {
     type: string,
     data: GetCallEdgeServerRequest,
   ) => {
+    // update
     return this.client.post<GetCallEdgeServerResponse>(
       `/call/{type}/{id}/get_edge_server`,
       data,
