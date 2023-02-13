408958573d4dedd8904c33991ea904b51aa787c7
zwaardje <niels@seiton.io>
2023-02-13 21:06:06 +0100

    feat: invite panel
    - copy url to clipboard
    - generate url from call id
    
    ** todo: still need a shorter URL

---
+++ sample-apps/react-video-demo/src/components/InvitePanel/InvitePanel.tsx
@@ -0,0 +1,77 @@
+import { FC, useCallback, useState } from 'react';
+import { QRCodeSVG } from 'qrcode.react';
+import classnames from 'classnames';
+
+import { Copy, UserChecked } from '../Icons';
+import Panel from '../Panel';
+
+import styles from './InvitePanel.module.css';
+import { root } from 'postcss';
+
+export type Props = {
+  className?: string;
+  callId: string;
+  isFocused?: boolean;
+};
+
+export const InvitePanel: FC<Props> = ({ className, isFocused, callId }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyUrl = useCallback(
+    (value: string) => {
+      console.log(value);
+      try {
+        navigator.clipboard.writeText(value).then(
+          function () {
+            setIsCopied(!isCopied);
+          },
+          function (err) {
+            console.error('Async: Could not copy text: ', err);
+          },
+        );
+      } catch (error) {}
+    },
+    [isCopied],
+  );
+
+  const rootClassname = classnames(styles.root, className);
+
+  return (
+    <Panel
+      className={rootClassname}
+      title="Invite more people"
+      isFocused={isFocused}
+    >
+      <>
+        <p className={styles.description}>
+          Send the URL below to someone and have them join this private call:
+        </p>
+        <div className={styles.copy}>
+          {isCopied ? (
+            <div className={styles.copied}>
+              <UserChecked className={styles.copiedIcon} />
+
+              <span className={styles.copied}>Link copied</span>
+              <Copy className={styles.copyIcon} />
+            </div>
+          ) : (
+            <input
+              className={styles.input}
+              value={callId}
+              onClick={(e) => copyUrl(e.currentTarget.value)}
+              readOnly={true}
+            />
+          )}
+          <Copy className={styles.copyIcon} />
+        </div>
+
+        <p className={styles.description}>
+          Or scan the QR code with your phone to test it yourself:
+        </p>
+        <div className={styles.qr}>
+          <QRCodeSVG className={styles.code} value={callId} />
+        </div>
+      </>
+    </Panel>
+  );
+};
---
+++ sample-apps/react-video-demo/src/components/InvitePanel/index.ts
@@ -0,0 +1,1 @@
+export { InvitePanel as default } from './InvitePanel';
