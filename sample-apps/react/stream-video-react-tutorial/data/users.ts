e3ac9404b3f547df69727918395b872884c9d250
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-02-14 14:54:39 +0100

    fix: integrate stream-video-client in TestContext

--- sample-apps/react/stream-video-react-tutorial/data/users.ts
+++ sample-apps/react/stream-video-react-tutorial/data/users.ts
@@ -4,15 +4,11 @@ export default {
   alice: {
     id: 'alice',
     name: 'Alice',
-    imageUrl: '/alice.jpg',
-    role: 'user',
-    teams: [],
+    image: 'https://randomuser.me/api/portraits/women/47.jpg',
   },
   mark: {
     id: 'mark',
     name: 'Mark',
-    imageUrl: '/mark.jpg',
-    role: 'user',
-    teams: [],
+    image: 'https://randomuser.me/api/portraits/men/38.jpg',
   },
 } as Record<string, User>;
