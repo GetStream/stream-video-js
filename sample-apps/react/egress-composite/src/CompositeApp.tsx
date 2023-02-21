a410de80162bb41aab09f4d7400e53727e76a240
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-02-21 16:09:16 +0100

    feat: New Egress Composite App (#249)
    - Rewritten from scratch, now it uses the React Video SDK underneath
    - Support for external configuration via query parameters (see the
    readme)
    - Support for different layouts (`dominant-speaker` and `grid`)
    - Video and ScreenShare support
    - DOM-element-based notification mechanism for the Recorder
      - for audio-only calls, we wait until we see the placeholder
    - for video calls, we wait until the video player is ready to play
    (content buffered, codecs initialized, etc...)
    - for screen-share, we wait until the screen-share feed is ready to be
    played (buffered, etc...)
    - for the Grid layout, we hook the above-mentioned rules to the very
    first participant on the screen
    - we have a 4s timeout that expects any of the rules above to be met. If
    breached, will kick-start the recording process regardless
    
    - We drop the Github Pages deployment in favor of a Vercel deployment.
    We already have our sample apps deployed there, it doesn't make sense to
    maintain two different deployment environments.

---
+++ sample-apps/react/egress-composite/src/CompositeApp.tsx
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import {
+  StreamClientOptions,
+  StreamVideo,
+  useCreateStreamVideoClient,
+  useHasOngoingScreenShare,
+} from '@stream-io/video-react-sdk';
+import Layouts, { DEFAULT_LAYOUT_ID, LayoutId } from './layouts';
+import { useAppConfig } from './hooks/useAppConfig';
+import { EgressReadyNotificationProvider } from './hooks/useNotifyEgress';
+import './CompositeApp.scss';
+
+export const CompositeApp = () => {
+  const config = useAppConfig();
+  const options: StreamClientOptions = {};
+  if (config.baseURL) {
+    options.baseURL = config.baseURL;
+  }
+  const client = useCreateStreamVideoClient({
+    apiKey: config.apiKey,
+    token: config.token,
+    options,
+    user: {
+      id: config.userId,
+    },
+  });
+
+  useEffect(() => {
+    if (!client) return;
+    const currentCall = client.joinCall(config.callId, config.callType);
+    return () => {
+      currentCall.then((call) => {
+        call?.leave();
+      });
+    };
+  }, [client, config.callId, config.callType]);
+
+  if (!client) {
+    return <h2>Connecting...</h2>;
+  }
+
+  return (
+    <StreamVideo client={client}>
+      <EgressReadyNotificationProvider>
+        <UiDispatcher layout={config.layout} />
+      </EgressReadyNotificationProvider>
+    </StreamVideo>
+  );
+};
+
+const UiDispatcher = (props: { layout: LayoutId }) => {
+  const { layout } = props;
+  const { ParticipantsView, ScreenShareView } =
+    Layouts[layout || DEFAULT_LAYOUT_ID];
+
+  const hasScreenShare = useHasOngoingScreenShare();
+  if (hasScreenShare) {
+    return <ScreenShareView />;
+  }
+
+  return <ParticipantsView />;
+};
---
+++ sample-apps/react/egress-composite/src/layouts/dominant-speaker/useSpotlightParticipant.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import {
+  SfuModels,
+  StreamVideoParticipant,
+  useDominantSpeaker,
+  useRemoteParticipants,
+} from '@stream-io/video-react-sdk';
+import { useAppConfig } from '../../hooks/useAppConfig';
+
+export const useSpotlightParticipant = () => {
+  const [speakerInSpotlight, setSpeakerInSpotlight] =
+    useState<StreamVideoParticipant | null>(null);
+
+  const { spotlightMode } = useAppConfig();
+  const dominantSpeaker = useDominantSpeaker();
+  const allParticipants = useRemoteParticipants();
+  useEffect(() => {
+    let shuffleId: NodeJS.Timeout;
+    if (spotlightMode === 'shuffle') {
+      shuffleId = setInterval(() => {
+        const randomParticipant =
+          allParticipants[Math.floor(Math.random() * allParticipants.length)];
+        setSpeakerInSpotlight(randomParticipant);
+      }, 3500);
+    } else {
+      const spotlightSpeaker =
+        dominantSpeaker ||
+        allParticipants.find((p) =>
+          p.publishedTracks.includes(SfuModels.TrackType.VIDEO),
+        ) ||
+        allParticipants[0];
+
+      setSpeakerInSpotlight(spotlightSpeaker);
+    }
+    return () => {
+      clearInterval(shuffleId);
+    };
+  }, [allParticipants, dominantSpeaker, spotlightMode]);
+
+  return speakerInSpotlight;
+};
