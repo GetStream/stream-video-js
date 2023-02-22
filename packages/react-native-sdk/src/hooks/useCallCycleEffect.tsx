8779bb22f48a2eb14fabcf83396230f10b4381fa
khushal87 <khushal.agarwal987@gmail.com>
2023-02-22 16:03:31 +0530

    refactor: remove StreamMeeting and StreamCall from RN SDK and restructuring the providers

---
+++ packages/react-native-sdk/src/hooks/useCallCycleEffect.tsx
@@ -0,0 +1,25 @@
+import {
+  useActiveCall,
+  useIncomingCalls,
+  useOutgoingCalls,
+} from '@stream-io/video-react-bindings';
+import { useEffect } from 'react';
+import { CallCycleHandlersType } from '../providers/CallCycle';
+
+export const useCallCycleEffect = (
+  callCycleHandlers: CallCycleHandlersType,
+) => {
+  const [outgoingCall] = useOutgoingCalls();
+  const [incomingCall] = useIncomingCalls();
+  const activeCall = useActiveCall();
+  const { onActiveCall, onHangupCall, onIncomingCall, onOutgoingCall } =
+    callCycleHandlers;
+
+  useEffect(() => {
+    if (incomingCall && onIncomingCall) onIncomingCall();
+    else if (outgoingCall && onOutgoingCall) onOutgoingCall();
+    else if (activeCall && onActiveCall) onActiveCall();
+    else if (onHangupCall) onHangupCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeCall, incomingCall, outgoingCall]);
+};
