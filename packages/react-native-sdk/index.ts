d929dd6686a0dbd6b463fe9197f1f31392ae2145
khushal87 <khushal.agarwal987@gmail.com>
2023-02-22 16:08:50 +0530

    refactor: add back StreamVideo comment

--- packages/react-native-sdk/index.ts
+++ packages/react-native-sdk/index.ts
@@ -2,7 +2,7 @@ export * from '@stream-io/video-react-bindings';
 export * from './src/components';
 export * from './src/contexts';
 export * from './src/hooks';
-export { StreamVideo } from './src/providers';
 
 // Overriding 'StreamVideo' from '@stream-io/video-react-bindings'
 // Explicitly re-exporting to resolve ambiguity.
+export { StreamVideo } from './src/providers';
