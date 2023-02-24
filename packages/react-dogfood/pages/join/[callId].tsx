3c244bd7687bb9a2968d53ac274bc43634d5725f
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-02-24 16:19:21 +0100

    fix: remove unused import

--- packages/react-dogfood/pages/join/[callId].tsx
+++ packages/react-dogfood/pages/join/[callId].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Gleap from 'gleap';
 import { useRouter } from 'next/router';
 import { authOptions } from '../api/auth/[...nextauth]';
