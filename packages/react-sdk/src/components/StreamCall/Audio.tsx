cf99efeb5e5e4c2116f8ceb7af8720cb34ab8bcd
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-03-02 14:03:20 +0100

    fix: decouple sinkId setting

--- packages/react-sdk/src/components/StreamCall/Audio.tsx
+++ packages/react-sdk/src/components/StreamCall/Audio.tsx
@@ -16,20 +16,26 @@ export type AudioProps = DetailedHTMLProps<
 
 export const Audio = ({ audioStream, sinkId, ...rest }: AudioProps) => {
   const audioRef = useRef<HTMLAudioElement>(null);
-
   useEffect(() => {
     const $el = audioRef.current;
     if (!($el && audioStream)) return;
 
     console.log(`Attaching audio stream`, $el, audioStream);
     $el.srcObject = audioStream;
-    if (($el as any).setSinkId) {
-      ($el as any).setSinkId(sinkId || '');
-    }
     return () => {
       $el.srcObject = null;
     };
-  }, [audioStream, sinkId]);
+  }, [audioStream]);
+
+  useEffect(() => {
+    const $el = audioRef.current;
+    if (!$el || !sinkId) return;
+
+    console.log(`Setting sinkId`, $el, sinkId);
+    if (($el as any).setSinkId) {
+      ($el as any).setSinkId(sinkId);
+    }
+  }, [sinkId]);
 
   return <audio autoPlay ref={audioRef} {...rest} />;
 };
