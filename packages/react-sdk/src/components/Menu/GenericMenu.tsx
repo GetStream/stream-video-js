6309f4cbc5b7e4346c67d40fc41e8a99715eaf46
Anton Arnautov <arnautov.anton@gmail.com>
2023-03-10 17:38:18 +0100

    Add GenericMenu reusable component

---
+++ packages/react-sdk/src/components/Menu/GenericMenu.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps, PropsWithChildren } from 'react';
+
+export const GenericMenu = ({ children }: PropsWithChildren) => {
+  return <ul className="str-video__generic-menu">{children}</ul>;
+};
+
+export const GenericMenuButtonItem = ({
+  children,
+  ...rest
+}: Omit<ComponentProps<'button'>, 'ref'>) => {
+  return (
+    <li className="str-video__generic-menu-item">
+      <button {...rest}>{children}</button>
+    </li>
+  );
+};
