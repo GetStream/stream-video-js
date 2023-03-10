0688bbf51c95123c51c0013858a772a1c74a26d8
vanGalilea <galiliziv@gmail.com>
2023-03-10 11:53:37 +0100

    Merge remote-tracking branch 'origin/docs/fundamentals-structure' into docs/fundamentals-structure

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -68,45 +68,13 @@ do
   )
 done
 
-#sed -i '' -e 's/interfaces/..\/Interfaces/g' 'temp-docs/modules.md'
-#sed -i '' -e 's/\.md/\//g' 'temp-docs/modules.md'
+cd ../../
+cp -a ./generated-docs/client/ "docusaurus/docs/$SDK_DIR_IN_DOCS/"
+rm -rf generated-docs/client/
 
-##copy from the temp-docs to the structure we want in docusaurus
-#mkdir generated-docs
-#mkdir "docusaurus/docs/$SDK_DIR_IN_DOCS/04-call-engine"
-#mkdir "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference"
-#touch "docusaurus/docs/$SDK_DIR_IN_DOCS/04-call-engine/_category_.json"
-#touch "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference/_category_.json"
-#echo "{
-#  \"label\": \"Call Engine\",
-#  \"position\": 4
-#}" > "docusaurus/docs/$SDK_DIR_IN_DOCS/04-call-engine/_category_.json"
-#echo "{
-#  \"label\": \"Reference\",
-#  \"position\": 7
-#}" > "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference/_category_.json"
-#cp -r temp-docs/interfaces generated-docs/Interfaces
-#cp temp-docs/modules.md generated-docs/components.md
-#rm -rf temp-docs
-#
-## move client docs to SDK's docs and mark as generated
-#cp -a ../client/docusaurus/docs/client/ generated-docs/client
-#cd generated-docs/client || exit
-#for sub_directories in * ;
-#do
-#  (
-#    cd "$sub_directories" || exit
-#    for f in * ; do mv -- "$f" "${f%.*}.gen.${f##*.}" ; done
-#  )
-#done
-#
-#cd ../../
-#cp -a ./generated-docs/client/ "docusaurus/docs/$SDK_DIR_IN_DOCS/"
-#rm -rf generated-docs/client/
-#
-## copy shared JS docs to the docs to SDK's docusaurus
-#cp -a ../client/generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/04-call-engine"
-#cp -a ../react-bindings/generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference"
-#cp -a ./generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference"
-#
-#echo "Done!"
+# copy shared JS docs to the docs to SDK's docusaurus
+cp -a ../client/generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/04-call-engine"
+cp -a ../react-bindings/generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference"
+cp -a ./generated-docs/. "docusaurus/docs/$SDK_DIR_IN_DOCS/07-reference"
+
+echo "Done!"
