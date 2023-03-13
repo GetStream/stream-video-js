424f109844429f1a74b904ea6441e04492821486
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-03-13 11:56:15 +0530

    fix: storing members of call in pendingcalls and usage in RN SDK (#294)
    The members were not stored in the `PendingCalls` when a new call was
    created. This PR aims to solve it. As a result, `outgoingCall.users` has
    the needed members who were called by the caller. The usage of avatar
    and name is also solved in the incoming and outgoing call view in RN
    SDK.

--- packages/react-native-sdk/src/utils/index.ts
+++ packages/react-native-sdk/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { CallMetadata, User, UserResponse } from '@stream-io/video-client';
 import { MAX_AVATARS_IN_VIEW } from '../constants';
 
 // Utility to join strings with commas and 'and'
@@ -20,4 +21,24 @@ export const generateParticipantTitle = (memberUserId: string) => {
   return memberUserId.length > 15
     ? memberUserId.slice(0, 15) + '...'
     : memberUserId;
+};
+
+// Utility to generate array of member user ids from outgoing call meta data
+export const getMembersForOutgoingCall = (outgoingCall: CallMetadata) => {
+  return Object.values(outgoingCall.users);
+};
+
+// Utility to generate array of member user ids from incoming call meta data
+export const getMembersForIncomingCall = (
+  incomingCall: CallMetadata,
+  connectedUser: User | undefined,
+) => {
+  let members: UserResponse[] = [];
+  Object.values(incomingCall.users).forEach((user) => {
+    if (connectedUser?.id !== user.id) members.push(user);
+  });
+  const callCreatedBy = incomingCall.call.created_by;
+  members.push(callCreatedBy);
+
+  return members;
 };
