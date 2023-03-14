49e7ea1772bdc547775e72c670711ddca3d0f412
vanGalilea <galiliziv@gmail.com>
2023-03-14 09:02:28 +0100

    fixes the meeting flow, ring still WIP

--- packages/react-native-sdk/src/providers/StreamVideo.tsx
+++ packages/react-native-sdk/src/providers/StreamVideo.tsx
@@ -1,14 +1,20 @@
 import {
+  StreamCallProvider,
   StreamVideo as StreamVideoProvider,
   StreamVideoProps,
+  useActiveCall,
 } from '@stream-io/video-react-bindings';
 import React, { PropsWithChildren } from 'react';
 import { Provider } from '../contexts/StreamVideoContext';
 import { MediaDevicesProvider } from '../contexts/MediaDevicesContext';
-import {
-  CallCycleProvider,
-  CallCycleHandlersType,
-} from '../contexts/CallCycleContext';
+import { CallCycleHandlersType, CallCycleProvider } from '../contexts';
+
+const StreamCallWrapper = ({ children }: PropsWithChildren) => {
+  const activeCall = useActiveCall();
+  if (!activeCall) return <>{children}</>;
+
+  return <StreamCallProvider call={activeCall}>{children}</StreamCallProvider>;
+};
 
 export const StreamVideo = (
   props: PropsWithChildren<
@@ -19,11 +25,13 @@ export const StreamVideo = (
 
   return (
     <StreamVideoProvider client={client}>
-      <CallCycleProvider callCycleHandlers={callCycleHandlers}>
-        <MediaDevicesProvider>
-          <Provider>{children}</Provider>
-        </MediaDevicesProvider>
-      </CallCycleProvider>
+      <StreamCallWrapper>
+        <CallCycleProvider callCycleHandlers={callCycleHandlers}>
+          <MediaDevicesProvider>
+            <Provider>{children}</Provider>
+          </MediaDevicesProvider>
+        </CallCycleProvider>
+      </StreamCallWrapper>
     </StreamVideoProvider>
   );
 };
