36d3431f579675ef7e6b658d1ff4edf79a986b03
vanGalilea <galiliziv@gmail.com>
2023-03-24 17:39:17 +0100

    assistanceModal

--- sample-apps/react-native/cookbook/src/screens/CustomParticipantsLayoutScreen.tsx
+++ sample-apps/react-native/cookbook/src/screens/CustomParticipantsLayoutScreen.tsx
@@ -1,5 +1,12 @@
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import {
+  ActivityIndicator,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   ActiveCall,
   StreamVideo,
@@ -16,6 +23,9 @@ import {useNavigation} from '@react-navigation/core';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {theme} from '@stream-io/video-react-native-sdk/dist/src/theme';
 import InCallManager from 'react-native-incall-manager';
+// @ts-ignore
+import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser';
+import {customAlphabet} from 'nanoid';
 
 const USER = {
   id: STREAM_USER_ID,
@@ -26,6 +36,10 @@ const USER = {
   },
 };
 console.log('User loaded: ', USER);
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 5);
+
+const generateCallId = () => nanoid(5);
 
 export default () => {
   const navigation = useNavigation();
@@ -42,7 +56,9 @@ export default () => {
         onActiveCall: () => null,
         onHangupCall: () => navigation.navigate('WelcomeScreen'),
       }}>
-      <Inner />
+      <>
+        <Inner />
+      </>
     </StreamVideo>
   );
 };
@@ -51,12 +67,13 @@ const Inner = () => {
   const activeCall = useActiveCall();
   const videoClient = useStreamVideoClient();
   const insets = useSafeAreaInsets();
-  const CALL_ID = 'test-call-id';
+  const callId = useRef<string>(generateCallId());
+  const [isIntroModalVisible, setIsIntroModalVisible] = useState(true);
   const onOpenCallParticipantsInfoViewHandler = () => null;
 
   useEffect(() => {
     videoClient
-      ?.joinCall(CALL_ID, 'default')
+      ?.joinCall(callId.current, 'default')
       .then(() => {
         InCallManager.start({media: 'video'});
         InCallManager.setForceSpeakerphoneOn(true);
@@ -64,7 +81,7 @@ const Inner = () => {
       .catch(err => {
         console.log('Error joining call', err);
       });
-  }, []);
+  }, [callId.current]);
 
   if (!activeCall) {
     return <ActivityIndicator size={'large'} style={StyleSheet.absoluteFill} />;
@@ -72,6 +89,60 @@ const Inner = () => {
 
   return (
     <View style={[styles.wrapper, {paddingTop: insets.top}]}>
+      <Modal
+        presentationStyle={'overFullScreen'}
+        visible={isIntroModalVisible}
+        animationType="slide"
+        transparent
+        onRequestClose={() => {
+          setIsIntroModalVisible(false);
+        }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: 36,
+              marginTop: 50,
+              marginVertical: 36,
+              backgroundColor: 'rgba(255,0,0,0.2)',
+            }}>
+            <Text>
+              The call has just started, to check the full implementation join
+              the call from another device
+            </Text>
+            <Text>Call ID: {callId.current}</Text>
+            <Pressable
+              style={{}}
+              onPress={() =>
+                openURLInBrowser(
+                  `https://stream-calls-dogfood.vercel.app/join/${callId.current}`,
+                )
+              }>
+              <Text>
+                Click here to join the call with more participants via a web
+                browser
+              </Text>
+            </Pressable>
+            <Pressable
+              style={{}}
+              onPress={() =>
+                openURLInBrowser(
+                  'https://github.com/GetStream/stream-video-buddy',
+                )
+              }>
+              <Text>Click here to join via Stream Video Buddy CLI</Text>
+            </Pressable>
+            <Pressable onPress={() => setIsIntroModalVisible(false)}>
+              <Text>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <ActiveCall
         onOpenCallParticipantsInfoView={onOpenCallParticipantsInfoViewHandler}
       />
