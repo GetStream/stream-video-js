04a945ee56b708359fed3b011b75b158d8aaf887
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-03-27 09:47:13 +0200

    fix: navigator useragent undefined in react native

--- packages/client/src/helpers/browsers.ts
+++ packages/client/src/helpers/browsers.ts
@@ -2,22 +2,19 @@
  * Checks whether the current browser is Safari.
  */
 export const isSafari = () => {
-  if (typeof navigator === 'undefined') return false;
-  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent || '');
 };
 
 /**
  * Checks whether the current browser is Firefox.
  */
 export const isFirefox = () => {
-  if (typeof navigator === 'undefined') return false;
-  return navigator.userAgent.includes('Firefox');
+  return navigator.userAgent?.includes('Firefox');
 };
 
 /**
  * Checks whether the current browser is Google Chrome.
  */
 export const isChrome = () => {
-  if (typeof navigator === 'undefined') return false;
-  return navigator.userAgent.includes('Chrome');
+  return navigator.userAgent?.includes('Chrome');
 };
