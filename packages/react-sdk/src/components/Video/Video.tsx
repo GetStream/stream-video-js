3e5426e8948ee37164a8dc529b6c15a46c32e3ac
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-03-31 10:52:46 +0200

    fix: take sessionId in consideration

--- packages/react-sdk/src/components/Video/Video.tsx
+++ packages/react-sdk/src/components/Video/Video.tsx
@@ -5,8 +5,6 @@ import {
   useRef,
   useState,
   VideoHTMLAttributes,
-  createElement,
-  ElementType,
 } from 'react';
 import {
   Call,
@@ -45,6 +43,7 @@ export const Video = (
       : SfuModels.TrackType.SCREEN_SHARE,
   );
 
+  const lastSessionId = useRef<string>(sessionId);
   const lastDimensionRef = useRef<SfuModels.VideoDimension | undefined>();
   const updateSubscription = useCallback(() => {
     let nextDimension;
@@ -61,6 +60,7 @@ export const Video = (
 
     const lastDimension = lastDimensionRef.current;
     if (
+      sessionId !== lastSessionId.current ||
       nextDimension?.width !== lastDimension?.width ||
       nextDimension?.height !== lastDimension?.height
     ) {
@@ -70,6 +70,7 @@ export const Video = (
         },
       });
       lastDimensionRef.current = nextDimension;
+      lastSessionId.current = sessionId;
     }
   }, [
     call,
