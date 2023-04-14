GetStream/stream-video-js

commit 8bcdd7228e622b6dca1dfef700fb121c48a30256

feat: Call permissions and capabilities (#383)
Implements the missing functionalities around call permissions and user
capabilities.

#### Added:
- New APIs and React Hooks which allow easy access to user's permissions
and capabilities
- New UI components for requesting and approving permissions

#### Changed:
- Own capabilities shall be updated in and read from
`call.state.metadata`
- Events: a few of the default event handlers are now attached to the
appropriate call instance. This allows cleaner code with fewer side
effects in their implementation. The remaining event handlers will be
tackled in a follow-up PR.
- Internal: The coordinator client also accepts generic type parameters
for request data.
- OpenAPI code-gen: Enums are now generated in UPPER_CASE notation (SFU
enum notation alignment). The schema validation is enabled again.

packages/react-dogfood/components/Stage.tsx
@@ -9,23 +9,16 @@ export const Stage = ({
 }) => {
   const call = useCall();
   const searchParams = useSearchParams();
-
   const groupSize = +(searchParams.get('group_size') ?? '0');
 
   const SelectedComponent = LayoutMap[selectedLayout].Component;
 
-  if (selectedLayout === 'LegacyGrid' || selectedLayout === 'LegacySpeaker') {
-    return (
-      <div className="str-video__stage">
-        <SelectedComponent call={call!} />
-      </div>
-    );
-  }
-
   return (
-    // @ts-expect-error
-    <SelectedComponent
-      groupSize={!groupSize || groupSize > 16 ? undefined : groupSize}
-    />
+    <div className="str-video__stage">
+      <SelectedComponent
+        call={call!}
+        groupSize={!groupSize || groupSize > 16 ? undefined : groupSize}
+      />
+    </div>
   );
 };

packages/client/generate-openapi.sh
@@ -5,12 +5,12 @@ FROM_REPO=$1;
 
 if  [ "$FROM_REPO" == 'chat' ]; then
   PROTOCOL_REPO_DIR="../../../chat"
-else 
+else
   PROTOCOL_REPO_DIR="../../../protocol"
 fi
 if  [ "$FROM_REPO" == 'chat' ]; then
   SCHEMA_FILE="$PROTOCOL_REPO_DIR/releases/video-openapi.yaml"
-else 
+else
   SCHEMA_FILE="$PROTOCOL_REPO_DIR/openapi/video-openapi.yaml"
 fi
 
@@ -32,10 +32,9 @@ yarn openapi-generator-cli generate \
   -i "$SCHEMA_FILE" \
   -g typescript-fetch \
   -o "$TEMP_OUTPUT_DIR" \
-  --skip-validate-spec \
   --additional-properties=supportsES6=true \
   --additional-properties=modelPropertyNaming=original \
-  --additional-properties=enumPropertyNaming=original \
+  --additional-properties=enumPropertyNaming=UPPERCASE \
   --additional-properties=withoutRuntimeChecks=true
 
 # Remove the generated API client, just keep the models

packages/client/src/StreamVideoClient.ts
@@ -17,17 +17,9 @@ import {
 import { Call } from './rtc/Call';
 
 import {
-  watchBlockedUser,
   watchCallAccepted,
   watchCallCancelled,
-  watchCallCreated,
-  watchCallPermissionRequest,
-  watchCallPermissionsUpdated,
-  watchCallRecordingStarted,
-  watchCallRecordingStopped,
   watchCallRejected,
-  watchNewReactions,
-  watchUnblockedUser,
 } from './events';
 
 import { CALL_CONFIG, CallConfig } from './config';
@@ -35,6 +27,7 @@ import {
   EventHandler,
   EventTypes,
   StreamClientOptions,
+  StreamVideoEvent,
   TokenOrProvider,
   User,
 } from './coordinator/connection/types';
@@ -54,7 +47,7 @@ export class StreamVideoClient {
    */
   readonly readOnlyStateStore: StreamVideoReadOnlyStateStore;
   private readonly writeableStateStore: StreamVideoWriteableStateStore;
-  public streamClient: StreamClient;
+  streamClient: StreamClient;
 
   /**
    * You should create only one instance of `StreamVideoClient`.
@@ -97,40 +90,32 @@ export class StreamVideoClient {
       tokenOrProvider,
     );
 
-    this.on(
-      'call.created',
-      watchCallCreated(this.writeableStateStore, this.streamClient),
-    );
+    // FIXME: OL: unregister the event listeners.
+    this.on('call.created', (event: StreamVideoEvent) => {
+      if (event.type !== 'call.created') return;
+      const { call, members, ringing } = event;
+
+      if (user.id === call.created_by.id) {
+        console.warn('Received CallCreatedEvent sent by the current user');
+        return;
+      }
+
+      this.writeableStateStore.setPendingCalls((pendingCalls) => [
+        ...pendingCalls,
+        new Call({
+          streamClient: this.streamClient,
+          type: call.type,
+          id: call.id,
+          metadata: call,
+          members,
+          ringing,
+          clientStore: this.writeableStateStore,
+        }),
+      ]);
+    });
     this.on('call.accepted', watchCallAccepted(this.writeableStateStore));
     this.on('call.rejected', watchCallRejected(this.writeableStateStore));
     this.on('call.ended', watchCallCancelled(this.writeableStateStore));
-    this.on(
-      'call.permission_request',
-      watchCallPermissionRequest(this.writeableStateStore),
-    );
-
-    this.on(
-      'call.permissions_updated',
-      watchCallPermissionsUpdated(this.writeableStateStore),
-    );
-
-    this.on('call.blocked_user', watchBlockedUser(this.writeableStateStore));
-    this.on(
-      'call.unblocked_user',
-      watchUnblockedUser(this.writeableStateStore),
-    );
-
-    this.on(
-      'call.recording_started',
-      watchCallRecordingStarted(this.writeableStateStore),
-    );
-
-    this.on(
-      'call.recording_stopped',
-      watchCallRecordingStopped(this.writeableStateStore),
-    );
-
-    this.on('call.reaction_new', watchNewReactions(this.writeableStateStore));
 
     this.writeableStateStore.setConnectedUser(user);
   };

packages/client/src/coordinator/connection/client.ts
@@ -485,10 +485,10 @@ export class StreamClient {
     });
   }
 
-  doAxiosRequest = async <T>(
+  doAxiosRequest = async <T, D = unknown>(
     type: string,
     url: string,
-    data?: unknown,
+    data?: D,
     options: AxiosRequestConfig & {
       config?: AxiosRequestConfig & { maxBodyLength?: number };
     } = {},
@@ -538,7 +538,7 @@ export class StreamClient {
             await sleep(retryInterval(this.consecutiveFailures));
           }
           await this.tokenManager.loadToken();
-          return await this.doAxiosRequest<T>(type, url, data, options);
+          return await this.doAxiosRequest<T, D>(type, url, data, options);
         }
         return this.handleResponse(e.response);
       } else {
@@ -549,25 +549,25 @@ export class StreamClient {
   };
 
   get<T>(url: string, params?: AxiosRequestConfig['params']) {
-    return this.doAxiosRequest<T>('get', url, null, {
+    return this.doAxiosRequest<T, unknown>('get', url, null, {
       params,
     });
   }
 
-  put<T>(url: string, data?: unknown) {
-    return this.doAxiosRequest<T>('put', url, data);
+  put<T, D = unknown>(url: string, data?: D) {
+    return this.doAxiosRequest<T, D>('put', url, data);
   }
 
-  post<T>(url: string, data?: unknown) {
-    return this.doAxiosRequest<T>('post', url, data);
+  post<T, D = unknown>(url: string, data?: D) {
+    return this.doAxiosRequest<T, D>('post', url, data);
   }
 
-  patch<T>(url: string, data?: unknown) {
-    return this.doAxiosRequest<T>('patch', url, data);
+  patch<T, D = unknown>(url: string, data?: D) {
+    return this.doAxiosRequest<T, D>('patch', url, data);
   }
 
   delete<T>(url: string, params?: AxiosRequestConfig['params']) {
-    return this.doAxiosRequest<T>('delete', url, null, {
+    return this.doAxiosRequest<T, unknown>('delete', url, null, {
       params,
     });
   }

packages/client/src/coordinator/connection/types.ts
@@ -117,26 +117,3 @@ export type StreamClientOptions = Partial<AxiosRequestConfig> & {
 
 export type TokenProvider = () => Promise<string>;
 export type TokenOrProvider = null | string | TokenProvider | undefined;
-
-// FIXME: have this coming from OpenAPI schema
-const OwnCapabilitiesEnum = [
-  'join-call',
-  'stop-record-call',
-  'end-call',
-  'mute-users',
-  'start-broadcast-call',
-  'block-users',
-  'read-call',
-  'join-ended-call',
-  'screenshare',
-  'send-video',
-  'send-audio',
-  'start-record-call',
-  'update-call-permissions',
-  'create-call',
-  'update-call',
-  'update-call-settings',
-  'stop-broadcast-call',
-] as const;
-
-export type OwnCapabilities = Array<(typeof OwnCapabilitiesEnum)[number]>;

packages/client/src/events/call-permissions.ts
@@ -1,85 +1,29 @@
 import { StreamVideoEvent } from '../coordinator/connection/types';
-import { StreamVideoWriteableStateStore } from '../store';
+import { CallState } from '../store';
 
 /**
  * Event handler that watches for `call.permission_request` events
  * Updates the state store using the `callPermissionRequest$` stream
  */
-export const watchCallPermissionRequest = (
-  store: StreamVideoWriteableStateStore,
-) => {
+export const watchCallPermissionRequest = (state: CallState) => {
   return function onCallPermissionRequest(event: StreamVideoEvent) {
-    if (event.type !== 'call.permission_request') {
-      return;
-    }
-    const activeCall = store.activeCall;
-    if (!activeCall) {
-      console.warn(
-        `Ignoring "call.permission_request" as there is no active call`,
-        event,
-      );
-      return;
-    }
-
-    if (activeCall.cid !== event.call_cid) {
-      console.warn(
-        `Ignoring "call.permission_request" as it doesn't belong to the active call`,
-        event,
-      );
-      return;
-    }
-
-    const state = activeCall.state;
-    const localParticipant = state.localParticipant;
-    if (
-      !localParticipant?.ownCapabilities.includes('update-call-permissions')
-    ) {
-      console.warn(
-        `Ignoring "call.permission_request" as the user doesn't have permission to handle it`,
-      );
-      return;
-    }
-
+    if (event.type !== 'call.permission_request') return;
     state.setCallPermissionRequest(event);
   };
 };
 
 /**
  * Event handler that watches for `call.permissions_updated` events
- * It will update the `localParticipant$` or `remoteParticipants$` based on whose permissions were changed.
  */
-export const watchCallPermissionsUpdated = (
-  store: StreamVideoWriteableStateStore,
-) => {
+export const watchCallPermissionsUpdated = (state: CallState) => {
   return function onCallPermissionsUpdated(event: StreamVideoEvent) {
-    if (event.type !== 'call.permissions_updated') {
-      return;
-    }
-    const activeCall = store.activeCall;
-    if (!activeCall) {
-      console.warn(
-        `Ignoring "call.permissions_updated" as there is no active call`,
-        event,
-      );
-      return;
-    }
-
-    if (activeCall.cid !== event.call_cid) {
-      console.warn(
-        `Ignoring "call.permissions_updated" as it doesn't belong to the active call`,
-        event,
-      );
-      return;
-    }
-
-    const state = activeCall.state;
-    const localParticipant = state.localParticipant;
+    if (event.type !== 'call.permissions_updated') return;
+    const { localParticipant } = state;
     if (event.user.id === localParticipant?.userId) {
-      state.updateParticipant(localParticipant.sessionId, {
-        ownCapabilities: event.own_capabilities,
-      });
+      state.setMetadata((metadata) => ({
+        ...metadata!,
+        own_capabilities: event.own_capabilities,
+      }));
     }
-
-    // TODO: update remote participant once SFU includes that info in the participant data model
   };
 };

packages/client/src/events/call.ts
@@ -1,47 +1,5 @@
-import { StreamVideoWriteableStateStore } from '../store';
-import { Call } from '../rtc/Call';
-import { StreamClient } from '../coordinator/connection/client';
+import { CallState, StreamVideoWriteableStateStore } from '../store';
 import { StreamVideoEvent } from '../coordinator/connection/types';
-
-/**
- * Event handler that watches the delivery of CallCreated Websocket event
- * Updates the state store and notifies its subscribers that
- * a new pending call has been initiated.
- */
-export const watchCallCreated = (
-  store: StreamVideoWriteableStateStore,
-  streamClient: StreamClient,
-) => {
-  return function onCallCreated(event: StreamVideoEvent) {
-    if (event.type !== 'call.created') {
-      return;
-    }
-    const { call, members, ringing } = event;
-    if (!call) {
-      console.warn("Can't find call in CallCreatedEvent");
-      return;
-    }
-
-    const currentUser = store.connectedUser;
-    if (currentUser?.id === call.created_by.id) {
-      console.warn('Received CallCreatedEvent sent by the current user');
-      return;
-    }
-
-    store.setPendingCalls((pendingCalls) => [
-      ...pendingCalls,
-      new Call({
-        streamClient,
-        type: call.type,
-        id: call.id,
-        metadata: call,
-        members,
-        ringing,
-        clientStore: store,
-      }),
-    ]);
-  };
-};
 
 /**
  * Event handler that watched the delivery of CallAcceptedEvent
@@ -50,15 +8,8 @@ export const watchCallCreated = (
  */
 export const watchCallAccepted = (store: StreamVideoWriteableStateStore) => {
   return function onCallAccepted(event: StreamVideoEvent) {
-    if (event.type !== 'call.accepted') {
-      return;
-    }
+    if (event.type !== 'call.accepted') return;
     const { call_cid } = event;
-    if (!call_cid) {
-      console.warn("Can't find call_cid in CallAcceptedEvent");
-      return;
-    }
-
     const acceptedIncomingCall = store.incomingCalls.find(
       (incomingCall) => incomingCall.cid === call_cid,
     );
@@ -104,15 +55,8 @@ export const watchCallAccepted = (store: StreamVideoWriteableStateStore) => {
  */
 export const watchCallRejected = (store: StreamVideoWriteableStateStore) => {
   return function onCallRejected(event: StreamVideoEvent) {
-    if (event.type !== 'call.rejected') {
-      return;
-    }
+    if (event.type !== 'call.rejected') return;
     const { call_cid } = event;
-    if (!call_cid) {
-      console.warn("Can't find call_cid in CallRejectedEvent");
-      return;
-    }
-
     const rejectedIncomingCall = store.incomingCalls.find(
       (incomingCall) => incomingCall.cid === call_cid,
     );
@@ -152,15 +96,8 @@ export const watchCallRejected = (store: StreamVideoWriteableStateStore) => {
  */
 export const watchCallCancelled = (store: StreamVideoWriteableStateStore) => {
   return function onCallCancelled(event: StreamVideoEvent) {
-    if (event.type !== 'call.ended') {
-      return;
-    }
+    if (event.type !== 'call.ended') return;
     const { call_cid } = event;
-    if (!call_cid) {
-      console.log("Can't find call in CallEndedEvent");
-      return;
-    }
-
     const cancelledIncomingCall = store.incomingCalls.find(
       (incomingCall) => incomingCall.cid === call_cid,
     );
@@ -181,5 +118,16 @@ export const watchCallCancelled = (store: StreamVideoWriteableStateStore) => {
     store.setPendingCalls((pendingCalls) =>
       pendingCalls.filter((pendingCall) => pendingCall.cid !== call_cid),
     );
+  };
+};
+
+/**
+ * An event handler which listens to `call.updated` events
+ * and updates the given call state accordingly.
+ */
+export const watchCallUpdated = (state: CallState) => {
+  return function onCallUpdated(event: StreamVideoEvent) {
+    if (event.type !== 'call.updated') return;
+    state.setMetadata(event.call);
   };
 };

packages/client/src/events/moderation.ts
@@ -1,5 +1,5 @@
 import { StreamVideoEvent } from '../coordinator/connection/types';
-import { StreamVideoWriteableStateStore } from '../store';
+import { CallState } from '../store';
 
 /**
  * Event handler that watches for `call.blocked_user` events,
@@ -7,25 +7,8 @@ import { StreamVideoWriteableStateStore } from '../store';
  * `event.user_id` to the list
  */
 export const watchBlockedUser =
-  (store: StreamVideoWriteableStateStore) => (event: StreamVideoEvent) => {
-    if (event.type !== 'call.blocked_user') {
-      return;
-    }
-    const activeCall = store.activeCall;
-    if (!activeCall || activeCall.cid !== event.call_cid) {
-      console.warn(
-        `Received "call.blocked_user" for an inactive or unknown call`,
-        event,
-      );
-      return;
-    }
-
-    const state = activeCall.state;
-    // FIXME: end call
-    if (state.localParticipant?.userId === event.user.id) {
-      activeCall.leave();
-    }
-
+  (state: CallState) => (event: StreamVideoEvent) => {
+    if (event.type !== 'call.blocked_user') return;
     state.setMetadata((metadata) => ({
       ...metadata!,
       blocked_user_ids: [...metadata!.blocked_user_ids, event.user.id],
@@ -38,20 +21,8 @@ export const watchBlockedUser =
  * removing `event.user_id` from the list
  */
 export const watchUnblockedUser =
-  (store: StreamVideoWriteableStateStore) => (event: StreamVideoEvent) => {
-    if (event.type !== 'call.unblocked_user') {
-      return;
-    }
-    const activeCall = store.activeCall;
-    if (!activeCall || activeCall.cid !== event.call_cid) {
-      console.warn(
-        `Received "call.unblocked_user" for an inactive or unknown call`,
-        event,
-      );
-      return;
-    }
-
-    const state = activeCall.state;
+  (state: CallState) => (event: StreamVideoEvent) => {
+    if (event.type !== 'call.unblocked_user') return;
     state.setMetadata((metadata) => {
       const blocked_user_ids = metadata!.blocked_user_ids.filter(
         (userId) => event.user.id !== userId,

commit 4b903ca09f0ff5ba3a7d655703fd61d9712740db

fix: revert previous "stage" implementation (#390)
Fixes a layout issue with Speaker view introduced with #383

packages/react-dogfood/components/Stage.tsx
@@ -13,12 +13,18 @@ export const Stage = ({
 
   const SelectedComponent = LayoutMap[selectedLayout].Component;
 
+  if (selectedLayout === 'LegacyGrid' || selectedLayout === 'LegacySpeaker') {
+    return (
+      <div className="str-video__stage">
+        <SelectedComponent call={call!} />
+      </div>
+    );
+  }
+
   return (
-    <div className="str-video__stage">
-      <SelectedComponent
-        call={call!}
-        groupSize={!groupSize || groupSize > 16 ? undefined : groupSize}
-      />
-    </div>
+    // @ts-expect-error
+    <SelectedComponent
+      groupSize={!groupSize || groupSize > 16 ? undefined : groupSize}
+    />
   );
 };