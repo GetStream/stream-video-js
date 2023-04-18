fc02d252479662c2acb4d4510aad301892f96250
Stefan <stefan.blos@gmail.com>
2023-04-18 08:51:56 +0200

    Add icons to use in separate file

--- sample-apps/react/audio-rooms/src/components/home/RoomActive.tsx
+++ sample-apps/react/audio-rooms/src/components/home/RoomActive.tsx
@@ -3,8 +3,18 @@ import { Audio } from '@stream-io/video-react-sdk';
 import { useEffect } from 'react';
 import { useAudioRoomContext } from '../../contexts/AudioRoomContext/AudioRoomContext';
 import RoomOverview from './RoomOverview';
+import {
+  AddPersonIcon,
+  ChatIcon,
+  LeaveIcon,
+  PersonIcon,
+  RaiseHandIcon,
+  StarIcon,
+} from '../icons';
+import { useUserContext } from '../../contexts/UserContext/UserContext';
 
 const RoomActive = () => {
+  const { user } = useUserContext();
   const { currentRoom, leave } = useAudioRoomContext();
   const participants = useParticipants();
 
@@ -19,6 +29,10 @@ const RoomActive = () => {
       }, {}),
     );
   }, []);
+
+  // helper variables
+  const speakers = participants.map((p) => p.isSpeaking);
+  const isUserHost = currentRoom?.hosts.some((e) => e.id === user?.id);
 
   return (
     <section className="active-room">
@@ -39,31 +53,9 @@ const RoomActive = () => {
           </p>
         ))}
         <p className="user-counts secondaryText">
-          {(currentRoom?.speakers.length ?? 0) +
-            (currentRoom?.listeners.length ?? 0)}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-              clipRule="evenodd"
-            />
-          </svg>
-          / {currentRoom?.speakers.length ?? 0}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12 2.25c-2.429 0-4.817.178-7.152.521C2.87 3.061 1.5 4.795 1.5 6.741v6.018c0 1.946 1.37 3.68 3.348 3.97.877.129 1.761.234 2.652.316V21a.75.75 0 001.28.53l4.184-4.183a.39.39 0 01.266-.112c2.006-.05 3.982-.22 5.922-.506 1.978-.29 3.348-2.023 3.348-3.97V6.741c0-1.947-1.37-3.68-3.348-3.97A49.145 49.145 0 0012 2.25zM8.25 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zm2.625 1.125a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
-              clipRule="evenodd"
-            />
-          </svg>
+          {participants.length}
+          <PersonIcon />/ {speakers.length ?? 0}
+          <ChatIcon />
         </p>
         <h3>Hosts ({currentRoom?.hosts.length ?? 0})</h3>
         <div className="hosts-grid-detail">
@@ -71,20 +63,7 @@ const RoomActive = () => {
             <div key={host.id}>
               <img src={host.imageUrl} alt={`Profile of ${host.name}`} />
               <p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                  />
-                </svg>
+                <StarIcon />
                 <span>{host.name}</span>
               </p>
             </div>
@@ -102,39 +81,16 @@ const RoomActive = () => {
             </div>
           ))}
           <div className="button-list">
-            <button className="leave-button" onClick={() => leave()}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Leave Quietly
+            <button className="leave-button" onClick={() => endOrLeaveCall()}>
+              <LeaveIcon />
+              {isUserHost ? 'End room' : 'Leave Quietly'}
             </button>
             <div>
               <button className="icon-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z" />
-                </svg>
+                <AddPersonIcon />
               </button>
               <button className="icon-button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M10.5 1.875a1.125 1.125 0 012.25 0v8.219c.517.162 1.02.382 1.5.659V3.375a1.125 1.125 0 012.25 0v10.937a4.505 4.505 0 00-3.25 2.373 8.963 8.963 0 014-.935A.75.75 0 0018 15v-2.266a3.368 3.368 0 01.988-2.37 1.125 1.125 0 011.591 1.59 1.118 1.118 0 00-.329.79v3.006h-.005a6 6 0 01-1.752 4.007l-1.736 1.736a6 6 0 01-4.242 1.757H10.5a7.5 7.5 0 01-7.5-7.5V6.375a1.125 1.125 0 012.25 0v5.519c.46-.452.965-.832 1.5-1.141V3.375a1.125 1.125 0 012.25 0v6.526c.495-.1.997-.151 1.5-.151V1.875z" />
-                </svg>
+                <RaiseHandIcon />
               </button>
             </div>
           </div>
@@ -142,6 +98,13 @@ const RoomActive = () => {
       </div>
     </section>
   );
+
+  function endOrLeaveCall() {
+    if (isUserHost) {
+      currentRoom?.call?.endCall();
+    }
+    leave();
+  }
 };
 
 export default RoomActive;
