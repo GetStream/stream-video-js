218a4d7f6f6c859899fb037af85ce5f014257c0f
Stefan <stefan.blos@gmail.com>
2023-04-19 12:00:51 +0200

    Only join a room when it is live

--- sample-apps/react/audio-rooms/src/components/home/RoomActive/RoomActiveContainer.tsx
+++ sample-apps/react/audio-rooms/src/components/home/RoomActive/RoomActiveContainer.tsx
@@ -2,27 +2,30 @@ import { StreamCallProvider } from '@stream-io/video-react-bindings';
 import { useAudioRoomContext } from '../../../contexts/AudioRoomContext/AudioRoomContext';
 import { useCallback, useEffect, useState } from 'react';
 import RoomActive from '../RoomActive';
-import { Call, CallingState, getAudioStream } from '@stream-io/video-client';
-import { useUserContext } from '../../../contexts/UserContext/UserContext';
+import { Call, CallingState } from '@stream-io/video-client';
+import { MediaDevicesProvider } from '@stream-io/video-react-sdk';
 
 function RoomActiveContainer() {
-  const { user } = useUserContext();
   const { currentRoom } = useAudioRoomContext();
   const [call, setCall] = useState<Call | undefined>(undefined);
 
   const getCall = useCallback(async (callToJoin: Call) => {
-    console.log('getCall');
-    const callingState = call?.state.callingState;
+    const callingState = callToJoin.state.callingState;
+    console.log(`getCall: current callingState: ${callingState}`);
+    // We only want to join if the call is not in backstage, but live
+    const isBackstage = callToJoin.state.metadata?.backstage;
     if (
       callingState !== CallingState.JOINED &&
-      callingState !== CallingState.JOINING
+      callingState !== CallingState.JOINING &&
+      !isBackstage
     ) {
       await callToJoin.join();
-      const audioStream = await getAudioStream();
-      await callToJoin.publishAudioStream(audioStream);
+      // const audioStream = await getAudioStream();
+      // await callToJoin.publishAudioStream(audioStream);
     }
 
-    // await callToJoin.goLive();
+    // await callToJoin.endCall();
+    // await callToJoin.muteAllUsers('audio');
     setCall(callToJoin);
   }, []);
 
@@ -39,9 +42,14 @@ function RoomActiveContainer() {
   return (
     <>
       {call && (
-        <StreamCallProvider call={call}>
-          <RoomActive />
-        </StreamCallProvider>
+        <MediaDevicesProvider
+          initialAudioEnabled={false}
+          initialVideoEnabled={false}
+        >
+          <StreamCallProvider call={call}>
+            <RoomActive />
+          </StreamCallProvider>
+        </MediaDevicesProvider>
       )}
       {!call && <div className="active-room">Loading</div>}
     </>
