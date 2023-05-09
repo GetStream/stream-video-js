f6fe30abcf60c83d5bd97c9e393bb640b4b65935
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-05-09 18:49:55 +0200

    feat: call broadcasting events and apis

--- packages/client/src/events/recording.ts
+++ packages/client/src/events/recording.ts
@@ -7,7 +7,10 @@ import { StreamVideoEvent } from '../coordinator/connection/types';
 export const watchCallRecordingStarted = (state: CallState) => {
   return function onCallRecordingStarted(event: StreamVideoEvent) {
     if (event.type !== 'call.recording_started') return;
-    state.setCallRecordingInProgress(true);
+    state.setMetadata((metadata) => ({
+      ...metadata!,
+      recording: true,
+    }));
   };
 };
 
@@ -17,6 +20,33 @@ export const watchCallRecordingStarted = (state: CallState) => {
 export const watchCallRecordingStopped = (state: CallState) => {
   return function onCallRecordingStopped(event: StreamVideoEvent) {
     if (event.type !== 'call.recording_stopped') return;
-    state.setCallRecordingInProgress(false);
+    state.setMetadata((metadata) => ({
+      ...metadata!,
+      recording: false,
+    }));
+  };
+};
+
+/**
+ * Watches for `call.broadcasting_started` events.
+ */
+export const watchCallBroadcastingStarted = (state: CallState) => {
+  return function onCallBroadcastingStarted(event: StreamVideoEvent) {
+    if (event.type !== 'call.broadcasting_started') return;
+    state.setMetadata((metadata) => ({
+      ...metadata!,
+      broadcasting: true,
+      hls_playlist_url: event.hls_playlist_url,
+    }));
+  };
+};
+
+export const watchCallBroadcastingStopped = (state: CallState) => {
+  return function onCallBroadcastingStopped(event: StreamVideoEvent) {
+    if (event.type !== 'call.broadcasting_stopped') return;
+    state.setMetadata((metadata) => ({
+      ...metadata!,
+      broadcasting: false,
+    }));
   };
 };
