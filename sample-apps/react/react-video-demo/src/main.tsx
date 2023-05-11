cbfb1f2df409d6a7982f54bbb7b08a3cc91cd5ad
zwaardje <niels@seiton.io>
2023-05-11 14:09:22 +0200

    feat(react-demo): add GTM
    Add google tag manager

--- sample-apps/react/react-video-demo/src/main.tsx
+++ sample-apps/react/react-video-demo/src/main.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import TagManager from 'react-gtm-module';
+
+if (import.meta.env.MODE === 'production') {
+  const tagManagerArgs = {
+    gtmId: import.meta.env.VITE_GTM_ID,
+    dataLayer: {
+      userProject: import.meta.env.VITE_GTM_PROJECT,
+    },
+  };
+
+  TagManager.initialize(tagManagerArgs);
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />,
