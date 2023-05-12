GetStream/stream-video-js

commit a5db5a831ccfc833ecfc29e22540be0f0121287d

feat: enable codec preferring through sdp munging (#395)
In react-native, we dont have trasceiver.setCodecPreferences support. So
we have to do sdp-munging.

But this support can be added in the future, upon which this should be
removed. currently, its unknown when, so we had to add sdp-munging.

## Some client JS changes related to web

* If red is to be disabled, we remove only RED codec from the codec
preferences
* If Dtx is disabled, we set it to `usedtx=0` explicitly
* removed the support for setting codecs through the web query params

---------

Co-authored-by: vanGalilea <galiliziv@gmail.com>
Co-authored-by: Oliver Lazoroski <oliver.lazoroski@gmail.com>
Co-authored-by: GitHub Actions Bot <>
Co-authored-by: MartinCupela <32706194+MartinCupela@users.noreply.github.com>
Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
Co-authored-by: Niels Aulman <niels@seiton.io>
Co-authored-by: Vandré Leal <vandreleal@users.noreply.github.com>
Co-authored-by: Stefan Blos <stefan.blos@gmail.com>
Co-authored-by: Tommaso Barbugli <tbarbugli@gmail.com>

packages/client/src/rtc/codecs.ts
@@ -1,8 +1,11 @@
+import { isReactNative } from '../helpers/platforms';
+import { removeCodec, setPreferredCodec } from '../helpers/sdp-munging';
+
 export const getPreferredCodecs = (
   kind: 'audio' | 'video',
   preferredCodec: string,
-  returnOnlyMatched = false,
-) => {
+  codecToRemove?: string,
+): RTCRtpCodecCapability[] | undefined => {
   if (!('getCapabilities' in RTCRtpSender)) {
     console.warn('RTCRtpSender.getCapabilities is not supported');
     return;
@@ -16,6 +19,9 @@ export const getPreferredCodecs = (
   cap.codecs.forEach((c) => {
     const codec = c.mimeType.toLowerCase();
     console.log(c);
+    const shouldRemoveCodec =
+      codecToRemove && codec === `${kind}/${codecToRemove}`;
+    if (shouldRemoveCodec) return;
     const matchesCodec = codec === `${kind}/${preferredCodec}`;
     if (!matchesCodec) {
       unmatched.push(c);
@@ -35,36 +41,54 @@ export const getPreferredCodecs = (
     matched.push(c);
   });
 
-  return returnOnlyMatched
-    ? [...matched]
-    : ([
-        ...matched,
-        ...partialMatched,
-        ...unmatched,
-      ] as RTCRtpCodecCapability[]);
+  return [...matched, ...partialMatched, ...unmatched];
 };
 
 export const getGenericSdp = async (
   direction: RTCRtpTransceiverDirection,
-  preferredCodec: string,
+  isRedEnabled: boolean,
+  preferredVideoCodec: string | undefined,
 ) => {
   const tempPc = new RTCPeerConnection();
+  const videoTransceiver = tempPc.addTransceiver('video', { direction });
+
+  if ('setCodecPreferences' in videoTransceiver) {
+    const videoCodecPreferences = getPreferredCodecs(
+      'audio',
+      preferredVideoCodec ?? 'vp8',
+    );
+    // @ts-ignore
+    videoCodecPreferences.setCodecPreferences([
+      ...(videoCodecPreferences ?? []),
+    ]);
+  }
+
   const audioTransceiver = tempPc.addTransceiver('audio', { direction });
-  tempPc.addTransceiver('video', { direction });
+  const preferredAudioCodec = isRedEnabled ? 'red' : 'opus';
+  const audioCodecToRemove = !isRedEnabled ? 'red' : undefined;
 
   if ('setCodecPreferences' in audioTransceiver) {
-    let returnOnlyMatched = preferredCodec === 'opus';
     const audioCodecPreferences = getPreferredCodecs(
       'audio',
-      preferredCodec,
-      returnOnlyMatched,
+      preferredAudioCodec,
+      audioCodecToRemove,
     );
     // @ts-ignore
     audioTransceiver.setCodecPreferences([...(audioCodecPreferences || [])]);
   }
 
   const offer = await tempPc.createOffer();
-  const sdp = offer.sdp;
+  let sdp = offer.sdp ?? '';
+
+  if (isReactNative()) {
+    if (preferredVideoCodec) {
+      sdp = setPreferredCodec(sdp, 'video', preferredVideoCodec);
+    }
+    sdp = setPreferredCodec(sdp, 'audio', preferredAudioCodec);
+    if (audioCodecToRemove) {
+      sdp = removeCodec(sdp, 'audio', audioCodecToRemove);
+    }
+  }
 
   tempPc.getTransceivers().forEach((t) => {
     t.stop();

packages/client/src/Call.ts
@@ -172,26 +172,6 @@ export class Call {
    */
   private readonly leaveCallHooks: Function[] = [];
 
-  private get preferredAudioCodec() {
-    const audioSettings = this.data?.settings.audio;
-    let preferredCodec =
-      audioSettings?.redundant_coding_enabled === undefined
-        ? 'opus'
-        : audioSettings.redundant_coding_enabled
-        ? 'red'
-        : 'opus';
-    if (
-      typeof window !== 'undefined' &&
-      window.location &&
-      window.location.search
-    ) {
-      const queryParams = new URLSearchParams(window.location.search);
-      preferredCodec = queryParams.get('codec') || preferredCodec;
-    }
-
-    return preferredCodec;
-  }
-
   private readonly streamClientBasePath: string;
   private streamClientEventHandlers = new Map<Function, CallEventHandler>();
 
@@ -663,25 +643,15 @@ export class Call {
     });
 
     const audioSettings = this.data?.settings.audio;
-    let isDtxEnabled =
-      audioSettings?.opus_dtx_enabled === undefined
-        ? false
-        : audioSettings?.opus_dtx_enabled;
-    // TODO: SZ: Remove once SFU team don't need this
-    if (
-      typeof window !== 'undefined' &&
-      window.location &&
-      window.location.search
-    ) {
-      const queryParams = new URLSearchParams(window.location.search);
-      isDtxEnabled = queryParams.get('dtx') === 'false' ? false : isDtxEnabled;
-    }
-    console.log('DTX enabled', isDtxEnabled);
+    const isDtxEnabled = !!audioSettings?.opus_dtx_enabled;
+    const isRedEnabled = !!audioSettings?.redundant_coding_enabled;
     this.publisher = new Publisher({
       sfuClient,
       state: this.state,
       connectionConfig: call.connectionConfig,
       isDtxEnabled,
+      isRedEnabled,
+      preferredVideoCodec: this.streamClient.options.preferredVideoCodec,
     });
 
     this.statsReporter = createStatsReporter({
@@ -720,7 +690,13 @@ export class Call {
         // prepare a generic SDP and send it to the SFU.
         // this is a throw-away SDP that the SFU will use to determine
         // the capabilities of the client (codec support, etc.)
-        .then(() => getGenericSdp('recvonly', this.preferredAudioCodec))
+        .then(() =>
+          getGenericSdp(
+            'recvonly',
+            isRedEnabled,
+            this.streamClient.options.preferredVideoCodec,
+          ),
+        )
         .then((sdp) => sfuClient.join({ subscriberSdp: sdp || '' }));
 
       // 2. in parallel, wait for the SFU to send us the "joinResponse"
@@ -827,9 +803,6 @@ export class Call {
       audioStream,
       audioTrack,
       TrackType.AUDIO,
-      {
-        preferredCodec: this.preferredAudioCodec,
-      },
     );
   };
 

packages/client/src/coordinator/connection/types.ts
@@ -112,6 +112,10 @@ export type StreamClientOptions = Partial<AxiosRequestConfig> & {
   // Set the instance of StableWSConnection on chat client. Its purely for testing purpose and should
   // not be used in production apps.
   wsConnection?: StableWSConnection;
+  /**
+   * The preferred video codec to use.
+   */
+  preferredVideoCodec?: string;
 };
 
 export type TokenProvider = () => Promise<string>;

packages/client/src/helpers/__tests__/sdp-munging.test.ts
@@ -1,0 +1,278 @@
+import { describe, expect, it } from 'vitest';
+import { setPreferredCodec, removeCodec, toggleDtx } from '../sdp-munging';
+
+const sdpWithRed = `v=0
+o=- 3265541491372987511 2 IN IP4 127.0.0.1
+s=-
+t=0 0
+a=group:BUNDLE 0 1
+a=extmap-allow-mixed
+a=msid-semantic: WMS 2bcd08db-9e74-421c-a679-f505bbe3b7d8
+m=audio 9 UDP/TLS/RTP/SAVPF 111 63 103 104 9 102 0 8 106 105 13 110 112 113 126
+c=IN IP4 0.0.0.0
+a=rtcp:9 IN IP4 0.0.0.0
+a=ice-ufrag:bIQJ
+a=ice-pwd:OQKiPkYpIOfGNxR1KUMNgo+N
+a=ice-options:trickle renomination
+a=fingerprint:sha-256 33:C9:9E:47:E5:2A:14:04:F8:F7:79:BE:4A:87:C4:B8:47:D6:57:0B:54:0A:40:8B:84:15:1E:C0:C9:6F:B9:64
+a=setup:actpass
+a=mid:0
+a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid
+a=sendonly
+a=msid:- 4dd21ddd-75b9-4ed5-b7dd-a92ef83c5774
+a=rtcp-mux
+a=rtpmap:111 opus/48000/2
+a=rtcp-fb:111 transport-cc
+a=fmtp:111 minptime=10;useinbandfec=1;usedtx=1
+a=rtpmap:63 red/48000/2
+a=fmtp:63 111/111
+a=rtpmap:103 ISAC/16000
+a=rtpmap:104 ISAC/32000
+a=rtpmap:9 G722/8000
+a=rtpmap:102 ILBC/8000
+a=rtpmap:0 PCMU/8000
+a=rtpmap:8 PCMA/8000
+a=rtpmap:106 CN/32000
+a=rtpmap:105 CN/16000
+a=rtpmap:13 CN/8000
+a=rtpmap:110 telephone-event/48000
+a=rtpmap:112 telephone-event/32000
+a=rtpmap:113 telephone-event/16000
+a=rtpmap:126 telephone-event/8000
+a=ssrc:1923158012 cname:HuoSC6N7qMCUMmlY
+a=ssrc:1923158012 msid:- 4dd21ddd-75b9-4ed5-b7dd-a92ef83c5774
+m=video 9 UDP/TLS/RTP/SAVPF 96 97 98 99 35 36 100 101 125 124 127
+c=IN IP4 0.0.0.0
+a=rtcp:9 IN IP4 0.0.0.0
+a=ice-ufrag:bIQJ
+a=ice-pwd:OQKiPkYpIOfGNxR1KUMNgo+N
+a=ice-options:trickle renomination
+a=fingerprint:sha-256 33:C9:9E:47:E5:2A:14:04:F8:F7:79:BE:4A:87:C4:B8:47:D6:57:0B:54:0A:40:8B:84:15:1E:C0:C9:6F:B9:64
+a=setup:actpass
+a=mid:1
+a=extmap:14 urn:ietf:params:rtp-hdrext:toffset
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:13 urn:3gpp:video-orientation
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay
+a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type
+a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing
+a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid
+a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id
+a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id
+a=sendonly
+a=msid:2bcd08db-9e74-421c-a679-f505bbe3b7d8 56c65a8a-74b8-410e-a90b-5edceb321cfc
+a=rtcp-mux
+a=rtcp-rsize
+a=rtpmap:96 VP8/90000
+a=rtcp-fb:96 goog-remb
+a=rtcp-fb:96 transport-cc
+a=rtcp-fb:96 ccm fir
+a=rtcp-fb:96 nack
+a=rtcp-fb:96 nack pli
+a=rtpmap:97 rtx/90000
+a=fmtp:97 apt=96
+a=rtpmap:98 VP9/90000
+a=rtcp-fb:98 goog-remb
+a=rtcp-fb:98 transport-cc
+a=rtcp-fb:98 ccm fir
+a=rtcp-fb:98 nack
+a=rtcp-fb:98 nack pli
+a=rtpmap:99 rtx/90000
+a=fmtp:99 apt=98
+a=rtpmap:35 AV1/90000
+a=rtcp-fb:35 goog-remb
+a=rtcp-fb:35 transport-cc
+a=rtcp-fb:35 ccm fir
+a=rtcp-fb:35 nack
+a=rtcp-fb:35 nack pli
+a=rtpmap:36 rtx/90000
+a=fmtp:36 apt=35
+a=rtpmap:100 H264/90000
+a=rtcp-fb:100 goog-remb
+a=rtcp-fb:100 transport-cc
+a=rtcp-fb:100 ccm fir
+a=rtcp-fb:100 nack
+a=rtcp-fb:100 nack pli
+a=fmtp:100 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
+a=rtpmap:101 rtx/90000
+a=fmtp:101 apt=100
+a=rtpmap:125 red/90000
+a=rtpmap:124 rtx/90000
+a=fmtp:124 apt=125
+a=rtpmap:127 ulpfec/90000
+a=rid:q send
+a=rid:h send
+a=rid:f send
+a=simulcast:send q;h;f
+`;
+
+// 63 is red so remove 63
+const sdpWithoutRED = `v=0
+o=- 3265541491372987511 2 IN IP4 127.0.0.1
+s=-
+t=0 0
+a=group:BUNDLE 0 1
+a=extmap-allow-mixed
+a=msid-semantic: WMS 2bcd08db-9e74-421c-a679-f505bbe3b7d8
+m=audio 9 UDP/TLS/RTP/SAVPF 111 103 104 9 102 0 8 106 105 13 110 112 113 126
+c=IN IP4 0.0.0.0
+a=rtcp:9 IN IP4 0.0.0.0
+a=ice-ufrag:bIQJ
+a=ice-pwd:OQKiPkYpIOfGNxR1KUMNgo+N
+a=ice-options:trickle renomination
+a=fingerprint:sha-256 33:C9:9E:47:E5:2A:14:04:F8:F7:79:BE:4A:87:C4:B8:47:D6:57:0B:54:0A:40:8B:84:15:1E:C0:C9:6F:B9:64
+a=setup:actpass
+a=mid:0
+a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid
+a=sendonly
+a=msid:- 4dd21ddd-75b9-4ed5-b7dd-a92ef83c5774
+a=rtcp-mux
+a=rtpmap:111 opus/48000/2
+a=rtcp-fb:111 transport-cc
+a=fmtp:111 minptime=10;useinbandfec=1;usedtx=1
+a=rtpmap:103 ISAC/16000
+a=rtpmap:104 ISAC/32000
+a=rtpmap:9 G722/8000
+a=rtpmap:102 ILBC/8000
+a=rtpmap:0 PCMU/8000
+a=rtpmap:8 PCMA/8000
+a=rtpmap:106 CN/32000
+a=rtpmap:105 CN/16000
+a=rtpmap:13 CN/8000
+a=rtpmap:110 telephone-event/48000
+a=rtpmap:112 telephone-event/32000
+a=rtpmap:113 telephone-event/16000
+a=rtpmap:126 telephone-event/8000
+a=ssrc:1923158012 cname:HuoSC6N7qMCUMmlY
+a=ssrc:1923158012 msid:- 4dd21ddd-75b9-4ed5-b7dd-a92ef83c5774
+m=video 9 UDP/TLS/RTP/SAVPF 96 97 98 99 35 36 100 101 125 124 127
+c=IN IP4 0.0.0.0
+a=rtcp:9 IN IP4 0.0.0.0
+a=ice-ufrag:bIQJ
+a=ice-pwd:OQKiPkYpIOfGNxR1KUMNgo+N
+a=ice-options:trickle renomination
+a=fingerprint:sha-256 33:C9:9E:47:E5:2A:14:04:F8:F7:79:BE:4A:87:C4:B8:47:D6:57:0B:54:0A:40:8B:84:15:1E:C0:C9:6F:B9:64
+a=setup:actpass
+a=mid:1
+a=extmap:14 urn:ietf:params:rtp-hdrext:toffset
+a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+a=extmap:13 urn:3gpp:video-orientation
+a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01
+a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay
+a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type
+a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing
+a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space
+a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid
+a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id
+a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id
+a=sendonly
+a=msid:2bcd08db-9e74-421c-a679-f505bbe3b7d8 56c65a8a-74b8-410e-a90b-5edceb321cfc
+a=rtcp-mux
+a=rtcp-rsize
+a=rtpmap:96 VP8/90000
+a=rtcp-fb:96 goog-remb
+a=rtcp-fb:96 transport-cc
+a=rtcp-fb:96 ccm fir
+a=rtcp-fb:96 nack
+a=rtcp-fb:96 nack pli
+a=rtpmap:97 rtx/90000
+a=fmtp:97 apt=96
+a=rtpmap:98 VP9/90000
+a=rtcp-fb:98 goog-remb
+a=rtcp-fb:98 transport-cc
+a=rtcp-fb:98 ccm fir
+a=rtcp-fb:98 nack
+a=rtcp-fb:98 nack pli
+a=rtpmap:99 rtx/90000
+a=fmtp:99 apt=98
+a=rtpmap:35 AV1/90000
+a=rtcp-fb:35 goog-remb
+a=rtcp-fb:35 transport-cc
+a=rtcp-fb:35 ccm fir
+a=rtcp-fb:35 nack
+a=rtcp-fb:35 nack pli
+a=rtpmap:36 rtx/90000
+a=fmtp:36 apt=35
+a=rtpmap:100 H264/90000
+a=rtcp-fb:100 goog-remb
+a=rtcp-fb:100 transport-cc
+a=rtcp-fb:100 ccm fir
+a=rtcp-fb:100 nack
+a=rtcp-fb:100 nack pli
+a=fmtp:100 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
+a=rtpmap:101 rtx/90000
+a=fmtp:101 apt=100
+a=rtpmap:125 red/90000
+a=rtpmap:124 rtx/90000
+a=fmtp:124 apt=125
+a=rtpmap:127 ulpfec/90000
+a=rid:q send
+a=rid:h send
+a=rid:f send
+a=simulcast:send q;h;f
+`;
+
+describe('sdp-munging', () => {
+  it('When H264 video codec is the preferred codec move it to the front of other codecs such as VP8', () => {
+    const mediaType = 'video';
+    const preferredCodec = 'H264';
+    // 100 is H264 so only the order of 100 and 96 should be changed
+    const expectedOutput = sdpWithRed.replace(
+      'm=video 9 UDP/TLS/RTP/SAVPF 96 97 98 99 35 36 100 101 125 124 127',
+      'm=video 9 UDP/TLS/RTP/SAVPF 100 96 97 98 99 35 36 101 125 124 127',
+    );
+    expect(setPreferredCodec(sdpWithRed, mediaType, preferredCodec)).toEqual(
+      expectedOutput,
+    );
+  });
+
+  it('When RED audio codec is the preferred codec move it to the front of other codecs such as OPUS', () => {
+    const mediaType = 'audio';
+    const preferredCodec = 'RED';
+    // 63 is red so only the order of 63 and 111 should be changed
+    const expectedOutput = sdpWithRed.replace(
+      'm=audio 9 UDP/TLS/RTP/SAVPF 111 63 103 104 9 102 0 8 106 105 13 110 112 113 126',
+      'm=audio 9 UDP/TLS/RTP/SAVPF 63 111 103 104 9 102 0 8 106 105 13 110 112 113 126',
+    );
+    expect(setPreferredCodec(sdpWithRed, mediaType, preferredCodec)).toEqual(
+      expectedOutput,
+    );
+  });
+
+  it('When RED audio codec is the preferred codec and if original SDP doesnt support RED, dont do anything', () => {
+    const mediaType = 'audio';
+    const preferredCodec = 'RED';
+    expect(setPreferredCodec(sdpWithoutRED, mediaType, preferredCodec)).toEqual(
+      sdpWithoutRED,
+    );
+  });
+
+  it('Supporting the removal of RED audio codec', () => {
+    const mediaType = 'audio';
+    const codecToRemove = 'RED';
+    expect(removeCodec(sdpWithRed, mediaType, codecToRemove)).toEqual(
+      sdpWithoutRED,
+    );
+  });
+
+  it('Supporting the enabling and disabling of DTX audio codec', () => {
+    const sdp = `m=audio 54312 RTP/AVP 101
+a=rtpmap:101 opus/48000/2
+a=fmtp:101 maxplaybackrate=16000; sprop-maxcapturerate=16000;
+maxaveragebitrate=20000; stereo=1; useinbandfec=1; usedtx=0
+a=ptime:40
+a=maxptime:40`;
+    const dtxEnabledSdp = toggleDtx(sdp, true);
+    expect(dtxEnabledSdp.search('usedtx=1') !== -1).toBeTruthy();
+    const dtxDisabledSdp = toggleDtx(dtxEnabledSdp, false);
+    expect(dtxDisabledSdp.search('usedtx=0') !== -1).toBeTruthy();
+  });
+});

packages/client/src/helpers/sdp-munging.ts
@@ -1,0 +1,233 @@
+type Media = {
+  original: string;
+  mediaWithPorts: string;
+  codecOrder: string;
+};
+
+type RtpMap = {
+  original: string;
+  payload: string;
+  codec: string;
+};
+
+type Fmtp = {
+  original: string;
+  payload: string;
+  config: string;
+};
+
+const getRtpMap = (line: string): RtpMap | undefined => {
+  // Example: a=rtpmap:110 opus/48000/2
+  const rtpRegex = /^a=rtpmap:(\d*) ([\w\-.]*)(?:\s*\/(\d*)(?:\s*\/(\S*))?)?/;
+  // The first captured group is the payload type number, the second captured group is the encoding name, the third captured group is the clock rate, and the fourth captured group is any additional parameters.
+  const rtpMatch = rtpRegex.exec(line);
+  if (rtpMatch) {
+    return {
+      original: rtpMatch[0],
+      payload: rtpMatch[1],
+      codec: rtpMatch[2],
+    };
+  }
+};
+
+const getFmtp = (line: string): Fmtp | undefined => {
+  // Example: a=fmtp:111 minptime=10; useinbandfec=1
+  const fmtpRegex = /^a=fmtp:(\d*) (.*)/;
+  const fmtpMatch = fmtpRegex.exec(line);
+  // The first captured group is the payload type number, the second captured group is any additional parameters.
+  if (fmtpMatch) {
+    return {
+      original: fmtpMatch[0],
+      payload: fmtpMatch[1],
+      config: fmtpMatch[2],
+    };
+  }
+};
+
+/**
+ * gets the media section for the specified media type.
+ * The media section contains the media type, port, codec, and payload type.
+ * Example: m=video 9 UDP/TLS/RTP/SAVPF 100 101 96 97 35 36 102 125 127
+ */
+const getMedia = (line: string, mediaType: string): Media | undefined => {
+  const regex = new RegExp(`(m=${mediaType} \\d+ [\\w/]+) ([\\d\\s]+)`);
+  const match = regex.exec(line);
+  if (match) {
+    return {
+      original: match[0],
+      mediaWithPorts: match[1],
+      codecOrder: match[2],
+    };
+  }
+};
+
+const getMediaSection = (sdp: string, mediaType: 'video' | 'audio') => {
+  let media: Media | undefined;
+  const rtpMap: RtpMap[] = [];
+  const fmtp: Fmtp[] = [];
+  let isTheRequiredMediaSection = false;
+  sdp.split(/(\r\n|\r|\n)/).forEach((line) => {
+    const isValidLine = /^([a-z])=(.*)/.test(line);
+    if (!isValidLine) return;
+    /* 
+      NOTE: according to https://www.rfc-editor.org/rfc/rfc8866.pdf
+      Each media description starts with an "m=" line and continues to the next media description or the end of the whole session description, whichever comes first
+    */
+    const type = line[0];
+    if (type === 'm') {
+      const _media = getMedia(line, mediaType);
+      isTheRequiredMediaSection = !!_media;
+      if (_media) {
+        media = _media;
+      }
+    } else if (isTheRequiredMediaSection && type === 'a') {
+      const rtpMapLine = getRtpMap(line);
+      const fmtpLine = getFmtp(line);
+      if (rtpMapLine) {
+        rtpMap.push(rtpMapLine);
+      } else if (fmtpLine) {
+        fmtp.push(fmtpLine);
+      }
+    }
+  });
+  if (media) {
+    return {
+      media,
+      rtpMap,
+      fmtp,
+    };
+  }
+};
+
+/**
+ * Returns a string of codec IDs with the preferred codec ID in front of the other codec IDs.
+ * It is used to ensure that a preferred codec is used when decoding a media stream.
+ * Example: Suppose we want to prefer VP8 which has id 96
+ * 1. If codec order is 100 101 96 97 35 36 102 125 127
+ * 2. The function returns 96 100 101 97 35 36 102 125 127
+ */
+const moveCodecToFront = (codecOrder: string, preferredCodecId: string) => {
+  const codecIds = codecOrder.split(' ');
+  const index = codecIds.indexOf(preferredCodecId);
+  if (index > -1) {
+    codecIds.splice(index, 1);
+    codecIds.unshift(preferredCodecId);
+  }
+  return codecIds.join(' ');
+};
+
+/**
+ * Returns a string of codec IDs with the given codec ID removed
+ * It is used to ensure that a codec is disabled when processing a media stream.
+ * Example: Suppose we want to prefer RED which has id 63
+ * 1. If codec order is 111 63 103 104 9 102 0 8 106 105 13 110 112 113 126
+ * 2. The function returns 111 103 104 9 102 0 8 106 105 13 110 112 113 126
+ */
+const removeCodecFromOrder = (codecOrder: string, codecIdToRemove: string) => {
+  const codecIds = codecOrder.split(' ');
+  return codecIds.filter((codecID) => codecID !== codecIdToRemove).join(' ');
+};
+
+/**
+ * Returns an SDP with the preferred codec in front of the other codecs.
+ * Example: Suppose we want to prefer VP8
+ * 1. find video media specification m=video 9 UDP/TLS/RTP/SAVPF 100 101 96 97 35 36 102 125 127
+ * 2. look for specified codec (VP8)  a=rtpmap:96 VP8/90000
+ * 3. extract 96 as an identifier of VP8
+ * 4. move 96 to the front
+ * 5. now media looks like this: m=video 9 UDP/TLS/RTP/SAVPF 96 100 101 97 35 36 102 125 127
+ */
+export const setPreferredCodec = (
+  sdp: string,
+  mediaType: 'video' | 'audio',
+  preferredCodec: string,
+) => {
+  const section = getMediaSection(sdp, mediaType);
+  if (!section) return sdp;
+  const rtpMap = section.rtpMap.find(
+    (r) => r.codec.toLowerCase() === preferredCodec.toLowerCase(),
+  );
+  const codecId = rtpMap?.payload;
+  if (!codecId) return sdp;
+  const newCodecOrder = moveCodecToFront(section.media.codecOrder, codecId);
+  return sdp.replace(
+    section.media.original,
+    `${section.media.mediaWithPorts} ${newCodecOrder}`,
+  );
+};
+
+/**
+ * Returns an SDP with the specified codec removed.
+ * Example: Suppose we want to remove RED
+ *  1. find audio media specification m=video 9 UDP/TLS/RTP/SAVPF 100 101 96 97 35 36 102 125 127
+ *  2. look for specified codec (RED)  a=rtpmap:127 red/90000
+ *  3. extract 127 as an identifier of RED
+ *  4. remove 127 from the codec order
+ *  5. remove a=rtpmap:127 red/90000
+ *  6. remove a=fmtp:127 ...
+ */
+export const removeCodec = (
+  sdp: string,
+  mediaType: 'video' | 'audio',
+  codecToRemove: string,
+) => {
+  const section = getMediaSection(sdp, mediaType);
+  const mediaSection = section?.media;
+  if (!mediaSection) {
+    return sdp;
+  }
+  const rtpMap = section?.rtpMap.find(
+    (r) => r.codec.toLowerCase() === codecToRemove.toLowerCase(),
+  );
+  const codecId = rtpMap?.payload;
+  if (!codecId) {
+    return sdp;
+  }
+  const newCodecOrder = removeCodecFromOrder(mediaSection.codecOrder, codecId);
+  const fmtp = section?.fmtp.find((f) => f.payload === codecId);
+  return sdp
+    .replace(
+      mediaSection.original,
+      `${mediaSection.mediaWithPorts} ${newCodecOrder}`,
+    )
+    .replace(new RegExp(`${rtpMap.original}[\r\n|\r|\n]`), '') // remove the corresponding rtpmap line
+    .replace(
+      fmtp?.original ? new RegExp(`${fmtp?.original}[\r\n|\r|\n]`) : '',
+      '',
+    ); // remove the corresponding fmtp line
+};
+
+/**
+ * Gets the fmtp line corresponding to opus
+ */
+const getOpusFmtp = (sdp: string) => {
+  const section = getMediaSection(sdp, 'audio');
+  const rtpMap = section?.rtpMap.find((r) => r.codec.toLowerCase() === 'opus');
+  const codecId = rtpMap?.payload;
+  if (codecId) {
+    const fmtp = section?.fmtp.find((f) => f.payload === codecId);
+    return fmtp;
+  }
+};
+
+/**
+ * Returns an SDP with DTX enabled or disabled.
+ */
+export const toggleDtx = (sdp: string, enable: boolean) => {
+  const opusFmtp = getOpusFmtp(sdp);
+  if (opusFmtp) {
+    const matchDtx = /usedtx=(\d)/.exec(opusFmtp.config);
+    const requiredDtxConfig = `usedtx=${enable ? '1' : '0'}`;
+    if (matchDtx) {
+      const newFmtp = opusFmtp.original.replace(
+        /usedtx=(\d)/,
+        requiredDtxConfig,
+      );
+      return sdp.replace(opusFmtp.original, newFmtp);
+    } else {
+      const newFmtp = `${opusFmtp.original};${requiredDtxConfig}`;
+      return sdp.replace(opusFmtp.original, newFmtp);
+    }
+  }
+  return sdp;
+};

packages/client/src/rtc/__tests__/publisher.test.ts
@@ -49,6 +49,7 @@ describe('Publisher', () => {
       sfuClient,
       state,
       isDtxEnabled: true,
+      isRedEnabled: true,
     });
   });
 

packages/client/src/rtc/publisher.ts
@@ -18,12 +18,20 @@ import {
 } from './helpers/tracks';
 import { CallState } from '../store';
 import { PublishOptions } from '../types';
+import { isReactNative } from '../helpers/platforms';
+import {
+  removeCodec,
+  setPreferredCodec,
+  toggleDtx,
+} from '../helpers/sdp-munging';
 
 export type PublisherOpts = {
   sfuClient: StreamSfuClient;
   state: CallState;
   connectionConfig?: RTCConfiguration;
   isDtxEnabled: boolean;
+  isRedEnabled: boolean;
+  preferredVideoCodec?: string;
 };
 
 /**
@@ -53,12 +61,16 @@ export class Publisher {
     [TrackType.UNSPECIFIED]: undefined,
   };
   private isDtxEnabled: boolean;
+  private isRedEnabled: boolean;
+  private preferredVideoCodec?: string;
 
   constructor({
     connectionConfig,
     sfuClient,
     state,
     isDtxEnabled,
+    isRedEnabled,
+    preferredVideoCodec,
   }: PublisherOpts) {
     const pc = new RTCPeerConnection(connectionConfig);
     pc.addEventListener('icecandidate', this.onIceCandidate);
@@ -78,6 +90,8 @@ export class Publisher {
     this.sfuClient = sfuClient;
     this.state = state;
     this.isDtxEnabled = isDtxEnabled;
+    this.isRedEnabled = isRedEnabled;
+    this.preferredVideoCodec = preferredVideoCodec;
   }
 
   /**
@@ -298,8 +312,13 @@ export class Publisher {
       return getPreferredCodecs('video', preferredCodec || 'vp8');
     }
     if (trackType === TrackType.AUDIO) {
-      const matchedOnly = preferredCodec === 'opus';
-      return getPreferredCodecs('audio', preferredCodec || 'opus', matchedOnly);
+      const defaultAudioCodec = this.isRedEnabled ? 'red' : 'opus';
+      const codecToRemove = !this.isRedEnabled ? 'red' : undefined;
+      return getPreferredCodecs(
+        'audio',
+        preferredCodec ?? defaultAudioCodec,
+        codecToRemove,
+      );
     }
   };
 
@@ -318,12 +337,24 @@ export class Publisher {
   private onNegotiationNeeded = async () => {
     console.log('AAA onNegotiationNeeded');
     const offer = await this.publisher.createOffer();
-    if (this.isDtxEnabled && offer.sdp) {
-      offer.sdp = offer.sdp.replace(
-        'useinbandfec=1',
-        'useinbandfec=1;usedtx=1',
-      );
+    let sdp = offer.sdp;
+    if (sdp) {
+      toggleDtx(sdp, this.isDtxEnabled);
+      if (isReactNative()) {
+        if (this.preferredVideoCodec) {
+          sdp = setPreferredCodec(sdp, 'video', this.preferredVideoCodec);
+        }
+        sdp = setPreferredCodec(
+          sdp,
+          'audio',
+          this.isRedEnabled ? 'red' : 'opus',
+        );
+        if (!this.isRedEnabled) {
+          sdp = removeCodec(sdp, 'audio', 'red');
+        }
+      }
     }
+    offer.sdp = sdp;
     await this.publisher.setLocalDescription(offer);
 
     const trackInfos = this.publisher

commit 5bca195dbb99efb77c5d7106a251e88d9a045498

fix: disable sdp munging (#491)
Overview: a hot fix for a regression introduced with #395

packages/client/src/rtc/codecs.ts
@@ -52,30 +52,26 @@ export const getGenericSdp = async (
   const tempPc = new RTCPeerConnection();
   const videoTransceiver = tempPc.addTransceiver('video', { direction });
 
-  if ('setCodecPreferences' in videoTransceiver) {
-    const videoCodecPreferences = getPreferredCodecs(
-      'audio',
-      preferredVideoCodec ?? 'vp8',
-    );
-    // @ts-ignore
-    videoCodecPreferences.setCodecPreferences([
-      ...(videoCodecPreferences ?? []),
-    ]);
-  }
+  // if ('setCodecPreferences' in videoTransceiver) {
+  //   const videoCodecPreferences = getPreferredCodecs(
+  //     'audio',
+  //     preferredVideoCodec ?? 'vp8',
+  //   );
+  //   videoTransceiver.setCodecPreferences([...(videoCodecPreferences ?? [])]);
+  // }
 
   const audioTransceiver = tempPc.addTransceiver('audio', { direction });
   const preferredAudioCodec = isRedEnabled ? 'red' : 'opus';
   const audioCodecToRemove = !isRedEnabled ? 'red' : undefined;
 
-  if ('setCodecPreferences' in audioTransceiver) {
-    const audioCodecPreferences = getPreferredCodecs(
-      'audio',
-      preferredAudioCodec,
-      audioCodecToRemove,
-    );
-    // @ts-ignore
-    audioTransceiver.setCodecPreferences([...(audioCodecPreferences || [])]);
-  }
+  // if ('setCodecPreferences' in audioTransceiver) {
+  //   const audioCodecPreferences = getPreferredCodecs(
+  //     'audio',
+  //     preferredAudioCodec,
+  //     // audioCodecToRemove,
+  //   );
+  //   audioTransceiver.setCodecPreferences([...(audioCodecPreferences || [])]);
+  // }
 
   const offer = await tempPc.createOffer();
   let sdp = offer.sdp ?? '';