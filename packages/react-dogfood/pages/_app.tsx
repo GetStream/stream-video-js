3c89208b2051104d997670d9f28f601fd88f9a90
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-05-12 16:54:57 +0200

    feat: new favicons for Pronto (#496)

--- packages/react-dogfood/pages/_app.tsx
+++ packages/react-dogfood/pages/_app.tsx
@@ -43,6 +43,36 @@ export default function App({
       <Head>
         <title>Stream Calls</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
       </Head>
 
       <CssBaseline />
