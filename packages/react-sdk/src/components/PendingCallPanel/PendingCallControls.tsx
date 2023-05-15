404d3548e5663e9a95358cdedaa5a83cb7967a7c
martincupela <martin.cupela@gmail.com>
2023-05-15 12:50:45 +0200

    refactor(react-sdk): return call.leave promise

--- packages/react-sdk/src/components/PendingCallPanel/PendingCallControls.tsx
+++ packages/react-sdk/src/components/PendingCallPanel/PendingCallControls.tsx
@@ -17,9 +17,7 @@ export const PendingCallControls = () => {
         <>
           <AcceptCallButton call={call} disabled={buttonsDisabled} />
           <CancelCallButton
-            onClick={() => {
-              call.leave({ reject: true });
-            }}
+            onClick={() => call.leave({ reject: true })}
             disabled={buttonsDisabled}
           />
         </>
