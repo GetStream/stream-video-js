58cc252e540ffd8c33c67f304d5c346fad7970e0
martincupela <martin.cupela@gmail.com>
2023-05-17 12:55:59 +0200

    fix(react-video-demo): observe devices in DeviceSettings

--- sample-apps/react/react-video-demo/src/components/DeviceSettings/DeviceSettings.tsx
+++ sample-apps/react/react-video-demo/src/components/DeviceSettings/DeviceSettings.tsx
@@ -1,6 +1,11 @@
 import { FC, useCallback, useState } from 'react';
 
-import { useMediaDevices } from '@stream-io/video-react-sdk';
+import {
+  useMediaDevices,
+  useObserveAudioInputDevices,
+  useObserveAudioOutputDevices,
+  useObserveVideoDevices,
+} from '@stream-io/video-react-sdk';
 
 import { Cog } from '../Icons';
 
@@ -19,15 +24,14 @@ export type Props = {
 export const DeviceSettings: FC<Props> = ({ className }) => {
   const {
     selectedVideoDeviceId,
-    audioInputDevices,
     isAudioOutputChangeSupported,
     selectedAudioInputDeviceId,
     selectedAudioOutputDeviceId,
-    videoDevices,
-    audioOutputDevices,
     switchDevice,
   } = useMediaDevices();
-
+  const videoDevices = useObserveVideoDevices();
+  const audioInputDevices = useObserveAudioInputDevices();
+  const audioOutputDevices = useObserveAudioOutputDevices();
   const { close } = useModalContext();
 
   const [audioInputId, setAudioInputId] = useState<string>();
