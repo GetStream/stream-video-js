7fcdb0e8ef4fc8b1afae471e66d3ac8a4834a1a6
Anton Arnautov <arnautov.anton@gmail.com>
2023-05-25 14:09:55 +0200

    Return UNSPECIFIED connectionQuality check

--- packages/react-sdk/src/core/components/ParticipantView/DefaultParticipantViewUI.tsx
+++ packages/react-sdk/src/core/components/ParticipantView/DefaultParticipantViewUI.tsx
@@ -88,6 +88,7 @@ export const ParticipantDetails = ({
   const call = useCall()!;
 
   const connectionQualityAsString =
+    !!connectionQuality &&
     SfuModels.ConnectionQuality[connectionQuality].toLowerCase();
 
   const hasAudio = publishedTracks.includes(SfuModels.TrackType.AUDIO);
