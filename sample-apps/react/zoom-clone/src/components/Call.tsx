aa1e64f102827d85f705fe5aa91fcd8f6255711d
Zita Szupera <szuperaz@gmail.com>
2023-06-06 16:25:07 +0200

    feat: Update zoom clone

--- sample-apps/react/zoom-clone/src/components/Call.tsx
+++ sample-apps/react/zoom-clone/src/components/Call.tsx
@@ -1,30 +1,52 @@
-import { useState } from 'react';
-import { StreamCall } from '@stream-io/video-react-sdk';
+import { useEffect, useState } from 'react';
+import {
+  Call,
+  StreamCall,
+  useConnectedUser,
+  useStreamVideoClient,
+} from '@stream-io/video-react-sdk';
 import { useParams } from 'react-router-dom';
 
 import { MeetingUI } from './MeetingUI';
 import { ChatSidebar } from './ChatSidebar';
 
-export const Call = () => {
+export const CallUI = () => {
   const { callId } = useParams();
+  const client = useStreamVideoClient();
+  const user = useConnectedUser();
   const [chatOpen, setChatOpen] = useState(false);
+  const [call, setCall] = useState<Call | undefined>(undefined);
+
+  useEffect(() => {
+    if (!callId || !client || !user) {
+      return;
+    }
+
+    setCall(client.call('default', callId));
+  }, [client, user, callId]);
+
+  useEffect(() => {
+    if (!call) {
+      return;
+    }
+
+    call.join({ create: true });
+  }, [call]);
+
   return (
     <div className="flex w-full h-full">
-      <StreamCall
-        callId={callId!}
-        callType="default"
-        data={{ create: true }}
-        autoJoin
-      >
-        <MeetingUI>
-          <button
-            onClick={() => setChatOpen((pv) => !pv)}
-            className="absolute right-4 bg-zinc-600 rounded-full flex justify-center items-center text-white px-2 py-1"
-          >
-            {chatOpen ? 'Close' : 'Open'} chat
-          </button>
-        </MeetingUI>
-      </StreamCall>
+      {call && (
+        <StreamCall call={call}>
+          <MeetingUI>
+            <button
+              onClick={() => setChatOpen((pv) => !pv)}
+              className="absolute right-4 bg-zinc-600 rounded-full flex justify-center items-center text-white px-2 py-1"
+            >
+              {chatOpen ? 'Close' : 'Open'} chat
+            </button>
+          </MeetingUI>
+        </StreamCall>
+      )}
 
       {chatOpen && <ChatSidebar />}
     </div>
