91ce541b5ecdbe24d5c5b5402bca3222222c95fb
vanGalilea <galiliziv@gmail.com>
2023-06-06 13:26:30 +0200

    remove duplicate extend expect in config

--- packages/react-native-sdk/jest.config.ts
+++ packages/react-native-sdk/jest.config.ts
@@ -2,10 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'react-native',
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-    '<rootDir>/jest-setup.ts',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/__tests__/mocks/',
     '<rootDir>/__tests__/utils/',
