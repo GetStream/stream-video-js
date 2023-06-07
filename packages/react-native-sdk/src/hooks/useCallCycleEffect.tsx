44f354d765a53e4e1adc532d9ef8120e11980c75
khushal87 <khushal.agarwal987@gmail.com>
2023-06-07 12:46:48 +0530

    fix: types of StreamCall props

--- packages/react-native-sdk/src/hooks/useCallCycleEffect.tsx
+++ packages/react-native-sdk/src/hooks/useCallCycleEffect.tsx
@@ -28,9 +28,9 @@ export const useCallCycleEffect = (
 
   useEffect(() => {
     if (!call || NON_ACTIVE_CALLING_STATES.includes(callingState)) return;
-    const isCallCreatedByMe = call?.data?.created_by.id === call?.currentUserId;
+    const isCallCreatedByMe = call.data?.created_by.id === call?.currentUserId;
     const isCallCreatedByOther =
-      !!call?.data?.created_by.id && !isCallCreatedByMe;
+      !!call.data?.created_by.id && !isCallCreatedByMe;
     const isIncomingCall =
       callingState === CallingState.RINGING &&
       !isCallCreatedByMe &&
