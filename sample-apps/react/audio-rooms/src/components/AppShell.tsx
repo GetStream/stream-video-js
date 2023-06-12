7ec007ae7d3b730993303140beff67e2db5ead68
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-06-12 13:19:42 +0200

    fix: adjust to the new client API

--- sample-apps/react/audio-rooms/src/components/AppShell.tsx
+++ sample-apps/react/audio-rooms/src/components/AppShell.tsx
@@ -1,8 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import {
-  StreamVideo,
-  useCreateStreamVideoClient,
-} from '@stream-io/video-react-sdk';
+import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
 import { useUserContext } from '../contexts/UserContext';
 import Sidebar from './Sidebar';
 import { useEffect, useState } from 'react';
@@ -12,20 +9,26 @@ import { LoadingPanel } from './Loading';
 const apiKey = import.meta.env.VITE_STREAM_API_KEY as string;
 
 export const AppShell = () => {
-  const { user } = useUserContext();
-
-  const client = useCreateStreamVideoClient({
-    apiKey,
-    tokenOrProvider: user?.token || '',
-    user: {
-      id: user?.id || '',
-      image: user?.imageUrl,
-      name: user?.name,
-    },
-  });
-
   const [loadingCalls, setLoadingCalls] = useState(true);
-  const [loadingError, setLoadingError] = useState<Error | undefined>();
+  const [loadingError, setLoadingError] = useState<Error>();
+
+  const [client] = useState(() => new StreamVideoClient(apiKey));
+  const { user } = useUserContext();
+  useEffect(() => {
+    if (!user) return;
+    const connect = client.connectUser(user, user.token).catch((err) => {
+      console.log('Failed to establish connection', err);
+      setLoadingError(err);
+    });
+    return () => {
+      connect
+        .then(() => client.disconnectUser())
+        .catch((err) => {
+          console.log('Failed to disconnect', err);
+          setLoadingError(err);
+        });
+    };
+  }, [client, user]);
 
   useEffect(() => {
     if (!client) return;
