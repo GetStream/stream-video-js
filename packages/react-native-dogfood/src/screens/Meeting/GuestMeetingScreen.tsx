a942a06f8b6bd974300c2b1afa8ff8c52ab20b9b
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-06-14 10:09:14 +0200

    fix: unnecessary import

--- packages/react-native-dogfood/src/screens/Meeting/GuestMeetingScreen.tsx
+++ packages/react-native-dogfood/src/screens/Meeting/GuestMeetingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
   StreamCall,
