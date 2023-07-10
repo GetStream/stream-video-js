1c193f8a89e1c9c16aa362f448e760f688b8aceb
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-07-10 13:40:03 +0200

    fix: option to opt-out from call creation
    This fixes an edge case in the tutorial flow that caused audio rooms to be created by a wrong user (buddy user).

--- sample-apps/react/audio-rooms/src/components/Error.tsx
+++ sample-apps/react/audio-rooms/src/components/Error.tsx
@@ -1,6 +1,13 @@
-export const ErrorPanel = ({ error }: { error: Error }) => (
-  <div className="error-panel">
-    <h2>Error</h2>
-    {error.message}
-  </div>
-);
+export const ErrorPanel = ({ error }: { error: Error }) => {
+  let message = error.message;
+  // @ts-ignore See APIErrorCodes
+  if (error.code === 16) {
+    message = `Room doesn't exist yet. Please create it first.`;
+  }
+  return (
+    <div className="error-panel">
+      <h2>Error</h2>
+      {message}
+    </div>
+  );
+};
