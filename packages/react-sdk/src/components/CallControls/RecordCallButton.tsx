e6fab59d3ebc4b91b8b8ed79e6f56bf6b6b10b52
Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
2023-07-17 15:33:23 +0200

    feat(react-sdk): extract toggle functions to hooks + permissions (#750)
    ### Known bugs for permissions
    
    1. permission states not persisted/properly propagated
    - disable audio and video by clicking the context buttons - whatever
    comes first (in this case audio) is overriden and can be published by
    the user
    - after reload (user which had their audio/video disabled) can seemingly
    publish tracks but other users won't see/hear anything - works only
    after explicitly allowing it (sometimes)
    
    2. missing dismiss events (do nothing)
    - currently "dismissing" permission requests leads to revoking (user A
    and user B receive request from user C, user A allows, user B
    "dismisses", user C still cannot publish)
    - dismissing permission request from the requestor side won't remove
    dismissed requests from the list of the target users
    
    TODO:
    - [x] add dismiss button to the `PermissionRequestList` element
    - [x] finish adjusting documentation
    - [x] add new translations

--- packages/react-sdk/src/components/CallControls/RecordCallButton.tsx
+++ packages/react-sdk/src/components/CallControls/RecordCallButton.tsx
@@ -1,12 +1,8 @@
-import { useCallback, useEffect, useState } from 'react';
 import { OwnCapability } from '@stream-io/video-client';
-import {
-  Restricted,
-  useCall,
-  useIsCallRecordingInProgress,
-} from '@stream-io/video-react-bindings';
+import { Restricted, useCall, useI18n } from '@stream-io/video-react-bindings';
 import { CompositeButton, IconButton } from '../Button/';
 import { LoadingIndicator } from '../LoadingIndicator';
+import { useToggleCallRecording } from '../../hooks/useToggleCallRecording';
 
 export type RecordCallButtonProps = {
   caption?: string;
@@ -16,30 +12,10 @@ export const RecordCallButton = ({
   caption = 'Record',
 }: RecordCallButtonProps) => {
   const call = useCall();
-  const isCallRecordingInProgress = useIsCallRecordingInProgress();
-  const [isAwaitingResponse, setIsAwaitingResponse] = useState(false);
-  useEffect(() => {
-    // we wait until call.recording_started/stopped event to flips the
-    // `isCallRecordingInProgress` state variable.
-    // Once the flip happens, we remove the loading indicator
-    setIsAwaitingResponse((isAwaiting) => {
-      if (isAwaiting) return false;
-      return isAwaiting;
-    });
-  }, [isCallRecordingInProgress]);
 
-  const toggleRecording = useCallback(async () => {
-    try {
-      setIsAwaitingResponse(true);
-      if (isCallRecordingInProgress) {
-        await call?.stopRecording();
-      } else {
-        await call?.startRecording();
-      }
-    } catch (e) {
-      console.error(`Failed start recording`, e);
-    }
-  }, [call, isCallRecordingInProgress]);
+  const { t } = useI18n();
+  const { toggleCallRecording, isAwaitingResponse, isCallRecordingInProgress } =
+    useToggleCallRecording();
 
   return (
     <Restricted
@@ -53,8 +29,8 @@ export const RecordCallButton = ({
           <LoadingIndicator
             tooltip={
               isCallRecordingInProgress
-                ? 'Waiting for recording to stop... '
-                : 'Waiting for recording to start...'
+                ? t('Waiting for recording to stop...')
+                : t('Waiting for recording to start...')
             }
           />
         ) : (
@@ -63,8 +39,8 @@ export const RecordCallButton = ({
             enabled={!!call}
             disabled={!call}
             icon={isCallRecordingInProgress ? 'recording-on' : 'recording-off'}
-            title="Record call"
-            onClick={toggleRecording}
+            title={t('Record call')}
+            onClick={toggleCallRecording}
           />
         )}
       </CompositeButton>
--- packages/react-sdk/src/components/CallControls/ScreenShareButton.tsx
+++ packages/react-sdk/src/components/CallControls/ScreenShareButton.tsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from 'react';
-import {
-  getScreenShareStream,
-  OwnCapability,
-  SfuModels,
-} from '@stream-io/video-client';
+import { OwnCapability } from '@stream-io/video-client';
 import {
   Restricted,
   useCall,
   useHasOngoingScreenShare,
-  useHasPermissions,
-  useLocalParticipant,
+  useI18n,
 } from '@stream-io/video-react-bindings';
 import { CompositeButton, IconButton } from '../Button/';
 import { PermissionNotification } from '../Notification';
+import { useToggleScreenShare } from '../../hooks';
 
 export type ScreenShareButtonProps = {
   caption?: string;
@@ -22,60 +17,27 @@ export const ScreenShareButton = ({
   caption = 'Screen Share',
 }: ScreenShareButtonProps) => {
   const call = useCall();
-  const localParticipant = useLocalParticipant();
   const isSomeoneScreenSharing = useHasOngoingScreenShare();
-  const isScreenSharing = localParticipant?.publishedTracks.includes(
-    SfuModels.TrackType.SCREEN_SHARE,
-  );
 
-  const [isAwaitingApproval, setIsAwaitingApproval] = useState(false);
-  const hasPermission = useHasPermissions(OwnCapability.SCREENSHARE);
-  useEffect(() => {
-    if (hasPermission) {
-      setIsAwaitingApproval(false);
-    }
-  }, [hasPermission]);
+  const { t } = useI18n();
+  const { toggleScreenShare, isAwaitingPermission, isScreenSharing } =
+    useToggleScreenShare();
+
   return (
     <Restricted requiredGrants={[OwnCapability.SCREENSHARE]}>
       <PermissionNotification
         permission={OwnCapability.SCREENSHARE}
-        isAwaitingApproval={isAwaitingApproval}
-        messageApproved="You can now share your screen."
-        messageAwaitingApproval="Awaiting for an approval to share screen."
-        messageRevoked="You can no longer share your screen."
+        isAwaitingApproval={isAwaitingPermission}
+        messageApproved={t('You can now share your screen.')}
+        messageAwaitingApproval={t('Awaiting for an approval to share screen.')}
+        messageRevoked={t('You can no longer share your screen.')}
       >
         <CompositeButton active={isSomeoneScreenSharing} caption={caption}>
           <IconButton
             icon={isScreenSharing ? 'screen-share-on' : 'screen-share-off'}
-            title="Share screen"
+            title={t('Share screen')}
             disabled={(!isScreenSharing && isSomeoneScreenSharing) || !call}
-            onClick={async () => {
-              if (
-                !hasPermission &&
-                call?.permissionsContext.canRequest(OwnCapability.SCREENSHARE)
-              ) {
-                setIsAwaitingApproval(true);
-                await call
-                  .requestPermissions({
-                    permissions: [OwnCapability.SCREENSHARE],
-                  })
-                  .catch((reason) => {
-                    console.log('RequestPermissions failed', reason);
-                  });
-                return;
-              }
-
-              if (!isScreenSharing && hasPermission) {
-                const stream = await getScreenShareStream().catch((e) => {
-                  console.log(`Can't share screen: ${e}`);
-                });
-                if (stream) {
-                  await call?.publishScreenShareStream(stream);
-                }
-              } else {
-                await call?.stopPublish(SfuModels.TrackType.SCREEN_SHARE);
-              }
-            }}
+            onClick={toggleScreenShare}
           />
         </CompositeButton>
       </PermissionNotification>
