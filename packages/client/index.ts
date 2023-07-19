05d92b5910f9e6bcd576825692de7b18958760f8
Zita Szupera <szuperaz@gmail.com>
2023-07-19 09:56:59 +0200

    export streamvideoserverclient

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -13,6 +13,7 @@ export * from './src/stats/types';
 export * from './src/Call';
 export * from './src/CallType';
 export * from './src/StreamVideoClient';
+export * from './src/StreamVideoServerClient';
 export * from './src/StreamSfuClient';
 export * from './src/devices';
 export * from './src/store';
