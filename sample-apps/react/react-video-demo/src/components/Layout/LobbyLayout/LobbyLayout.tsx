c3ecb8c002e5423e4aa3b6b3c7589fa634a271e9
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-07-20 18:21:00 +0200

    feat: advertise native android apps in a banner

--- sample-apps/react/react-video-demo/src/components/Layout/LobbyLayout/LobbyLayout.tsx
+++ sample-apps/react/react-video-demo/src/components/Layout/LobbyLayout/LobbyLayout.tsx
@@ -1,10 +1,12 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useState } from 'react';
 import classnames from 'classnames';
+import { isAndroid } from 'mobile-device-detect';
 import { FeatureCollection, Geometry } from 'geojson';
 
 import LatencyMap from '../../LatencyMap';
 
 import styles from './LobbyLayout.module.css';
+import { MobileAppBanner } from '../../Header/MobileAppBanner';
 
 export type Props = {
   className?: string;
@@ -19,13 +21,31 @@ export const LobbyLayout: FC<Props> = ({
   children,
   edges,
 }) => {
+  const shouldRenderMobileAppBanner = isAndroid;
+  const [shouldCenterBody, setShouldCenterBody] = useState(
+    !shouldRenderMobileAppBanner,
+  );
   const rootClassName = classnames(styles.root, className);
   return (
     <section className={rootClassName}>
       <LatencyMap className={styles.latencyMap} sourceData={edges} />
+      {shouldRenderMobileAppBanner && (
+        <MobileAppBanner
+          className={styles.mobileBanner}
+          onDismiss={() => {
+            setShouldCenterBody(true);
+          }}
+        />
+      )}
       <section className={styles.layoutContainer}>
         <div className={styles.header}>{header}</div>
-        <div className={styles.body}>{children}</div>
+        <div
+          className={classnames(styles.body, {
+            'items-center': shouldCenterBody,
+          })}
+        >
+          {children}
+        </div>
       </section>
     </section>
   );
