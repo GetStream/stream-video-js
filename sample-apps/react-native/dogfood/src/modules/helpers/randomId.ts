10e5fb2e82a9143db2920640ce4fd397a2811a3d
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-07-20 10:34:33 +0200

    chore: 15 length id

--- sample-apps/react-native/dogfood/src/modules/helpers/randomId.ts
+++ sample-apps/react-native/dogfood/src/modules/helpers/randomId.ts
@@ -1,3 +1,8 @@
+// A random id of length 15. (based on https://stackoverflow.com/a/38622545)
 export function randomId() {
-  return Math.random().toString(36).substring(2, 12);
+  return (
+    Math.random().toString(36).slice(2, 7) +
+    Math.random().toString(36).slice(2, 7) +
+    Math.random().toString(36).slice(2, 7)
+  );
 }
