3f105cf7fc7cae50cd88c274fb82d56f49d58801
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-07-24 18:04:22 +0530

    refactor(i18n): remove de and es translations files

--- packages/i18n/src/translations/index.ts
+++ packages/i18n/src/translations/index.ts
@@ -1,6 +1,4 @@
-import de from './de.json';
 import en from './en.json';
-import es from './es.json';
 
-const translations = { de, en, es };
+const translations = { en };
 export default translations;
