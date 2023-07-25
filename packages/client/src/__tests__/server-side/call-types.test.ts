06759c1e657e3d35a923a85faf6bec66eb00e6da
Zita Szupera <zita.szupera@getstream.io>
2023-07-25 13:31:42 +0200

    chore: More integration tests (#850)

--- packages/client/src/__tests__/server-side/call-types.test.ts
+++ packages/client/src/__tests__/server-side/call-types.test.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import { beforeAll, describe, expect, it } from 'vitest';
 import { StreamVideoServerClient } from '../../StreamVideoServerClient';
 import { generateUUIDv4 } from '../../coordinator/connection/utils';
+import { LogLevel } from '../../coordinator/connection/types';
+import { OwnCapability } from '../../gen/coordinator';
 
 const apiKey = process.env.STREAM_API_KEY!;
 const secret = process.env.STREAM_SECRET!;
@@ -13,16 +15,42 @@ describe('call types CRUD API', () => {
   beforeAll(() => {
     client = new StreamVideoServerClient(apiKey, {
       secret,
-      logLevel: 'error',
+      logLevel: 'trace',
+      logger: (logLevel: LogLevel, message: string, ...args: unknown[]) => {
+        console.log(new Date().toISOString(), message, ...args);
+      },
     });
   });
 
   it('create', async () => {
     const createResponse = await client.createCallType({
       name: callTypeName,
+      settings: {
+        audio: { mic_default_on: true, default_device: 'speaker' },
+      },
+      grants: {
+        admin: [
+          OwnCapability.SEND_AUDIO,
+          OwnCapability.SEND_VIDEO,
+          OwnCapability.MUTE_USERS,
+        ],
+        user: [OwnCapability.SEND_AUDIO, OwnCapability.SEND_VIDEO],
+      },
     });
 
     expect(createResponse.name).toBe(callTypeName);
+    expect(createResponse.settings.audio.mic_default_on).toBe(true);
+    expect(createResponse.settings.audio.default_device).toBe('speaker');
+    expect(createResponse.grants.admin).toBeDefined();
+    expect(createResponse.grants.user).toBeDefined();
+  });
+
+  it('read', async () => {
+    const readResponse = await client.getCallTypes();
+
+    expect(readResponse.call_types[callTypeName]).toContain({
+      name: callTypeName,
+    });
   });
 
   it('update', async () => {
@@ -34,14 +62,6 @@ describe('call types CRUD API', () => {
 
     expect(updateResponse.settings.audio.mic_default_on).toBeFalsy();
     expect(updateResponse.settings.audio.default_device).toBe('earpiece');
-  });
-
-  it('read', async () => {
-    const readResponse = await client.getCallTypes();
-
-    expect(readResponse.call_types[callTypeName]).toContain({
-      name: callTypeName,
-    });
   });
 
   it('delete', async () => {
