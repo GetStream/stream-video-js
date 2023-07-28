c16d0a283b005a77dfbcbb3bb7c9946dcc501094
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-07-28 11:45:59 +0200

    docs: rewrite video-calling tutorial (#866)
    A simplified version of the Video Calling tutorial. Matches with the
    structure of Android and iOS counterparts.

--- sample-apps/react/stream-video-react-tutorial/src/App.tsx
+++ sample-apps/react/stream-video-react-tutorial/src/App.tsx
@@ -1,72 +1,73 @@
 import { useEffect, useState } from 'react';
 import {
   Call,
+  CallControls,
+  CallingState,
+  SpeakerLayout,
   StreamCall,
   StreamTheme,
   StreamVideo,
   StreamVideoClient,
-  PaginatedGridLayout,
-  CallControls,
-  CallingState,
+  useCallCallingState,
+  User,
 } from '@stream-io/video-react-sdk';
 
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 import './style.css';
 
+const apiKey = 'REPLACE_WITH_API_KEY'; // the API key can be found in the "Credentials" section
+const token = 'REPLACE_WITH_TOKEN'; // the token can be found in the "Credentials" section
+const userId = 'REPLACE_WITH_USER_ID'; // the user id can be found in the "Credentials" section
+const callId = 'REPLACE_WITH_CALL_ID'; // the call id can be found in the "Credentials" section
+
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+
+// initialize the StreamVideoClient
+const client = new StreamVideoClient({ apiKey, user, token });
+
 export default function App() {
-  const [client, setClient] = useState<StreamVideoClient | undefined>();
-  const [call, setCall] = useState<Call | undefined>();
-
+  const [call, setCall] = useState<Call>();
   useEffect(() => {
-    const user = {
-      id: import.meta.env.VITE_STREAM_USER_ID,
-    };
-    const token = import.meta.env.VITE_STREAM_USER_TOKEN;
-
-    const client = new StreamVideoClient({
-      apiKey: import.meta.env.VITE_STREAM_API_KEY,
-      user,
-      token,
+    const myCall = client.call('default', callId);
+    myCall.join({ create: true }).catch((err) => {
+      console.error(`Failed to join the call`, err);
     });
-    setClient(client);
+
+    setCall(myCall);
 
     return () => {
-      client?.disconnectUser();
+      setCall(undefined);
+      myCall.leave().catch((err) => {
+        console.error(`Failed to leave the call`, err);
+      });
     };
   }, []);
 
-  useEffect(() => {
-    const call = client?.call('default', import.meta.env.VITE_STREAM_CALL_ID);
-    call?.join({ create: true });
-    setCall(call);
-
-    return () => {
-      if (call?.state?.callingState !== CallingState.LEFT) {
-        call?.leave();
-      }
-      setCall(undefined);
-    };
-  }, [client]);
-
-  if (!client || !call) {
-    return null;
-  }
+  if (!call) return null;
 
   return (
     <StreamVideo client={client}>
       <StreamCall call={call}>
-        <StreamTheme className="video__call">
-          <UI />
-        </StreamTheme>
+        <UILayout />
       </StreamCall>
     </StreamVideo>
   );
 }
 
-export const UI = () => {
+export const UILayout = () => {
+  const callingState = useCallCallingState();
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <PaginatedGridLayout />
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition="bottom" />
       <CallControls />
-    </>
+    </StreamTheme>
   );
 };
