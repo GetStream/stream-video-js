24bad9032efb538f9cf0dbab360e888e63a2b9c3
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-08-03 19:50:57 +0200

    fix: hide participant actions menu in Composite App (#899)
    Hides the "Participant Actions" menu in the Grid layout.

--- sample-apps/react/egress-composite/src/layouts/grid/GridView.tsx
+++ sample-apps/react/egress-composite/src/layouts/grid/GridView.tsx
@@ -1,19 +1,21 @@
 import {
-  PaginatedGridLayout,
   DefaultParticipantViewUI,
+  PaginatedGridLayout,
 } from '@stream-io/video-react-sdk';
 import { useAppConfig } from '../../hooks/useAppConfig';
 
 import './GridView.scss';
 
 export const GridView = () => {
-  // const setParticipantVideoRef = useEgressReadyWhenAnyParticipantMounts();
   const { gridSize } = useAppConfig();
   return (
     <div className="grid-view">
       <PaginatedGridLayout
         ParticipantViewUI={
-          <DefaultParticipantViewUI indicatorsVisible={false} />
+          <DefaultParticipantViewUI
+            indicatorsVisible={false}
+            showMenuButton={false}
+          />
         }
         excludeLocalParticipant
         groupSize={gridSize || 25}
