a2500e741bbee551b152f979199adb4967e9c07c
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-08-11 20:43:21 +0200

    chore: revert the usage of sample app codes

--- sample-apps/react-native/dogfood/index.js
+++ sample-apps/react-native/dogfood/index.js
@@ -3,18 +3,8 @@ import 'react-native-gesture-handler';
 
 import { AppRegistry } from 'react-native';
 import * as Sentry from '@sentry/react-native';
-import App from './AppLive';
+import App from './App';
 import { name as appName } from './app.json';
-
-AppRegistry.registerHeadlessTask(
-  'RNCallKeepBackgroundMessage',
-  () =>
-    ({ name, callUUID, handle }) => {
-      // TODO: Make your call here
-
-      return Promise.resolve();
-    },
-);
 
 Sentry.init({
   dsn: 'https://3f853df83a5843bfa09a1e0dc785dbf6@o14368.ingest.sentry.io/4504248362926080',
