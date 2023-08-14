b05b2b1609ae14fe5f228df86930d8bd831cb80b
martincupela <martin.cupela@gmail.com>
2023-08-14 16:09:29 +0200

    feat(react-video-demo): add NewMessageNotification

--- sample-apps/react/react-video-demo/src/components/Views/MeetingView/MeetingView.tsx
+++ sample-apps/react/react-video-demo/src/components/Views/MeetingView/MeetingView.tsx
@@ -20,7 +20,6 @@ import MeetingLayout from '../../Layout/MeetingLayout';
 import { useWatchChannel } from '../../../hooks/useWatchChannel';
 
 import { useTourContext } from '../../../contexts/TourContext';
-import { usePanelContext } from '../../../contexts/PanelContext';
 import { useNotificationContext } from '../../../contexts/NotificationsContext';
 
 import { tour } from '../../../../data/tour';
@@ -63,7 +62,6 @@ export const MeetingView = ({
   const channelWatched = useWatchChannel({ chatClient, channelId: call.id });
 
   const { setSteps } = useTourContext();
-  const { chatPanelVisibility } = usePanelContext();
   const { addNotification } = useNotificationContext();
 
   const remoteScreenShare = useHasOngoingScreenShare();
@@ -171,6 +169,7 @@ export const MeetingView = ({
       }
       footer={
         <Footer
+          chatClient={chatClient}
           handleStartRecording={handleStartRecording}
           handleStopRecording={handleStopRecording}
           isAwaitingRecording={isAwaitingRecordingResponse}
