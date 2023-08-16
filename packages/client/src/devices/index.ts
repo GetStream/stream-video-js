2964eb16c405b7b7020ef9bfda81183f28e40b6b
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2023-08-16 19:17:17 +0200

    fix(client): export missing classes (#943)

--- packages/client/src/devices/index.ts
+++ packages/client/src/devices/index.ts
@@ -2,4 +2,6 @@ export * from './devices';
 export * from './InputMediaDeviceManager';
 export * from './InputMediaDeviceManagerState';
 export * from './CameraManager';
+export * from './CameraManagerState';
 export * from './MicrophoneManager';
+export * from './MicrophoneManagerState';
