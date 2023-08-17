d1c51b63823f1ad348f7e54f151800a6ebabf527
Khushal Agarwal <khushal.agarwal987@gmail.com>
2023-08-17 18:26:10 +0530

    fix(react-native): styles for Participant Grid and Layout mode

--- sample-apps/react-native/dogfood/src/components/ActiveCall.tsx
+++ sample-apps/react-native/dogfood/src/components/ActiveCall.tsx
@@ -7,7 +7,6 @@ import {
   CallControlsComponent,
   CallControlsComponentProps,
 } from './CallControlsComponent';
-import { appTheme } from '../theme';
 
 type ActiveCallProps = CallControlsComponentProps & {
   onBackPressed?: () => void;
@@ -44,6 +43,7 @@ export const ActiveCall = ({
   return (
     <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
       <CallContent
+        layout={'spotlight'}
         onBackPressed={onBackPressed}
         onParticipantInfoPress={onOpenCallParticipantsInfo}
         CallControls={CustomControlsComponent}
@@ -58,7 +58,6 @@ export const ActiveCall = ({
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: appTheme.colors.dark_gray,
     flex: 1,
   },
 });
