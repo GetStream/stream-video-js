8fa40732479b26c23054f3db6022675220e2b8f5
zwaardje <niels@seiton.io>
2023-08-25 21:46:51 +0200

    chore: rename chat send button
    - to keep in line with the rest

--- sample-apps/react/react-video-demo/src/components/ChatSendButton/ChatSendButton.tsx
+++ sample-apps/react/react-video-demo/src/components/ChatSendButton/ChatSendButton.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import { FC, ComponentProps } from 'react';
 import { ArrowRightIcon } from '../Icons';
 
 import type { Message } from 'stream-chat';
 
-type SendButtonProps = {
+export type Props = {
   sendMessage: (
     event: React.BaseSyntheticEvent,
     customMessageData?: Partial<Message>,
   ) => void;
-} & React.ComponentProps<'button'>;
+} & ComponentProps<'button'>;
 
-export const SendButton = ({ sendMessage, ...rest }: SendButtonProps) => {
+export const ChatSendButton: FC<Props> = ({ sendMessage, ...rest }) => {
   return (
     <div className="str-chat__send-button-container">
       <button
