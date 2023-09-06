e98b8f4129958da49bfb68e8f26649bfc9bf55fb
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-09-06 16:53:10 +0200

    fix: test failure
    fix: test failure

--- packages/react-native-sdk/expo-config-plugin/__tests__/withAndroidManifest.test.ts
+++ packages/react-native-sdk/expo-config-plugin/__tests__/withAndroidManifest.test.ts
@@ -1,5 +1,4 @@
-import { withAndroidManifest } from '@expo/config-plugins';
-import withStreamVideoReactNativeSDKManifest from '../src/withMainApplication';
+import withStreamVideoReactNativeSDKManifest from '../src/withAndroidManifest';
 import { ExpoConfig } from '@expo/config-types';
 import { AndroidConfig } from '@expo/config-plugins';
 import { getFixturePath } from '../fixtures';
@@ -15,37 +14,9 @@ const readAndroidManifestAsync =
 const getMainApplicationOrThrow =
   AndroidConfig.Manifest.getMainApplicationOrThrow;
 
-jest.mock('@expo/config-plugins', () => {
-  const originalModule = jest.requireActual('@expo/config-plugins');
-  return {
-    ...originalModule,
-    withMainApplication: jest.fn(),
-  };
-});
-
-jest.mock('@expo/config-plugins', () => {
-  const originalModule = jest.requireActual('@expo/config-plugins');
-  return {
-    ...originalModule,
-    withAndroidManifest: jest.fn(),
-  };
-});
-
 const sampleManifestPath = getFixturePath('AndroidManifest.xml');
 
 describe('withStreamVideoReactNativeSDKManifest', () => {
-  beforeEach(() => {
-    // Mock the behavior of withAndroidManifest
-    (withAndroidManifest as jest.Mock).mockImplementationOnce(
-      (config, callback) => {
-        const updatedConfig: CustomExpoConfig = callback(
-          config as CustomExpoConfig,
-        );
-        return updatedConfig;
-      },
-    );
-  });
-
   it('should modify Android Manifest', async () => {
     const manifest = await readAndroidManifestAsync(sampleManifestPath);
     // Prepare a mock config
@@ -61,13 +32,6 @@ describe('withStreamVideoReactNativeSDKManifest', () => {
 
     const mainApp = getMainApplicationOrThrow(updatedConfig.modResults);
 
-    // Assert that withAndroidManifest was called with the correct arguments
-    expect(withAndroidManifest).toHaveBeenCalledWith(
-      config,
-      expect.any(Function),
-    );
-
-    // Assert that the updated config contains the expected changes
     expect(
       mainApp.service?.some(
         (service) =>
