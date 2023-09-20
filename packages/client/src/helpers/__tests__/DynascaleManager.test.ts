GetStream/stream-video-js

commit 9efd84cb77b98c372917e6bfa36161763969dddd

fix: hide the video element when a placeholder is visible (#1094)
Applies `display: none` to the `<video>` element when the participant's
video isn't playing and a placeholder is visible.
Enables building nicer user experience in designs that Video Placeholder
that utilize transparency.

Styling is done through `str-video__video--no-video` CSS selector that
allows an integrator to further customize the behavior and attach more
custom styles.

### Implementation notes
The `<video>` element needs to stay mounted as we need to be notified
about it's `play` or `pause` events that may occur in an uncontrolled
fashion in poor network conditions (e.g.: huge packet loss, the browser
may pause the video, and once the network is stable enough, will resume
playback).

packages/react-sdk/src/core/components/Video/Video.tsx
@@ -62,20 +62,18 @@ export const Video = ({
   const [isWideMode, setIsWideMode] = useState(true);
 
   const stream =
-    trackType === 'none'
-      ? undefined
-      : trackType === 'videoTrack'
+    trackType === 'videoTrack'
       ? videoStream
-      : screenShareStream;
+      : trackType === 'screenShareTrack'
+      ? screenShareStream
+      : undefined;
 
   const isPublishingTrack =
-    trackType === 'none'
-      ? false
-      : publishedTracks.includes(
-          trackType === 'videoTrack'
-            ? SfuModels.TrackType.VIDEO
-            : SfuModels.TrackType.SCREEN_SHARE,
-        );
+    trackType === 'videoTrack'
+      ? publishedTracks.includes(SfuModels.TrackType.VIDEO)
+      : trackType === 'screenShareTrack'
+      ? publishedTracks.includes(SfuModels.TrackType.SCREEN_SHARE)
+      : false;
 
   const isInvisible =
     trackType === 'none' ||
@@ -118,14 +116,15 @@ export const Video = ({
 
   if (!call) return null;
 
+  const mirrorVideo = isLocalParticipant && trackType === 'videoTrack';
   return (
     <>
       <video
         {...rest}
         className={clsx(className, 'str-video__video', {
+          'str-video__video--no-video': displayPlaceholder,
           'str-video__video--tall': !isWideMode,
-          'str-video__video--mirror':
-            isLocalParticipant && trackType === 'videoTrack',
+          'str-video__video--mirror': mirrorVideo,
           'str-video__video--screen-share': trackType === 'screenShareTrack',
         })}
         data-user-id={userId}

packages/styling/src/Video/Video-layout.scss
@@ -12,4 +12,8 @@
   &--mirror {
     transform: scaleX(-1);
   }
+
+  &--no-video {
+    display: none;
+  }
 }

packages/react-sdk/src/core/components/Video/DefaultVideoPlaceholder.tsx
@@ -10,9 +10,7 @@ export const DefaultVideoPlaceholder = forwardRef<
   VideoPlaceholderProps
 >(({ participant, style }, ref) => {
   const [error, setError] = useState(false);
-
-  const name = participant?.name || participant?.userId;
-
+  const name = participant.name || participant.userId;
   return (
     <div className="str-video__video-placeholder" style={style} ref={ref}>
       {(!participant.image || error) &&

commit bd01835f4e93c981ca2e5a7e4e09142ea4e326cf

fix: unmount video element when there is no video track or participant is invisible (#1096)
Fixes a regression introduced in #1094.

`display: none` caused an unexpected side-effect where the dimensions of
the tracked video element are reported as `0x0`.
This case wasn't properly handled and led to a situation where the SFU
was assigning the lowest video track quality, although this should be
considered as "unsubscribing" from the participant's video as there
isn't enough screen space where the video track can be played.

Now, instead of visually hiding the video element when there is nothing
to be played, or the participant is invisible, we are completely
removing it from the DOM. We do the same for the audio elements as well.

### Implementation notes
We are now replacing the previously introduced
`str-video__video--no-video` class with `str-video__video--not-playing`
that doesn't have any rules attached to it. We expect this to be the
main extension point for our integrators.

---------

Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
Co-authored-by: Anton Arnautov <arnautov.anton@gmail.com>

packages/client/src/helpers/DynascaleManager.ts
@@ -152,6 +152,14 @@ export class DynascaleManager {
       debounceType: DebounceType,
       dimension: VideoDimension | undefined,
     ) => {
+      if (dimension && (dimension.width === 0 || dimension.height === 0)) {
+        // ignore 0x0 dimensions. this can happen when the video element
+        // is not visible (e.g., has display: none).
+        // we treat this as "unsubscription" as we don't want to keep
+        // consuming bandwidth for a video that is not visible on the screen.
+        this.logger('debug', `Ignoring 0x0 dimension`, boundParticipant);
+        dimension = undefined;
+      }
       this.call.updateSubscriptionsPartial(
         trackType,
         { [sessionId]: { dimension } },
@@ -171,6 +179,12 @@ export class DynascaleManager {
       shareReplay(1),
     );
 
+    /**
+     * Since the video elements are now being removed from the DOM (React SDK) upon
+     * visibility change, this subscription is not in use an stays here only for the
+     * plain JS integrations where integrators might choose not to remove the video
+     * elements from the DOM.
+     */
     // keep copy for resize observer handler
     let viewportVisibilityState: VisibilityState | undefined;
     const viewportVisibilityStateSubscription =
@@ -231,6 +245,8 @@ export class DynascaleManager {
         });
     resizeObserver?.observe(videoElement);
 
+    // element renders and gets bound - track subscription gets
+    // triggered first other ones get skipped on initial subscriptions
     const publishedTracksSubscription = boundParticipant.isLocalParticipant
       ? null
       : participant$
@@ -248,13 +264,13 @@ export class DynascaleManager {
           .subscribe((isPublishing) => {
             if (isPublishing) {
               // the participant just started to publish a track
-              requestTrackWithDimensions(DebounceType.IMMEDIATE, {
+              requestTrackWithDimensions(DebounceType.FAST, {
                 width: videoElement.clientWidth,
                 height: videoElement.clientHeight,
               });
             } else {
               // the participant just stopped publishing a track
-              requestTrackWithDimensions(DebounceType.IMMEDIATE, undefined);
+              requestTrackWithDimensions(DebounceType.FAST, undefined);
             }
           });
 
@@ -286,7 +302,7 @@ export class DynascaleManager {
     videoElement.muted = true;
 
     return () => {
-      requestTrackWithDimensions(DebounceType.IMMEDIATE, undefined);
+      requestTrackWithDimensions(DebounceType.FAST, undefined);
       viewportVisibilityStateSubscription?.unsubscribe();
       publishedTracksSubscription?.unsubscribe();
       streamSubscription.unsubscribe();

packages/react-sdk/src/core/components/ParticipantView/ParticipantView.tsx
@@ -163,7 +163,7 @@ export const ParticipantView = forwardRef<HTMLDivElement, ParticipantViewProps>(
       >
         <ParticipantViewContext.Provider value={participantViewContextValue}>
           {/* mute the local participant, as we don't want to hear ourselves */}
-          {!isLocalParticipant && !muteAudio && (
+          {!isLocalParticipant && !muteAudio && hasAudio && (
             <Audio participant={participant} />
           )}
           <Video

packages/react-sdk/src/core/components/Video/Video.tsx
@@ -2,6 +2,7 @@ import {
   ComponentPropsWithoutRef,
   ComponentType,
   useEffect,
+  useLayoutEffect,
   useState,
 } from 'react';
 import {
@@ -58,7 +59,8 @@ export const Video = ({
   const [videoElement, setVideoElement] = useState<HTMLVideoElement | null>(
     null,
   );
-  const [videoPlaying, setVideoPlaying] = useState(false);
+  // start with true, will flip once the video starts playing
+  const [isVideoPaused, setIsVideoPaused] = useState(true);
   const [isWideMode, setIsWideMode] = useState(true);
 
   const stream =
@@ -68,20 +70,7 @@ export const Video = ({
       ? screenShareStream
       : undefined;
 
-  const isPublishingTrack =
-    trackType === 'videoTrack'
-      ? publishedTracks.includes(SfuModels.TrackType.VIDEO)
-      : trackType === 'screenShareTrack'
-      ? publishedTracks.includes(SfuModels.TrackType.SCREEN_SHARE)
-      : false;
-
-  const isInvisible =
-    trackType === 'none' ||
-    viewportVisibilityState?.[trackType] === VisibilityState.INVISIBLE;
-
-  const displayPlaceholder = !isPublishingTrack || isInvisible || !videoPlaying;
-
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!call || !videoElement || trackType === 'none') return;
 
     const cleanup = call.bindVideoElement(videoElement, sessionId, trackType);
@@ -98,7 +87,7 @@ export const Video = ({
     if (!track) return;
 
     const handlePlayPause = () => {
-      setVideoPlaying(!videoElement.paused);
+      setIsVideoPaused(videoElement.paused);
 
       const { width = 0, height = 0 } = track.getSettings();
       setIsWideMode(width >= height);
@@ -116,25 +105,41 @@ export const Video = ({
 
   if (!call) return null;
 
+  const isPublishingTrack =
+    trackType === 'videoTrack'
+      ? publishedTracks.includes(SfuModels.TrackType.VIDEO)
+      : trackType === 'screenShareTrack'
+      ? publishedTracks.includes(SfuModels.TrackType.SCREEN_SHARE)
+      : false;
+
+  const isInvisible =
+    trackType === 'none' ||
+    viewportVisibilityState?.[trackType] === VisibilityState.INVISIBLE;
+
+  const hasNoVideoOrInvisible = !isPublishingTrack || isInvisible;
   const mirrorVideo = isLocalParticipant && trackType === 'videoTrack';
+  const isScreenShareTrack = trackType === 'screenShareTrack';
   return (
     <>
-      <video
-        {...rest}
-        className={clsx(className, 'str-video__video', {
-          'str-video__video--no-video': displayPlaceholder,
-          'str-video__video--tall': !isWideMode,
-          'str-video__video--mirror': mirrorVideo,
-          'str-video__video--screen-share': trackType === 'screenShareTrack',
-        })}
-        data-user-id={userId}
-        data-session-id={sessionId}
-        ref={(element) => {
-          setVideoElement(element);
-          refs?.setVideoElement?.(element);
-        }}
-      />
-      {displayPlaceholder && (
+      {!hasNoVideoOrInvisible && (
+        <video
+          {...rest}
+          className={clsx(className, 'str-video__video', {
+            'str-video__video--not-playing': isVideoPaused,
+            'str-video__video--tall': !isWideMode,
+            'str-video__video--mirror': mirrorVideo,
+            'str-video__video--screen-share': isScreenShareTrack,
+          })}
+          data-user-id={userId}
+          data-session-id={sessionId}
+          ref={(element) => {
+            setVideoElement(element);
+            refs?.setVideoElement?.(element);
+          }}
+        />
+      )}
+      {/* TODO: add condition to "hold" the placeholder until track unmutes as well */}
+      {(hasNoVideoOrInvisible || isVideoPaused) && (
         <VideoPlaceholder
           style={{ position: 'absolute' }}
           participant={participant}

packages/styling/src/Video/Video-layout.scss
@@ -12,8 +12,4 @@
   &--mirror {
     transform: scaleX(-1);
   }
-
-  &--no-video {
-    display: none;
-  }
 }

packages/client/src/__tests__/server-side/call.test.ts
@@ -120,9 +120,9 @@ describe('call API', () => {
       response = await call.update({
         settings_override: {
           recording: {
-            mode: RecordSettingsRequestModeEnum.AVAILABLE,
             audio_only: false,
             quality: RecordSettingsRequestQualityEnum._1080P,
+            mode: RecordSettingsRequestModeEnum.AUTO_ON,
           },
         },
       });

packages/client/src/helpers/__tests__/DynascaleManager.test.ts
@@ -4,7 +4,7 @@
 
 import '../../rtc/__tests__/mocks/webrtc.mocks';
 
-import { Mock, afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, Mock, vi } from 'vitest';
 import { DynascaleManager } from '../DynascaleManager';
 import { Call } from '../../Call';
 import { StreamClient } from '../../coordinator/connection/client';
@@ -195,7 +195,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
 
       call.state.updateParticipant('session-id', {
@@ -212,7 +212,7 @@ describe('DynascaleManager', () => {
             },
           },
         },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
 
       call.state.updateParticipant('session-id', {
@@ -222,7 +222,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
 
       cleanup?.();
@@ -230,7 +230,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
     });
 
@@ -271,7 +271,7 @@ describe('DynascaleManager', () => {
             },
           },
         },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
       expect(play).toHaveBeenCalled();
       expect(videoElement.srcObject).toBe(mediaStream);
@@ -281,7 +281,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
     });
 
@@ -308,7 +308,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
 
       call.state.updateParticipant('session-id', {
@@ -369,7 +369,7 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
     });
 
@@ -417,7 +417,7 @@ describe('DynascaleManager', () => {
             },
           },
         },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
       );
 
       // @ts-ignore simulate resize
@@ -439,7 +439,47 @@ describe('DynascaleManager', () => {
       expect(updateSubscription).toHaveBeenCalledWith(
         'videoTrack',
         { 'session-id': { dimension: undefined } },
-        DebounceType.IMMEDIATE,
+        DebounceType.FAST,
+      );
+    });
+
+    it('video: should unsubscribe when element dimensions are zero', () => {
+      // @ts-ignore
+      call.state.updateOrAddParticipant('session-id', {
+        userId: 'user-id',
+        sessionId: 'session-id',
+        publishedTracks: [TrackType.VIDEO],
+        viewportVisibilityState: {
+          videoTrack: VisibilityState.VISIBLE,
+          screenShareTrack: VisibilityState.UNKNOWN,
+        },
+      });
+
+      let updateSubscription = vi.spyOn(call, 'updateSubscriptionsPartial');
+
+      // @ts-ignore simulate resize
+      videoElement.clientHeight = 0;
+      // @ts-ignore simulate resize
+      videoElement.clientWidth = 0;
+
+      const cleanup = dynascaleManager.bindVideoElement(
+        videoElement,
+        'session-id',
+        'videoTrack',
+      );
+
+      expect(updateSubscription).toHaveBeenCalledWith(
+        'videoTrack',
+        { 'session-id': { dimension: undefined } },
+        DebounceType.FAST,
+      );
+
+      cleanup?.();
+
+      expect(updateSubscription).toHaveBeenLastCalledWith(
+        'videoTrack',
+        { 'session-id': { dimension: undefined } },
+        DebounceType.FAST,
       );
     });
   });