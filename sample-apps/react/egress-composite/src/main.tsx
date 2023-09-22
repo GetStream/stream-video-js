a98a129ccd85252ddefeda1553d1075fa5f61202
Anton Arnautov <arnautov.anton@gmail.com>
2023-09-22 18:24:30 +0200

    New options for testing purposes

--- sample-apps/react/egress-composite/src/main.tsx
+++ sample-apps/react/egress-composite/src/main.tsx
@@ -20,16 +20,29 @@ window.setupLayout = (configuration: ConfigurationValue) => {
   );
 };
 
-// (() => {
-//   const v = document.createElement('script');
-//   v.innerHTML = `window.setupLayout({
-//     call_id: "<call_id>",
-//     layout: "grid",
-//     screenshare_layout: "spotlight",
-//     ext_css: "https://github.githubassets.com/assets/light-983b05c0927a.css",
-//     options: {
-//       "logo.image_url": "https://theme.zdassets.com/theme_assets/9442057/efc3820e436f9150bc8cf34267fff4df052a1f9c.png",
-//     }
-//   });`;
-//   document.head.appendChild(v);
-// })();
+/**
+(() => {
+  const v = document.createElement('script');
+  v.innerHTML = `window.setupLayout(${JSON.stringify({
+    call_id: '<call_id>',
+    layout: 'grid',
+    screenshare_layout: 'spotlight',
+    ext_css: 'https://github.githubassets.com/assets/light-983b05c0927a.css',
+    options: {
+      'title.text': 'Hey Streamers!',
+      'logo.image_url':
+        'https://theme.zdassets.com/theme_assets/9442057/efc3820e436f9150bc8cf34267fff4df052a1f9c.png',
+      'layout.background_color': 'red',
+      'video.background_color': 'green',
+      'video.scale_mode': 'contain',
+      'video.screenshare_scale_mode': 'contain',
+      'participant_label.border_color': '#fff',
+      'participant_label.border_width': '3px',
+      'participant_label.border_radius': 'px',
+      'participant_label.background_color': '#ddd',
+      'participant_label.text_color': 'darkblue',
+    },
+  } satisfies Partial<ConfigurationValue>)});`;
+  document.head.appendChild(v);
+})();
+*/
