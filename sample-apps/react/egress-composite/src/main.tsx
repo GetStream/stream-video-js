a74abdd884a80911e0574e7aeec39def7d319ae8
Anton Arnautov <arnautov.anton@gmail.com>
2023-10-02 23:27:56 +0200

    Namespace Sentry tokens, adjust Egress config

--- sample-apps/react/egress-composite/src/main.tsx
+++ sample-apps/react/egress-composite/src/main.tsx
@@ -9,18 +9,21 @@ import {
 import { CompositeApp } from './CompositeApp';
 
 import '@stream-io/video-react-sdk/dist/css/styles.css';
-
 // Uncomment this line to test your own custom CSS
 // import cssUrl from '../public/example/custom.css?url';
 
-Sentry.init({
-  dsn: import.meta.env.VITE_EGRESS_SENTRY_DNS,
-  tracePropagationTargets: ['video-layout.getstream.io'],
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (import.meta.env.MODE === 'production') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_EGRESS_SENTRY_DNS,
+    tracePropagationTargets: ['video-layout.getstream.io'],
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    tracesSampleRate: 0.9,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    attachStacktrace: true,
+    maxBreadcrumbs: 10,
+  });
+}
 
 // @ts-expect-error TODO: this is a global function, we need to declare it
 window.setupLayout = (configuration: ConfigurationValue) => {
--- sample-apps/react/egress-composite/vite.config.ts
+++ sample-apps/react/egress-composite/vite.config.ts
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-
   plugins: [
     react(),
     sentryVitePlugin({
       org: 'stream',
       project: 'video-composite-layout-app',
+      authToken: process.env.EGRESS_SENTRY_AUTH_TOKEN,
+      disable: process.env.MODE !== 'production',
     }),
   ],
-
   build: {
     sourcemap: true,
   },
