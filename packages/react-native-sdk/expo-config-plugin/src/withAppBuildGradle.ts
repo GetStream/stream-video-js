bba3f8437cb0f7a662adef4e89fbc487225a5ed5
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-10-13 17:34:42 +0200

    fix(react-native): misc expo config plugin bugs

--- packages/react-native-sdk/expo-config-plugin/src/withAppBuildGradle.ts
+++ packages/react-native-sdk/expo-config-plugin/src/withAppBuildGradle.ts
@@ -12,9 +12,6 @@ android {
     compileOptions {
         sourceCompatibility JavaVersion.VERSION_1_8
         targetCompatibility JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }`,
     );
 
