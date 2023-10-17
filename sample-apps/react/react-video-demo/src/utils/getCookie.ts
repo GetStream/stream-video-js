7ed30c1f8ac45cd35342898daff57ba2f9d21c94
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-10-17 16:29:16 +0200

    feat: update eslint fix issues

--- sample-apps/react/react-video-demo/src/utils/getCookie.ts
+++ sample-apps/react/react-video-demo/src/utils/getCookie.ts
@@ -1,18 +1,18 @@
 export const getCookie = (name: string) => {
-  let cookieValue: string | null = null
-  if (typeof document === "undefined") {
-    return null
+  let cookieValue: string | null = null;
+  if (typeof document === 'undefined') {
+    return null;
   }
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";")
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim()
+      const cookie = cookies[i].trim();
       // Does this cookie string begin with the name we want?
       if (cookie.substring(0, name.length + 1) === `${name}=`) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
-        break
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
     }
   }
-  return cookieValue
-}
+  return cookieValue;
+};
