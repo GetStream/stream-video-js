412942a6d8ac1ad9f5595c60dde288e949f532e9
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-10-25 16:38:09 +0200

    fix: speaking while muted

--- packages/react-sdk/src/components/Notification/SpeakingWhileMutedNotification.tsx
+++ packages/react-sdk/src/components/Notification/SpeakingWhileMutedNotification.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import { useCallStateHooks, useI18n } from '@stream-io/video-react-bindings';
 import { Notification } from './Notification';
 
@@ -7,42 +7,19 @@ export type SpeakingWhileMutedNotificationProps = {
    * Text message displayed by the notification.
    */
   text?: string;
-
-  /**
-   * Duration in milliseconds for which the notification should be displayed.
-   * Default is 3500ms.
-   */
-  displayDuration?: number;
 };
 
 export const SpeakingWhileMutedNotification = ({
   children,
   text,
-  displayDuration = 3500,
 }: PropsWithChildren<SpeakingWhileMutedNotificationProps>) => {
   const { useMicrophoneState } = useCallStateHooks();
   const { isSpeakingWhileMuted } = useMicrophoneState();
   const { t } = useI18n();
 
-  const [showNotification, setShowNotification] = useState(false);
-  if (!showNotification && isSpeakingWhileMuted) {
-    setShowNotification(true);
-  }
-
-  useEffect(() => {
-    if (!showNotification) return;
-    const timeout = setTimeout(() => {
-      setShowNotification(false);
-    }, displayDuration);
-    return () => {
-      clearTimeout(timeout);
-      setShowNotification(false);
-    };
-  }, [showNotification, displayDuration]);
-
   const message = text ?? t('You are muted. Unmute to speak.');
   return (
-    <Notification message={message} isVisible={showNotification}>
+    <Notification message={message} isVisible={isSpeakingWhileMuted}>
       {children}
     </Notification>
   );
