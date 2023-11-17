098a7258e63a46be5ee495510ba7c2275981f166
zwaardje <niels@seiton.io>
2023-11-17 10:38:15 +0100

    chore: typscript issue
    WIP still need to look into the “any”

--- packages/react-sdk/src/components/Button/CompositeButton.tsx
+++ packages/react-sdk/src/components/Button/CompositeButton.tsx
@@ -12,44 +12,63 @@ export type IconButtonWithMenuProps = PropsWithChildren<{
   Menu?: ComponentType | JSX.Element;
   caption?: string;
   menuPlacement?: Placement;
+  ToggleMenuButton?: any;
 }>;
 
 export const CompositeButton = forwardRef<
   HTMLDivElement,
   IconButtonWithMenuProps
->(({ caption, children, active, Menu, menuPlacement }, ref) => {
-  return (
-    <div className="str-video__composite-button" ref={ref}>
-      <div
-        className={clsx('str-video__composite-button__button-group', {
-          'str-video__composite-button__button-group--active': active,
-        })}
-      >
-        {children}
-        {Menu && (
-          <MenuToggle placement={menuPlacement} ToggleButton={ToggleMenuButton}>
-            {isComponentType(Menu) ? <Menu /> : Menu}
-          </MenuToggle>
+>(
+  (
+    {
+      caption,
+      children,
+      active,
+      Menu,
+      menuPlacement,
+      ToggleMenuButton = DefaultToggleMenuButton,
+    },
+    ref,
+  ) => {
+    return (
+      <div className="str-video__composite-button" ref={ref}>
+        <div
+          className={clsx('str-video__composite-button__button-group', {
+            'str-video__composite-button__button-group--active': active,
+          })}
+        >
+          {children}
+          {Menu && (
+            <MenuToggle
+              placement={menuPlacement}
+              ToggleButton={ToggleMenuButton}
+            >
+              {isComponentType(Menu) ? <Menu /> : Menu}
+            </MenuToggle>
+          )}
+        </div>
+        {caption && (
+          <div className="str-video__composite-button__caption">{caption}</div>
         )}
       </div>
-      {caption && (
-        <div className="str-video__composite-button__caption">{caption}</div>
-      )}
-    </div>
-  );
-});
-
-const ToggleMenuButton = forwardRef<HTMLButtonElement, ToggleMenuButtonProps>(
-  ({ menuShown }, ref) => {
-    const { t } = useI18n();
-
-    return (
-      <IconButton
-        className={'str-video__menu-toggle-button'}
-        icon={menuShown ? 'caret-down' : 'caret-up'}
-        title={t('Toggle device menu')}
-        ref={ref}
-      />
     );
   },
 );
+
+const DefaultToggleMenuButton = forwardRef<
+  HTMLButtonElement,
+  ToggleMenuButtonProps
+>(({ menuShown }, ref) => {
+  const { t } = useI18n();
+
+  return (
+    <IconButton
+      className={clsx('str-video__menu-toggle-button', {
+        'str-video__menu-toggle-button--active': menuShown,
+      })}
+      icon={menuShown ? 'caret-down' : 'caret-up'}
+      title={t('Toggle device menu')}
+      ref={ref}
+    />
+  );
+});
