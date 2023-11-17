ce1f2d4ca05c70e9864561098400a683f71da3d4
zwaardje <niels@seiton.io>
2023-11-17 10:14:21 +0100

    feat: update language menu with dropdown select
    - Change language selection to dropdown select
    - Remove material ui from component
    - Still WIP

--- sample-apps/react/react-dogfood/components/Settings/LanguageMenu.tsx
+++ sample-apps/react/react-dogfood/components/Settings/LanguageMenu.tsx
@@ -1,13 +1,12 @@
+import { useCallback } from 'react';
+
 import { SettingsController } from './SettingsDialog';
-import { TranslationLanguage } from '@stream-io/video-react-sdk';
 import { Settings } from '../../context/SettingsContext';
+
 import {
-  FormControl,
-  MenuItem,
-  Select,
-  Stack,
-  Typography,
-} from '@mui/material';
+  TranslationLanguage,
+  DropDownSelect,
+} from '@stream-io/video-react-sdk';
 
 const LANGUAGES: Record<TranslationLanguage, string> = {
   de: 'German',
@@ -19,27 +18,21 @@ export type LanguageMenuProps = Pick<SettingsController, 'setLanguage'> &
   Pick<Settings, 'language'>;
 
 export const LanguageMenu = ({ language, setLanguage }: LanguageMenuProps) => {
+  const handleSelect = useCallback((index: number) => {
+    const selected = Object.keys(LANGUAGES)[index];
+    setLanguage(selected);
+  }, []);
+
   return (
-    <Stack>
-      <Typography variant="caption" color="#72767e">
-        Language of the Meeting
-      </Typography>
-      <FormControl fullWidth>
-        <Select
-          value={language?.split('-')[0]}
-          label="Language"
-          onChange={(e) => {
-            setLanguage(e.target.value);
-          }}
-          sx={{ fontSize: '0.875rem' }}
-        >
-          {Object.entries(LANGUAGES).map(([lngCode, languageName]) => (
-            <MenuItem key={`settings-language-${lngCode}`} value={lngCode}>
-              {languageName}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Stack>
+    <DropDownSelect
+      icon="language"
+      defaultSelectedIndex={0}
+      defaultSelectedLabel="English"
+      handleSelect={handleSelect}
+    >
+      {Object.entries(LANGUAGES).map(([lngCode, languageName]) => (
+        <div key={`settings-language-${lngCode}`}>{languageName}</div>
+      ))}
+    </DropDownSelect>
   );
 };
