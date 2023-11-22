2671ccf77bee1a92387196f92a739fb8dc985051
zwaardje <niels@seiton.io>
2023-11-22 15:03:27 +0100

    feat: update home screen

--- sample-apps/react/react-dogfood/pages/index.tsx
+++ sample-apps/react/react-dogfood/pages/index.tsx
@@ -8,23 +8,14 @@ import {
   useState,
 } from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
-import PhotoCameraFrontIcon from '@mui/icons-material/PhotoCameraFront';
-import { Box, Button, Stack, Typography } from '@mui/material';
-import { StreamI18nProvider, useI18n } from '@stream-io/video-react-sdk';
 
-import { LobbyHeader } from '../components/LobbyHeader';
+import { StreamI18nProvider, useI18n, Icon } from '@stream-io/video-react-sdk';
 
 import { meetingId } from '../lib/meetingId';
 import translations from '../translations';
 import { useSettings } from '../context/SettingsContext';
-import { Countdown } from '../components/Countdown';
 
-type HomeProps = {
-  launchDeadlineTimestamp: number;
-};
-
-export default function Home({ launchDeadlineTimestamp }: HomeProps) {
+export default function Home() {
   const { data: session, status } = useSession();
   const {
     settings: { language },
@@ -45,43 +36,18 @@ export default function Home({ launchDeadlineTimestamp }: HomeProps) {
       translationsOverrides={translations}
       language={language}
     >
-      <HomeContent launchDeadlineTimestamp={launchDeadlineTimestamp} />
+      <HomeContent />
     </StreamI18nProvider>
   );
 }
 
-const HomeContent = ({ launchDeadlineTimestamp }: HomeProps) => {
-  const { t } = useI18n();
-
-  return (
-    <>
-      <LobbyHeader />
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-        flexGrow={1}
-      >
-        <Stack spacing={2} alignItems="center" flexGrow={1}>
-          <Box padding={2}>
-            <Typography variant="h2" textAlign="center">
-              {t('Stream Meetings')}
-            </Typography>
-          </Box>
-          <JoinCallForm />
-        </Stack>
-      </Stack>
-      <Countdown deadlineTimestamp={launchDeadlineTimestamp} />
-    </>
-  );
-};
-
-const JoinCallForm = () => {
+const HomeContent = () => {
   const { t } = useI18n();
   const router = useRouter();
   const ref = useRef<HTMLInputElement | null>(null);
+
   const [disabled, setDisabled] = useState(true);
+
   const onJoin = useCallback(() => {
     router.push(`join/${ref.current!.value}`);
   }, [ref, router]);
@@ -104,55 +70,63 @@ const JoinCallForm = () => {
     [onJoin, disabled],
   );
   return (
-    <div
-      style={{
-        maxWidth: '300px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-      }}
-    >
-      {disabled ? (
-        <Link href={`/join/${meetingId()}`} legacyBehavior>
-          <Button
-            data-testid="create-and-join-meeting-button"
-            variant="contained"
-            fullWidth
+    <div className="rd__home">
+      <div className="rd__home-content">
+        <img className="rd__home-image" src="/home.png" alt="Home" />
+        <h1 className="rd__home-heading">
+          {t('Stream')}
+          <span>{t('[Video Calling]')}</span>
+          {t('Demo')}
+        </h1>
+        <p className="rd__home-description">
+          {t(
+            'Start a new call, join a meeting by entering the call ID or by scanning a QR code.',
+          )}
+        </p>
+        <div className="rd__home-join">
+          <input
+            className="rd__input rd__home-input"
+            data-testid="join-call-input"
+            ref={ref}
+            onChange={handleChange}
+            onKeyUp={handleKeyUp}
+            placeholder={t('Call ID')}
+          />
+          <button
+            className="rd__button rd__button--primary rd__button__join"
+            data-testid="join-call-button"
+            disabled={disabled}
+            onClick={onJoin}
           >
-            <PhotoCameraFrontIcon sx={{ mr: 1 }} />
-            {t('New meeting')}
-          </Button>
-        </Link>
-      ) : (
-        <Button
+            <Icon className="rd__button__icon" icon="grid" />
+            {t('Join call')}
+          </button>
+        </div>
+        <a
+          href={`/join/${meetingId()}`}
+          className="rd__link rd__link--faux-button"
+        >
+          <Icon className="rd__link__icon" icon="grid" />
+          {t('Start a new call')}
+        </a>
+        <button
+          className="rd__button"
           data-testid="join-call-button"
-          variant="contained"
+          disabled={true}
           onClick={onJoin}
         >
-          {t('Join')}
-        </Button>
-      )}
-      <input
-        className="rd__input rd__input--underlined rd__join-call-input"
-        data-testid="join-call-input"
-        ref={ref}
-        onChange={handleChange}
-        onKeyUp={handleKeyUp}
-        placeholder={t('Or join a call with code')}
-      />
+          <Icon className="rd__button__icon" icon="grid" />
+          {t('Scan QR code')}
+        </button>
+      </div>
     </div>
   );
 };
 
 export const getServerSideProps = async () => {
-  const launchDeadlineTimestamp = new Date(
-    new Date(process.env.LAUNCH_DEADLINE || '2023-06-01T00:00:00Z').getTime() +
-      12 * 3600 * 1000,
-  ).getTime();
-
   return {
     props: {
-      launchDeadlineTimestamp,
-    } as HomeProps,
+      title: 'a',
+    },
   };
 };
