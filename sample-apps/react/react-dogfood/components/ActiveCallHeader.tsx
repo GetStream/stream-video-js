a68c7f145efdf3ed028cac9558167ae80a8dccc8
zwaardje <niels@seiton.io>
2023-11-22 14:54:24 +0100

    feat: add latency indicator and elapsed time

--- sample-apps/react/react-dogfood/components/ActiveCallHeader.tsx
+++ sample-apps/react/react-dogfood/components/ActiveCallHeader.tsx
@@ -1,22 +1,76 @@
+import { useEffect, useState } from 'react';
 import {
   CallingState,
   CancelCallButton,
   LoadingIndicator,
   Notification,
   useCallStateHooks,
+  Icon,
 } from '@stream-io/video-react-sdk';
+import clsx from 'clsx';
+
+import { differenceInSeconds } from 'date-fns';
 
 import { CallHeaderTitle } from './CallHeaderTitle';
-import { ToggleSettingsTabModal } from './Settings/SettingsTabModal';
 
 import { LayoutSelectorProps } from './LayoutSelector';
 
+export const LatencyIndicator = () => {
+  const { useCallStatsReport } = useCallStateHooks();
+  const statsReport = useCallStatsReport();
+  const latency = statsReport?.publisherStats?.averageRoundTripTimeInMs ?? 0;
+
+  return (
+    <div className="rd__header__latency">
+      <div
+        className={clsx('rd__header__latency-indicator', {
+          'rd__header__latency-indicator--good': latency && latency <= 100,
+          'rd__header__latency-indicator--ok':
+            latency && latency > 100 && latency < 150,
+          'rd__header__latency-indicator--bad': latency && latency > 150,
+        })}
+      ></div>
+      {latency} mps
+    </div>
+  );
+};
+
+export const Elapsed = ({
+  joinedAt,
+}: {
+  className?: string;
+  joinedAt: number;
+}) => {
+  const [elapsed, setElapsed] = useState<string>();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const elapsedSeconds = differenceInSeconds(
+        Date.now(),
+        new Date(joinedAt * 1000),
+      );
+
+      const date = new Date(0);
+      date.setSeconds(elapsedSeconds);
+      const format = date.toISOString().substring(14, 19);
+
+      setElapsed(format);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [joinedAt]);
+
+  return (
+    <div className="rd__header__elapsed">
+      <Icon className="rd__header__elapsed-icon" icon="verified" />
+      <div className="rd__header__elapsed-time">{elapsed}</div>
+    </div>
+  );
+};
+
 export const ActiveCallHeader = ({
-  selectedLayout,
-  onMenuItemClick: setLayout,
   onLeave,
 }: { onLeave: () => void } & LayoutSelectorProps) => {
-  const { useCallCallingState } = useCallStateHooks();
+  const { useCallCallingState, useParticipants } = useCallStateHooks();
   const callingState = useCallCallingState();
   const isOffline = callingState === CallingState.OFFLINE;
   const isMigrating = callingState === CallingState.MIGRATING;
@@ -24,23 +78,20 @@ export const ActiveCallHeader = ({
   const isReconnecting = callingState === CallingState.RECONNECTING;
   const hasFailedToRecover = callingState === CallingState.RECONNECTING_FAILED;
 
+  const participants = useParticipants();
+  const me = participants?.find((p) => p.isLocalParticipant);
+
   return (
     <>
-      <div className="str-video__call-header">
+      <div className="rd__call-header">
         <CallHeaderTitle />
-        <div className="str-video__call-header__controls-group">
+        <div className="rd__call-header__controls-group">
+          <Elapsed joinedAt={Number(me?.joinedAt?.seconds) || Date.now()} />
+          <LatencyIndicator />
           <CancelCallButton onLeave={onLeave} />
-
-          <ToggleSettingsTabModal
-            selectedLayout={selectedLayout}
-            onMenuItemClick={setLayout}
-            close={() => {
-              console.log('Closing Settings Modal');
-            }}
-          />
         </div>
       </div>
-      <div className="str-video__call-header__notifications">
+      <div className="rd__call-header__notifications">
         {(() => {
           if (isOffline || hasFailedToRecover) {
             return (
