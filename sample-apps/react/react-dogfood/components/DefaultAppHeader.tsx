10720e0e0a8ae0576d9d81211d99e5945f92760c
zwaardje <niels@seiton.io>
2023-11-30 17:42:38 +0100

    feat: add docs to header

--- sample-apps/react/react-dogfood/components/DefaultAppHeader.tsx
+++ sample-apps/react/react-dogfood/components/DefaultAppHeader.tsx
@@ -1,12 +1,33 @@
 import { HomeButton, UserInfo } from './LobbyHeader';
 
+import { Icon, useI18n } from '@stream-io/video-react-sdk';
+
 export const DefaultAppHeader = () => {
+  const { t } = useI18n();
   return (
-    <div className="rd__call-header rd__call-header--transparent">
+    <div className="rd__call-header rd__call-header">
       <div className="rd__call-header__title-group">
         <HomeButton />
+        <UserInfo />
       </div>
-      <UserInfo />
+      <div className="rd__call-header__documentation">
+        <a
+          className="rd__button rd__button"
+          href="https://getstream.io/video/get_started/"
+          target="_blank"
+        >
+          <Icon className="rd__button__icon" icon="mediation" />
+          {t('Tutorials')}
+        </a>
+        <a
+          className="rd__button"
+          href="https://getstream.io/video/docs/"
+          target="_blank"
+        >
+          <Icon className="rd__button__icon" icon="folder" />
+          {t('Documentation')}
+        </a>
+      </div>
     </div>
   );
 };
