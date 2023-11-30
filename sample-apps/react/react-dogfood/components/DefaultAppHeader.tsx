eea022c89030fe939d620c1ca8bde206a8a37ec7
zwaardje <niels@seiton.io>
2023-11-30 09:48:59 +0100

    feat: add transparent header for lobby and home screen

--- sample-apps/react/react-dogfood/components/DefaultAppHeader.tsx
+++ sample-apps/react/react-dogfood/components/DefaultAppHeader.tsx
@@ -2,7 +2,7 @@ import { HomeButton, UserInfo } from './LobbyHeader';
 
 export const DefaultAppHeader = () => {
   return (
-    <div className="rd__call-header">
+    <div className="rd__call-header rd__call-header--transparent">
       <div className="rd__call-header__title-group">
         <HomeButton />
       </div>
