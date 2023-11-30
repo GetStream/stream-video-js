9ac9952674f65a3b331d8021283a1c28796f8002
zwaardje <niels@seiton.io>
2023-11-30 09:28:32 +0100

    feat: update latency chart with chartjs

--- packages/react-sdk/src/components/CallStats/CallStatsLatencyChart.tsx
+++ packages/react-sdk/src/components/CallStats/CallStatsLatencyChart.tsx
@@ -1,57 +1,88 @@
-import { ResponsiveLine } from '@nivo/line';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export const options = {
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      position: 'right',
+      stacked: true,
+      min: 0,
+      max: 100,
+      grid: {
+        display: true,
+        color: '#979ca0',
+      },
+    },
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        display: false,
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+  },
+};
 
 export const CallStatsLatencyChart = (props: {
   values: Array<{ x: number; y: number }>;
 }) => {
   const { values } = props;
   let max = 0;
-  const data = values.map((point) => {
-    const { y } = point;
-    max = Math.max(max, y);
-    return point;
-  });
+
+  const data = {
+    labels: values.map((point) => {
+      const date = new Date(point.x * 1000);
+      return `${date.getHours()}:${date.getMinutes()}`;
+    }),
+    datasets: [
+      {
+        label: 'Latency',
+        data: values.map((point) => {
+          const { y } = point;
+          max = Math.max(max, y);
+          return point;
+        }),
+        borderColor: '#00e2a1',
+        backgroundColor: '#00e2a1',
+      },
+    ],
+  };
+
   return (
     <div className="str-video__call-stats-line-chart-container">
-      <ResponsiveLine
-        colors={{ scheme: 'blues' }}
-        data={[
-          {
-            id: 'Latency',
-            data: data,
-          },
-        ]}
-        animate={false}
-        margin={{ top: 10, right: 5, bottom: 5, left: 30 }}
-        enablePoints
-        enableGridX={false}
-        enableGridY
-        enableSlices="x"
-        isInteractive
-        useMesh={false}
-        xScale={{ type: 'point' }}
-        yScale={{
-          type: 'linear',
-          min: 0,
-          max: max < 220 ? 220 : max + 30,
-          nice: true,
-        }}
-        theme={{
-          axis: {
-            ticks: {
-              text: {
-                fill: '#FCFCFD',
-              },
-              line: {
-                stroke: '#FCFCFD',
-              },
-            },
-          },
-          grid: {
-            line: {
-              strokeWidth: 0.1,
-            },
-          },
-        }}
+      <Line
+        options={options}
+        data={data}
+        className="str-video__call-stats__latencychart"
       />
     </div>
   );
