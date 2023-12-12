aa7062130224a8604e773011242e1bdc717d27c5
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-12-12 16:20:43 +0100

    feat: respect basePath configuration for favicons

--- sample-apps/react/react-dogfood/pages/_document.tsx
+++ sample-apps/react/react-dogfood/pages/_document.tsx
@@ -1,38 +1,40 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href={`${basePath}/favicon.ico`} />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={`${basePath}/favicon-32x32.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={`${basePath}/favicon-16x16.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href={`${basePath}/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/android-chrome-192x192.png"
+          href={`${basePath}/android-chrome-192x192.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="512x512"
-          href="/android-chrome-512x512.png"
+          href={`${basePath}/android-chrome-512x512.png`}
         />
         <link
           href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css"
