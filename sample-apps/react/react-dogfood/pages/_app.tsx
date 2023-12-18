2ac0c40831ddf837d6c028db57d24297213bff3d
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2023-12-18 18:54:53 +0100

    feat: add GTM integration

--- sample-apps/react/react-dogfood/pages/_app.tsx
+++ sample-apps/react/react-dogfood/pages/_app.tsx
@@ -5,6 +5,7 @@ import '../style/index.scss';
 import { ComponentType } from 'react';
 import { Session } from 'next-auth';
 import Head from 'next/head';
+import { GoogleTagManager } from '@next/third-parties/google';
 import { SessionProvider } from 'next-auth/react';
 import { StreamTheme } from '@stream-io/video-react-sdk';
 import { SettingsProvider } from '../context/SettingsContext';
@@ -16,6 +17,8 @@ type AppProps = {
     session: Session;
   };
 };
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export default function App({
   Component,
@@ -37,6 +40,7 @@ export default function App({
           </StreamTheme>
         </SettingsProvider>
       </AppEnvironmentProvider>
+      {GTM_ID && <GoogleTagManager gtmId={GTM_ID} />}
     </SessionProvider>
   );
 }
