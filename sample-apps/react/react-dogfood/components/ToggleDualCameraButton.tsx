22687f567a8155c2256bc2403091b2fe3d0cc00b
Matvei Andrienko <matvei.andrienko@getstream.io>
2024-01-08 17:07:52 +0100

    feat(react-dogfoog): adopt CompositeButton for dual buttons

--- sample-apps/react/react-dogfood/components/ToggleDualCameraButton.tsx
+++ sample-apps/react/react-dogfood/components/ToggleDualCameraButton.tsx
@@ -1,48 +1,17 @@
-import { forwardRef } from 'react';
 import clsx from 'clsx';
 
 import {
   DeviceSelectorVideo,
-  Icon,
-  MenuToggle,
-  MenuVisualType,
-  ToggleMenuButtonProps,
   ToggleVideoPublishingButton,
-  useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 
-const ToggleMenuButton = forwardRef<HTMLDivElement, ToggleMenuButtonProps>(
-  function ToggleMenuButton(props, ref) {
-    return (
-      <div
-        ref={ref}
-        className={clsx('rd__dual-toggle__device-selector', {
-          'rd__dual-toggle__device-selector--active': props.menuShown,
-        })}
-      >
-        <Icon icon={props.menuShown ? 'caret-down' : 'caret-up'} />
-      </div>
-    );
-  },
-);
-
 export const ToggleDualCameraButton = () => {
-  const { useCameraState } = useCallStateHooks();
-  const { status } = useCameraState();
   return (
-    <div
-      className={clsx('rd__dual-toggle', {
-        'rd__dual-toggle--active': status === 'disabled',
-      })}
-    >
-      <ToggleVideoPublishingButton />
-      <MenuToggle
-        placement="top"
-        ToggleButton={ToggleMenuButton}
-        visualType={MenuVisualType.MENU}
-      >
-        <DeviceSelectorVideo visualType="list" />
-      </MenuToggle>
+    <div className={clsx('rd__dual-toggle')}>
+      <ToggleVideoPublishingButton
+        Menu={<DeviceSelectorVideo visualType="list" />}
+        menuPlacement="top"
+      />
     </div>
   );
 };
