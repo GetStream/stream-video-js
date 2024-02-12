2d0a865e1f3d5a72df6bc528eb0ed5e2494eb734
Santhosh Vaiyapuri <3846977+santhoshvai@users.noreply.github.com>
2024-02-12 15:31:29 +0100

    feat(client): add stopOnLeave param to device manager (#1266)
    ## Goal
    
    Adds a parameter to device manager to enable automatically stopping
    streams when call is left
    
    fixes #1236

--- packages/client/src/devices/InputMediaDeviceManager.ts
+++ packages/client/src/devices/InputMediaDeviceManager.ts
@@ -20,6 +20,10 @@ export abstract class InputMediaDeviceManager<
    * @internal
    */
   disablePromise?: Promise<void>;
+  /**
+   * if true, stops the media stream when call is left
+   */
+  stopOnLeave = true;
   logger: Logger;
   private subscriptions: Subscription[] = [];
   private isTrackStoppedDueToTrackEnd = false;
@@ -67,12 +71,13 @@ export abstract class InputMediaDeviceManager<
   }
 
   /**
-   * Stops the stream.
+   * Stops or pauses the stream based on state.disableMode
+   * @param {boolean} [forceStop=false] when true, stops the tracks regardless of the state.disableMode
    */
-  async disable() {
+  async disable(forceStop: boolean = false) {
     this.state.prevStatus = this.state.status;
-    if (this.state.status === 'disabled') return;
-    const stopTracks = this.state.disableMode === 'stop-tracks';
+    if (!forceStop && this.state.status === 'disabled') return;
+    const stopTracks = forceStop || this.state.disableMode === 'stop-tracks';
     this.disablePromise = this.muteStream(stopTracks);
     try {
       await this.disablePromise;
