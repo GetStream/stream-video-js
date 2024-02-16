bd4c69cdeadf1a944dea69ed61afc270a71380cb
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-02-16 16:46:04 +0100

    add env support

--- sample-apps/react-native/dogfood/babel.config.js
+++ sample-apps/react-native/dogfood/babel.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+module.exports = function (api) {
+  // we disable caching for picking up changes in the environment variables - https://www.npmjs.com/package/react-native-dotenv
+  api.cache(false);
+
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'],
+  };
 };
--- sample-apps/react-native/dogfood/src/modules/helpers/createToken.ts
+++ sample-apps/react-native/dogfood/src/modules/helpers/createToken.ts
@@ -1,17 +1,14 @@
+import { ENVIRONMENT } from '@env';
+
 type ParamsType = {
   user_id: string;
   call_cids?: string;
 };
 
-type EnvironmentType = 'pronto' | 'demo';
-
-export const createToken = async (
-  params: ParamsType,
-  environment: EnvironmentType = 'pronto',
-) => {
+export const createToken = async (params: ParamsType) => {
   const endpoint = new URL('https://pronto.getstream.io/api/auth/create-token');
   endpoint.searchParams.set('user_id', params.user_id);
-  endpoint.searchParams.set('environment', environment);
+  endpoint.searchParams.set('environment', ENVIRONMENT);
   endpoint.searchParams.set('exp', String(4 * 60 * 60)); // 4 hours
   if (params.call_cids) {
     endpoint.searchParams.set('call_cids', params.call_cids);
