777f13f05045e1f2af18514dbcddb578d3be5c60
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2024-03-22 12:14:50 +0100

    chore: update the schema

--- packages/client/src/Call.ts
+++ packages/client/src/Call.ts
@@ -24,6 +24,7 @@ import {
   BlockUserResponse,
   EndCallResponse,
   GetCallResponse,
+  GetCallStatsResponse,
   GetOrCreateCallRequest,
   GetOrCreateCallResponse,
   GoLiveRequest,
@@ -1838,9 +1839,9 @@ export class Call {
   /**
    * Retrieve call statistics for a particular call session (historical).
    * Here `callSessionID` is mandatory.
-   * 
+   *
    * @param callSessionID the call session ID to retrieve statistics for.
-   * @returns The call stats, 
+   * @returns The call stats.
    */
   getCallStats = async (callSessionID: string) => {
     const endpoint = `${this.streamClientBasePath}/stats/${callSessionID}`;
