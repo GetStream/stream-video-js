b63b196b2cebdb62da8fabc0b32ba267ed639350
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-04-12 11:25:21 +0200

    remove the hook

--- packages/react-native-sdk/src/components/Call/CallControls/ScreenShareToggleButton.tsx
+++ packages/react-native-sdk/src/components/Call/CallControls/ScreenShareToggleButton.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { NativeModules, Platform, findNodeHandle } from 'react-native';
 import { ScreenCapturePickerView } from '@stream-io/react-native-webrtc';
 import { ScreenShare, StopScreenShare } from '../../../icons';
 import { CallControlsButton } from './CallControlsButton';
 import { useTheme } from '../../../contexts';
-import { useScreenShareToggle } from '../../../hooks/useScreenShareToggle';
+import { useCall, useCallStateHooks } from '@stream-io/video-react-bindings';
+import { useIsIosScreenshareBroadcastStarted } from '../../../hooks/useIsIosScreenshareBroadcastStarted';
+import { SfuModels } from '@stream-io/video-client';
+import { usePrevious } from '../../../utils/hooks/usePrevious';
+
+// ios >= 14.0 or android - platform restrictions
+const CanDeviceScreenShare =
+  (Platform.OS === 'ios' &&
+    // @ts-ignore
+    Number.parseInt(Platform.Version.split('.')[0], 10) >= 14) ||
+  Platform.OS === 'android';
 
 /**
  * The props for the Screen Share button in the Call Controls.
@@ -34,17 +44,87 @@ export const ScreenShareToggleButton = ({
     theme: { colors, screenShareToggleButton },
   } = useTheme();
 
-  const {
-    hasPublishedScreenShare,
-    isScreenSharingEnabledInCall,
-    isScreenSharingAccessRequestEnabled,
-    CanDeviceScreenShare,
-    onPress,
-    screenCapturePickerViewiOSRef,
-  } = useScreenShareToggle({
-    onScreenShareStartedHandler,
-    onScreenShareStoppedHandler,
-  });
+  const call = useCall();
+  const { useLocalParticipant, useCallSettings } = useCallStateHooks();
+  const callSettings = useCallSettings();
+  const isScreenSharingEnabledInCall = callSettings?.screensharing.enabled;
+  const isScreenSharingAccessRequestEnabled =
+    callSettings?.screensharing.access_request_enabled;
+
+  const onScreenShareStartedHandlerRef = useRef(onScreenShareStartedHandler);
+  onScreenShareStartedHandlerRef.current = onScreenShareStartedHandler;
+  const onScreenShareStoppedHandlerRef = useRef(onScreenShareStoppedHandler);
+  onScreenShareStoppedHandlerRef.current = onScreenShareStoppedHandler;
+
+  const iosScreenShareStartedFromSystem = useIsIosScreenshareBroadcastStarted();
+  const prevIosScreenShareStartedFromSystem = usePrevious(
+    iosScreenShareStartedFromSystem,
+  );
+
+  const localParticipant = useLocalParticipant();
+  const hasPublishedScreenShare = localParticipant?.publishedTracks.includes(
+    SfuModels.TrackType.SCREEN_SHARE,
+  );
+
+  // listens to iOS screen share broadcast started event from the system
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS !== 'ios') {
+        return;
+      }
+      if (
+        iosScreenShareStartedFromSystem &&
+        !prevIosScreenShareStartedFromSystem
+      ) {
+        onScreenShareStartedHandlerRef.current?.();
+        const media = await navigator.mediaDevices.getDisplayMedia({
+          // @ts-ignore
+          deviceId: 'broadcast',
+          video: true,
+          audio: true,
+        });
+        await call?.publishScreenShareStream(media);
+      } else if (
+        !iosScreenShareStartedFromSystem &&
+        prevIosScreenShareStartedFromSystem
+      ) {
+        onScreenShareStoppedHandlerRef.current?.();
+        await call?.stopPublish(SfuModels.TrackType.SCREEN_SHARE);
+      }
+    };
+    run();
+  }, [
+    call,
+    iosScreenShareStartedFromSystem,
+    prevIosScreenShareStartedFromSystem,
+  ]);
+
+  const screenCapturePickerViewiOSRef = React.useRef(null);
+
+  const onPress = async () => {
+    if (!hasPublishedScreenShare) {
+      if (Platform.OS === 'ios') {
+        const reactTag = findNodeHandle(screenCapturePickerViewiOSRef.current);
+        await NativeModules.ScreenCapturePickerViewManager.show(reactTag);
+        // After this the iOS screen share broadcast started/stopped event will be triggered
+        // and the useEffect listener will handle the rest
+      } else {
+        try {
+          const media = await navigator.mediaDevices.getDisplayMedia({
+            video: true,
+            audio: true,
+          });
+          onScreenShareStartedHandler?.();
+          await call?.publishScreenShareStream(media);
+        } catch (e) {
+          // ignored.. user didnt allow the screen share in the popup
+        }
+      }
+    } else if (hasPublishedScreenShare) {
+      onScreenShareStoppedHandler?.();
+      await call?.stopPublish(SfuModels.TrackType.SCREEN_SHARE);
+    }
+  };
 
   if (!isScreenSharingEnabledInCall || !CanDeviceScreenShare) {
     return null;
--- packages/react-native-sdk/src/components/Livestream/LivestreamControls/LivestreamScreenShareToggleButton.tsx
+++ packages/react-native-sdk/src/components/Livestream/LivestreamControls/LivestreamScreenShareToggleButton.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTheme } from '../../../contexts';
-import { Platform, Pressable, StyleSheet, View } from 'react-native';
+import {
+  NativeModules,
+  Platform,
+  Pressable,
+  StyleSheet,
+  View,
+  findNodeHandle,
+} from 'react-native';
 import { ScreenShare, StopScreenShare } from '../../../icons';
-import { useScreenShareToggle } from '../../../hooks/useScreenShareToggle';
 import { ScreenCapturePickerView } from '@stream-io/react-native-webrtc';
+import { SfuModels } from '@stream-io/video-client';
+import { useCall, useCallStateHooks } from '@stream-io/video-react-bindings';
+import { useIsIosScreenshareBroadcastStarted } from '../../..';
+import { usePrevious } from '../../../utils/hooks';
 
 export type LivestreamScreenShareToggleButtonProps = {};
+
+// ios >= 14.0 or android - platform restrictions
+const CanDeviceScreenShare =
+  (Platform.OS === 'ios' &&
+    // @ts-ignore
+    Number.parseInt(Platform.Version.split('.')[0], 10) >= 14) ||
+  Platform.OS === 'android';
 
 /**
  * The LivestreamVideoControlButton controls the screenshare stream publish/unpublish while in the livestream for the host.
  */
 export const LivestreamScreenShareToggleButton = () => {
-  const {
-    hasPublishedScreenShare,
-    onPress,
-    isScreenSharingEnabledInCall,
-    screenCapturePickerViewiOSRef,
-  } = useScreenShareToggle();
   const {
     theme: {
       colors,
@@ -25,7 +36,78 @@ export const LivestreamScreenShareToggleButton = () => {
     },
   } = useTheme();
 
-  if (!isScreenSharingEnabledInCall) {
+  const call = useCall();
+  const { useLocalParticipant, useCallSettings } = useCallStateHooks();
+  const callSettings = useCallSettings();
+  const isScreenSharingEnabledInCall = callSettings?.screensharing.enabled;
+
+  const iosScreenShareStartedFromSystem = useIsIosScreenshareBroadcastStarted();
+  const prevIosScreenShareStartedFromSystem = usePrevious(
+    iosScreenShareStartedFromSystem,
+  );
+
+  const localParticipant = useLocalParticipant();
+  const hasPublishedScreenShare = localParticipant?.publishedTracks.includes(
+    SfuModels.TrackType.SCREEN_SHARE,
+  );
+
+  // listens to iOS screen share broadcast started event from the system
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS !== 'ios') {
+        return;
+      }
+      if (
+        iosScreenShareStartedFromSystem &&
+        !prevIosScreenShareStartedFromSystem
+      ) {
+        const media = await navigator.mediaDevices.getDisplayMedia({
+          // @ts-ignore
+          deviceId: 'broadcast',
+          video: true,
+          audio: true,
+        });
+        await call?.publishScreenShareStream(media);
+      } else if (
+        !iosScreenShareStartedFromSystem &&
+        prevIosScreenShareStartedFromSystem
+      ) {
+        await call?.stopPublish(SfuModels.TrackType.SCREEN_SHARE);
+      }
+    };
+    run();
+  }, [
+    call,
+    iosScreenShareStartedFromSystem,
+    prevIosScreenShareStartedFromSystem,
+  ]);
+
+  const screenCapturePickerViewiOSRef = React.useRef(null);
+
+  const onPress = async () => {
+    if (!hasPublishedScreenShare) {
+      if (Platform.OS === 'ios') {
+        const reactTag = findNodeHandle(screenCapturePickerViewiOSRef.current);
+        await NativeModules.ScreenCapturePickerViewManager.show(reactTag);
+        // After this the iOS screen share broadcast started/stopped event will be triggered
+        // and the useEffect listener will handle the rest
+      } else {
+        try {
+          const media = await navigator.mediaDevices.getDisplayMedia({
+            video: true,
+            audio: true,
+          });
+          await call?.publishScreenShareStream(media);
+        } catch (e) {
+          // ignored.. user didnt allow the screen share in the popup
+        }
+      }
+    } else if (hasPublishedScreenShare) {
+      await call?.stopPublish(SfuModels.TrackType.SCREEN_SHARE);
+    }
+  };
+
+  if (!isScreenSharingEnabledInCall || !CanDeviceScreenShare) {
     return null;
   }
 
