7edd08227cfed2603b4f201975e2e3c98d387277
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-06-06 11:29:24 +0200

    fix: image filters did not work in android release mode

--- packages/video-filters-react-native/android/src/main/java/com/streamio/videofiltersreactnative/VideoFiltersReactNativeModule.kt
+++ packages/video-filters-react-native/android/src/main/java/com/streamio/videofiltersreactnative/VideoFiltersReactNativeModule.kt
@@ -43,7 +43,7 @@ class VideoFiltersReactNativeModule(reactContext: ReactApplicationContext) :
   fun registerVirtualBackgroundFilter(backgroundImageUrlString: String) {
     ProcessorProvider.addProcessor(
       "VirtualBackground-$backgroundImageUrlString",
-      VirtualBackgroundFactory(backgroundImageUrlString)
+      VirtualBackgroundFactory(reactApplicationContext, backgroundImageUrlString)
     )
   }
 
