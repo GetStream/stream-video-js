b69eeb4c6a3641cd132a3786fcb37054a880e6b0
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-06-10 12:35:13 +0200

    fix(ios): do not disable camera on inactive state

--- packages/react-native-sdk/src/providers/StreamCall.tsx
+++ packages/react-native-sdk/src/providers/StreamCall.tsx
@@ -1,5 +1,5 @@
 import { StreamCallProvider, useCall } from '@stream-io/video-react-bindings';
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, { PropsWithChildren, useEffect, useRef } from 'react';
 import { Call } from '@stream-io/video-client';
 import { useIosCallkeepWithCallingStateEffect } from '../hooks/push/useIosCallkeepWithCallingStateEffect';
 import {
@@ -7,8 +7,7 @@ import {
   clearPushWSEventSubscriptions,
 } from '../utils/push/utils';
 import { useAndroidKeepCallAliveEffect } from '../hooks/useAndroidKeepCallAliveEffect';
-import { useAppStateListener } from '../utils/hooks';
-import { NativeModules, Platform } from 'react-native';
+import { AppState, NativeModules, Platform } from 'react-native';
 
 export type StreamCallProps = {
   /**
@@ -39,31 +38,44 @@ export const StreamCall = ({
   );
 };
 
+// Resume/Disable video stream tracks when app goes to background/foreground
+// To save on CPU resources
 const AppStateListener = () => {
   const call = useCall();
-
-  // Resume/Disable video stream tracks when app goes to background/foreground
-  // To save on CPU resources
-  useAppStateListener(
-    async () => {
-      await call?.camera?.resume();
-    },
-    async () => {
-      if (Platform.OS === 'android') {
-        // in Android, we need to check if we are in PiP mode
-        // in PiP mode, we don't want to disable the camera
-        NativeModules?.StreamVideoReactNative?.isInPiPMode().then(
-          async (isInPiP: boolean | null | undefined) => {
-            if (!isInPiP) {
-              await call?.camera?.disable();
+  const appState = useRef(AppState.currentState);
+  useEffect(() => {
+    // due to strange behavior in iOS when app goes to "inactive" state
+    // we dont check for inactive states
+    // ref: https://www.reddit.com/r/reactnative/comments/15kib42/appstate_behavior_in_ios_when_swiping_down_to/
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (appState.current.match(/background/) && nextAppState === 'active') {
+        call?.camera?.resume();
+        appState.current = nextAppState;
+      } else if (
+        appState.current === 'active' &&
+        nextAppState.match(/background/)
+      ) {
+        if (Platform.OS === 'android') {
+          // in Android, we need to check if we are in PiP mode
+          // in PiP mode, we don't want to disable the camera
+          NativeModules?.StreamVideoReactNative?.isInPiPMode().then(
+            async (isInPiP: boolean | null | undefined) => {
+              if (!isInPiP) {
+                await call?.camera?.disable();
+              }
             }
-          }
-        );
-      } else {
-        await call?.camera?.disable();
+          );
+        } else {
+          call?.camera?.disable();
+        }
+        appState.current = nextAppState;
       }
-    }
-  );
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [call]);
 
   return null;
 };
