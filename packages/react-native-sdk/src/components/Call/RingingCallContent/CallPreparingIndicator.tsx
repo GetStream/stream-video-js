6ef25507e5b19332873bb0cdd872c09eeb9890d3
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2024-07-05 00:39:15 +0530

    fix: ringing call content prop did not handle reconnection

---
+++ packages/react-native-sdk/src/components/Call/RingingCallContent/CallPreparingIndicator.tsx
@@ -0,0 +1,21 @@
+import { useI18n } from '@stream-io/video-react-bindings';
+import React from 'react';
+import {
+  TextBasedIndicator,
+  TextBasedIndicatorProps,
+} from './TextBasedIndicator';
+
+export type CallPreparingIndicatorProps = Pick<
+  TextBasedIndicatorProps,
+  'onBackPress'
+>;
+
+export const CallPreparingIndicator = (props: CallPreparingIndicatorProps) => {
+  const { t } = useI18n();
+  return (
+    <TextBasedIndicator
+      text={t('Preparing call')}
+      onBackPress={props.onBackPress}
+    />
+  );
+};
