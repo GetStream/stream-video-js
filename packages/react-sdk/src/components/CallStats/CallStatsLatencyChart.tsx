e397d8fb5398b40785467702a0ecccdb9ab35245
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2024-09-16 17:20:18 +0200

    fix: extract CallStatsLatency chart in its own chunk

--- packages/react-sdk/src/components/CallStats/CallStatsLatencyChart.tsx
+++ packages/react-sdk/src/components/CallStats/CallStatsLatencyChart.tsx
@@ -1,16 +1,15 @@
-import {
-  CategoryScale,
-  Chart as ChartJS,
-  ChartData,
-  ChartOptions,
-  LinearScale,
-  LineElement,
-  PointElement,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import { useMemo } from 'react';
+import { lazy, Suspense, useMemo } from 'react';
+import type { ChartData, ChartOptions } from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement);
+const Line = lazy(() =>
+  import('chart.js')
+    .then(({ CategoryScale, Chart, LinearScale, LineElement, PointElement }) =>
+      Chart.register(CategoryScale, LinearScale, LineElement, PointElement),
+    )
+    .then(() =>
+      import('react-chartjs-2').then((module) => ({ default: module.Line })),
+    ),
+);
 
 export const CallStatsLatencyChart = (props: {
   values: Array<{ x: number; y: number }>;
@@ -35,53 +34,39 @@ export const CallStatsLatencyChart = (props: {
     ],
   };
 
-  const options = useMemo<ChartOptions<'line'>>(() => {
-    return {
-      maintainAspectRatio: false,
-      animation: {
-        duration: 0,
-      },
-      elements: {
-        line: {
-          borderWidth: 1,
-        },
-        point: {
-          radius: 2,
-        },
-      },
-      scales: {
-        y: {
-          position: 'right',
-          stacked: true,
-          min: 0,
-          max: Math.max(180, Math.ceil((max + 10) / 10) * 10),
-          grid: {
-            display: true,
-            color: '#979ca0',
-          },
-          ticks: {
-            stepSize: 30,
-          },
-        },
-        x: {
-          grid: {
-            display: false,
-          },
-          ticks: {
-            display: false,
-          },
-        },
-      },
-    };
-  }, [max]);
-
+  const options = useMemo(() => getLineOptions(max), [max]);
   return (
     <div className="str-video__call-stats-line-chart-container">
-      <Line
-        options={options}
-        data={data}
-        className="str-video__call-stats__latencychart"
-      />
+      <Suspense fallback={null}>
+        <Line
+          options={options}
+          data={data}
+          className="str-video__call-stats__latencychart"
+        />
+      </Suspense>
     </div>
   );
 };
+
+const getLineOptions = (max: number): ChartOptions<'line'> => ({
+  maintainAspectRatio: false,
+  animation: { duration: 0 },
+  elements: {
+    line: { borderWidth: 1 },
+    point: { radius: 2 },
+  },
+  scales: {
+    y: {
+      position: 'right',
+      stacked: true,
+      min: 0,
+      max: Math.max(180, Math.ceil((max + 10) / 10) * 10),
+      grid: { display: true, color: '#979ca0' },
+      ticks: { stepSize: 30 },
+    },
+    x: {
+      grid: { display: false },
+      ticks: { display: false },
+    },
+  },
+});
