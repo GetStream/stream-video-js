433f316c9340d8fdce2cefca2ee9e5dd2be01a1a
Kristian Martinoski <martinoskikristian@gmail.com>
2024-10-15 14:53:35 +0200

    fix: lint issue

--- packages/react-native-sdk/src/components/Call/CallContent/CallContent.tsx
+++ packages/react-native-sdk/src/components/Call/CallContent/CallContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import InCallManager from 'react-native-incall-manager';
 
