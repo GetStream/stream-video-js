4752a20441b036cf1dd2a3619a958c4831891b71
Kristian Martinoski <martinoskikristian@gmail.com>
2024-10-22 09:22:38 +0200

    chore: revert old implementation in call state

---
+++ sample-apps/react-native/dogfood/src/components/CallControlls/CallStatusBadge.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { CallDuration } from '../../assets/CallDuration';
+import { RecordCall } from '@stream-io/video-react-native-sdk/src/icons/RecordCall';
+import { IconWrapper } from '@stream-io/video-react-native-sdk/src/icons';
+import { useCallStateHooks, useTheme } from '@stream-io/video-react-native-sdk';
+
+/**
+ * Props for the CallStatusBadge component.
+ */
+interface CallStatusBadgeProps {
+  /**
+   * Indicates if the call is currently being recorded.
+   * When true, it shows "Recording in progress..." and a specific icon.
+   * When false, it shows the elapsed time.
+   */
+  isCallRecorded: boolean;
+}
+
+/**
+ * CallStatusBadge component to display the current status of a call.
+ * It shows either a recording message or the elapsed time, with an accompanying icon.
+ *
+ * @param {CallStatusBadgeProps} props - The props for the component.
+ */
+export const CallStatusBadge: React.FC<CallStatusBadgeProps> = ({
+  isCallRecorded,
+}) => {
+  const {
+    theme: {
+      colors,
+      variants: { iconSizes },
+    },
+  } = useTheme();
+
+  const { useCallDuration } = useCallStateHooks();
+  const duration = useCallDuration();
+
+  const styles = useStyles(isCallRecorded);
+
+  // Format duration to MM:SS
+  const minutes = Math.floor(duration / 60)
+    .toString()
+    .padStart(2, '0');
+  const seconds = (duration % 60).toString().padStart(2, '0');
+  const timestamp = `${minutes}:${seconds}`;
+  const text = isCallRecorded ? 'Recording in progress...' : timestamp;
+
+  const icon = isCallRecorded ? (
+    <RecordCall color={colors.iconAlertWarning} size={iconSizes.md} />
+  ) : (
+    <CallDuration color={colors.iconAlertSuccess} size={iconSizes.md} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <IconWrapper>
+        <View style={styles.icon}>{icon}</View>
+      </IconWrapper>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+};
+
+const useStyles = (isCallRecorded: boolean) => {
+  const { theme } = useTheme();
+
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        container: {
+          backgroundColor: theme.colors.buttonSecondaryDefault,
+          borderRadius: 8,
+          flexDirection: 'row',
+          height: 36,
+          paddingLeft: 17,
+          paddingRight: 5,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: isCallRecorded ? 200 : 80,
+        },
+        text: {
+          color: theme.colors.typePrimary,
+          fontSize: 14,
+          fontWeight: '600',
+          flexShrink: 0,
+          marginLeft: 10,
+          minWidth: 41,
+        },
+        icon: {
+          marginTop: 2,
+          marginRight: 5,
+        },
+      }),
+    [theme, isCallRecorded],
+  );
+};
