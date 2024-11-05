GetStream/stream-video-js

commit 562b5cca77264330d08dff5305eccc489970076a

fix: ignore camera direction for desktop devices (#1521)
Our default call types include a default setting for camera direction
(user-facing). Having this setting applied by default is useful for
mobile devices, but on desktop devices where camera direction reporting
is not reliable it leads to confusion, because it can cause a device
other than the system default to be used by default.

packages/client/src/devices/CameraManager.ts
@@ -5,6 +5,7 @@ import { InputMediaDeviceManager } from './InputMediaDeviceManager';
 import { getVideoDevices, getVideoStream } from './devices';
 import { TrackType } from '../gen/video/sfu/models/models';
 import { PreferredCodec, PublishOptions } from '../types';
+import { isMobile } from '../compatibility';
 
 export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
   private targetResolution = {
@@ -34,10 +35,14 @@ export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
    * @param direction the direction of the camera to select.
    */
   async selectDirection(direction: Exclude<CameraDirection, undefined>) {
-    this.state.setDirection(direction);
-    // Providing both device id and direction doesn't work, so we deselect the device
-    this.state.setDevice(undefined);
-    await this.applySettingsToStream();
+    if (isMobile()) {
+      this.state.setDirection(direction);
+      // Providing both device id and direction doesn't work, so we deselect the device
+      this.state.setDevice(undefined);
+      await this.applySettingsToStream();
+    } else {
+      this.logger('warn', 'Camera direction ignored for desktop devices');
+    }
   }
 
   /**
@@ -131,7 +136,7 @@ export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
     constraints.height = this.targetResolution.height;
     // We can't set both device id and facing mode
     // Device id has higher priority
-    if (!constraints.deviceId && this.state.direction) {
+    if (!constraints.deviceId && this.state.direction && isMobile()) {
       constraints.facingMode =
         this.state.direction === 'front' ? 'user' : 'environment';
     }

packages/client/src/devices/__tests__/CameraManager.test.ts
@@ -36,6 +36,13 @@ vi.mock('../../Call.ts', () => {
   };
 });
 
+vi.mock('../../compatibility.ts', () => {
+  console.log('MOCKING mobile device');
+  return {
+    isMobile: () => true,
+  };
+});
+
 describe('CameraManager', () => {
   let manager: CameraManager;
 

packages/client/src/compatibility.ts
@@ -1,0 +1,7 @@
+/**
+ * Checks if the current platform is a mobile device.
+ *
+ * See:
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
+ */
+export const isMobile = () => /Mobi/i.test(navigator.userAgent);

commit 423890cb2d1925366d8a63c29f93c4c92c8104ad

fix: camera flip did not work in react-native (#1554)
fixes regression caused by #1521

packages/client/src/devices/CameraManager.ts
@@ -6,6 +6,7 @@ import { getVideoDevices, getVideoStream } from './devices';
 import { TrackType } from '../gen/video/sfu/models/models';
 import { PreferredCodec } from '../types';
 import { isMobile } from '../compatibility';
+import { isReactNative } from '../helpers/platforms';
 
 export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
   private targetResolution = {
@@ -22,13 +23,17 @@ export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
     super(call, new CameraManagerState(), TrackType.VIDEO);
   }
 
+  private isDirectionSupportedByDevice() {
+    return isReactNative() || isMobile();
+  }
+
   /**
    * Select the camera direction.
    *
    * @param direction the direction of the camera to select.
    */
   async selectDirection(direction: Exclude<CameraDirection, undefined>) {
-    if (isMobile()) {
+    if (this.isDirectionSupportedByDevice()) {
       this.state.setDirection(direction);
       // Providing both device id and direction doesn't work, so we deselect the device
       this.state.setDevice(undefined);
@@ -102,7 +107,12 @@ export class CameraManager extends InputMediaDeviceManager<CameraManagerState> {
     constraints.height = this.targetResolution.height;
     // We can't set both device id and facing mode
     // Device id has higher priority
-    if (!constraints.deviceId && this.state.direction && isMobile()) {
+
+    if (
+      !constraints.deviceId &&
+      this.state.direction &&
+      this.isDirectionSupportedByDevice()
+    ) {
       constraints.facingMode =
         this.state.direction === 'front' ? 'user' : 'environment';
     }

packages/client/src/devices/__tests__/CameraManager.test.ts
@@ -43,6 +43,13 @@ vi.mock('../../compatibility.ts', () => {
   };
 });
 
+vi.mock('../../helpers/platforms', () => {
+  console.log('MOCKING mobile device');
+  return {
+    isReactNative: () => false,
+  };
+});
+
 describe('CameraManager', () => {
   let manager: CameraManager;
 