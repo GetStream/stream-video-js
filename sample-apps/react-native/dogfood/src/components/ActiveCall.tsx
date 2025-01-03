1a3ddae2b83e7871697219a71f07cfc503022675
Kristian Martinoski <martinoskikristian@gmail.com>
2025-01-03 10:18:13 +0100

    adds closed captions in the dogfood app

--- sample-apps/react-native/dogfood/src/components/ActiveCall.tsx
+++ sample-apps/react-native/dogfood/src/components/ActiveCall.tsx
@@ -12,9 +12,13 @@ import { useOrientation } from '../hooks/useOrientation';
 import { Z_INDEX } from '../constants';
 import { TopControls } from './CallControlls/TopControls';
 import { useLayout } from '../contexts/LayoutContext';
-import { useToggleCallRecording } from '@stream-io/video-react-bindings';
+import {
+  useCallStateHooks,
+  useToggleCallRecording,
+} from '@stream-io/video-react-bindings';
 import { useAppGlobalStoreValue } from '../contexts/AppContext';
 import DeviceInfo from 'react-native-device-info';
+import { ClosedCaptions } from './ClosedCaptions';
 
 type ActiveCallProps = {
   onHangupCallHandler?: () => void;
@@ -52,17 +56,22 @@ export const ActiveCall = ({
 
   const { toggleCallRecording, isAwaitingResponse, isCallRecordingInProgress } =
     useToggleCallRecording();
+  const { useIsCallCaptioningInProgress } = useCallStateHooks();
+  const isCaptioningInProgress = useIsCallCaptioningInProgress();
 
   const CustomBottomControls = useCallback(() => {
     return (
-      <BottomControls
-        onParticipantInfoPress={onOpenCallParticipantsInfo}
-        onChatOpenHandler={onChatOpenHandler}
-        unreadCountIndicator={unreadCountIndicator}
-        toggleCallRecording={toggleCallRecording}
-        isCallRecordingInProgress={isCallRecordingInProgress}
-        isAwaitingResponse={isAwaitingResponse}
-      />
+      <>
+        {isCaptioningInProgress && <ClosedCaptions />}
+        <BottomControls
+          onParticipantInfoPress={onOpenCallParticipantsInfo}
+          onChatOpenHandler={onChatOpenHandler}
+          unreadCountIndicator={unreadCountIndicator}
+          toggleCallRecording={toggleCallRecording}
+          isCallRecordingInProgress={isCallRecordingInProgress}
+          isAwaitingResponse={isAwaitingResponse}
+        />
+      </>
     );
   }, [
     onChatOpenHandler,
@@ -71,6 +80,7 @@ export const ActiveCall = ({
     toggleCallRecording,
     isAwaitingResponse,
     isCallRecordingInProgress,
+    isCaptioningInProgress,
   ]);
 
   const CustomTopControls = useCallback(() => {
