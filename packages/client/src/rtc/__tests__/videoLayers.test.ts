GetStream/stream-video-js

commit 2e9e344d5259e3069dddb17846013becef24829e

feat: Codec Negotiation (#1527)
Allows dynamic codec negotiation on the fly. Adds support for publishing
a single track in different encodings.
Spec:
https://www.notion.so/stream-wiki/Codec-negotiation-SDK-checklist-1506a5d7f9f680999494eaa2148b28fc

---------

Co-authored-by: thesyncim <thesyncim@gmail.com>

packages/client/src/rtc/videoLayers.ts
@@ -1,8 +1,11 @@
-import { PreferredCodec, PublishOptions } from '../types';
-import { TargetResolutionResponse } from '../gen/shims';
 import { isSvcCodec } from './codecs';
-import { getOptimalBitrate } from './bitrateLookup';
-import { VideoQuality } from '../gen/video/sfu/models/models';
+import {
+  PublishOption,
+  VideoDimension,
+  VideoLayer,
+  VideoQuality,
+} from '../gen/video/sfu/models/models';
+import { isAudioTrackType } from './helpers/tracks';
 
 export type OptimalVideoLayer = RTCRtpEncodingParameters & {
   width: number;
@@ -11,17 +14,10 @@ export type OptimalVideoLayer = RTCRtpEncodingParameters & {
   scalabilityMode?: string;
 };
 
-const DEFAULT_BITRATE = 1250000;
-const defaultTargetResolution: TargetResolutionResponse = {
-  bitrate: DEFAULT_BITRATE,
-  width: 1280,
-  height: 720,
-};
-
 const defaultBitratePerRid: Record<string, number> = {
   q: 300000,
   h: 750000,
-  f: DEFAULT_BITRATE,
+  f: 1250000,
 };
 
 /**
@@ -31,9 +27,17 @@ const defaultBitratePerRid: Record<string, number> = {
  *
  * @param layers the layers to process.
  */
-export const toSvcEncodings = (layers: OptimalVideoLayer[] | undefined) => {
-  // we take the `f` layer, and we rename it to `q`.
-  return layers?.filter((l) => l.rid === 'f').map((l) => ({ ...l, rid: 'q' }));
+export const toSvcEncodings = (
+  layers: OptimalVideoLayer[] | undefined,
+): RTCRtpEncodingParameters[] | undefined => {
+  if (!layers) return;
+  // we take the highest quality layer, and we assign it to `q` encoder.
+  const withRid = (rid: string) => (l: OptimalVideoLayer) => l.rid === rid;
+  const highestLayer =
+    layers.find(withRid('f')) ||
+    layers.find(withRid('h')) ||
+    layers.find(withRid('q'));
+  return [{ ...highestLayer, rid: 'q' }];
 };
 
 /**
@@ -48,60 +52,80 @@ export const ridToVideoQuality = (rid: string): VideoQuality => {
 };
 
 /**
- * Determines the most optimal video layers for simulcasting
- * for the given track.
+ * Converts the given video layers to SFU video layers.
+ */
+export const toVideoLayers = (
+  layers: OptimalVideoLayer[] | undefined = [],
+): VideoLayer[] => {
+  return layers.map<VideoLayer>((layer) => ({
+    rid: layer.rid || '',
+    bitrate: layer.maxBitrate || 0,
+    fps: layer.maxFramerate || 0,
+    quality: ridToVideoQuality(layer.rid || ''),
+    videoDimension: { width: layer.width, height: layer.height },
+  }));
+};
+
+/**
+ * Converts the spatial and temporal layers to a scalability mode.
+ */
+const toScalabilityMode = (spatialLayers: number, temporalLayers: number) =>
+  `L${spatialLayers}T${temporalLayers}${spatialLayers > 1 ? '_KEY' : ''}`;
+
+/**
+ * Determines the most optimal video layers for the given track.
  *
  * @param videoTrack the video track to find optimal layers for.
- * @param targetResolution the expected target resolution.
- * @param codecInUse the codec in use.
- * @param publishOptions the publish options for the track.
+ * @param publishOption the publish options for the track.
  */
-export const findOptimalVideoLayers = (
+export const computeVideoLayers = (
   videoTrack: MediaStreamTrack,
-  targetResolution: TargetResolutionResponse = defaultTargetResolution,
-  codecInUse?: PreferredCodec,
-  publishOptions?: PublishOptions,
-) => {
+  publishOption: PublishOption,
+): OptimalVideoLayer[] | undefined => {
+  if (isAudioTrackType(publishOption.trackType)) return;
   const optimalVideoLayers: OptimalVideoLayer[] = [];
   const settings = videoTrack.getSettings();
   const { width = 0, height = 0 } = settings;
   const {
-    scalabilityMode,
-    bitrateDownscaleFactor = 2,
-    maxSimulcastLayers = 3,
-  } = publishOptions || {};
+    bitrate,
+    codec,
+    fps,
+    maxSpatialLayers = 3,
+    maxTemporalLayers = 3,
+    videoDimension = { width: 1280, height: 720 },
+  } = publishOption;
   const maxBitrate = getComputedMaxBitrate(
-    targetResolution,
+    videoDimension,
     width,
     height,
-    codecInUse,
-    publishOptions,
+    bitrate,
   );
   let downscaleFactor = 1;
   let bitrateFactor = 1;
-  const svcCodec = isSvcCodec(codecInUse);
-  const totalLayers = svcCodec ? 3 : Math.min(3, maxSimulcastLayers);
-  for (const rid of ['f', 'h', 'q'].slice(0, totalLayers)) {
+  const svcCodec = isSvcCodec(codec?.name);
+  for (const rid of ['f', 'h', 'q'].slice(0, maxSpatialLayers)) {
     const layer: OptimalVideoLayer = {
       active: true,
       rid,
       width: Math.round(width / downscaleFactor),
       height: Math.round(height / downscaleFactor),
-      maxBitrate:
-        Math.round(maxBitrate / bitrateFactor) || defaultBitratePerRid[rid],
-      maxFramerate: 30,
+      maxBitrate: maxBitrate / bitrateFactor || defaultBitratePerRid[rid],
+      maxFramerate: fps,
     };
     if (svcCodec) {
       // for SVC codecs, we need to set the scalability mode, and the
       // codec will handle the rest (layers, temporal layers, etc.)
-      layer.scalabilityMode = scalabilityMode || 'L3T2_KEY';
+      layer.scalabilityMode = toScalabilityMode(
+        maxSpatialLayers,
+        maxTemporalLayers,
+      );
     } else {
       // for non-SVC codecs, we need to downscale proportionally (simulcast)
       layer.scaleResolutionDownBy = downscaleFactor;
     }
 
     downscaleFactor *= 2;
-    bitrateFactor *= bitrateDownscaleFactor;
+    bitrateFactor *= 2;
 
     // Reversing the order [f, h, q] to [q, h, f] as Chrome uses encoding index
     // when deciding which layer to disable when CPU or bandwidth is constrained.
@@ -124,29 +148,17 @@ export const findOptimalVideoLayers = (
  * @param targetResolution the target resolution.
  * @param currentWidth the current width of the track.
  * @param currentHeight the current height of the track.
- * @param codecInUse the codec in use.
- * @param publishOptions the publish options.
+ * @param bitrate the target bitrate.
  */
 export const getComputedMaxBitrate = (
-  targetResolution: TargetResolutionResponse,
+  targetResolution: VideoDimension,
   currentWidth: number,
   currentHeight: number,
-  codecInUse?: PreferredCodec,
-  publishOptions?: PublishOptions,
+  bitrate: number,
 ): number => {
   // if the current resolution is lower than the target resolution,
   // we want to proportionally reduce the target bitrate
-  const {
-    width: targetWidth,
-    height: targetHeight,
-    bitrate: targetBitrate,
-  } = targetResolution;
-  const { preferredBitrate } = publishOptions || {};
-  const frameHeight =
-    currentWidth > currentHeight ? currentHeight : currentWidth;
-  const bitrate =
-    preferredBitrate ||
-    (codecInUse ? getOptimalBitrate(codecInUse, frameHeight) : targetBitrate);
+  const { width: targetWidth, height: targetHeight } = targetResolution;
   if (currentWidth < targetWidth || currentHeight < targetHeight) {
     const currentPixels = currentWidth * currentHeight;
     const targetPixels = targetWidth * targetHeight;
@@ -187,24 +199,4 @@ const withSimulcastConstraints = (
     ...layer,
     rid: ridMapping[index], // reassign rid
   }));
-};
-
-export const findOptimalScreenSharingLayers = (
-  videoTrack: MediaStreamTrack,
-  publishOptions?: PublishOptions,
-  defaultMaxBitrate = 3000000,
-): OptimalVideoLayer[] => {
-  const { screenShareSettings: preferences } = publishOptions || {};
-  const settings = videoTrack.getSettings();
-  return [
-    {
-      active: true,
-      rid: 'q', // single track, start from 'q'
-      width: settings.width || 0,
-      height: settings.height || 0,
-      scaleResolutionDownBy: 1,
-      maxBitrate: preferences?.maxBitrate ?? defaultMaxBitrate,
-      maxFramerate: preferences?.maxFramerate ?? 30,
-    },
-  ];
 };

packages/client/src/rtc/__tests__/videoLayers.test.ts
@@ -1,48 +1,20 @@
 import './mocks/webrtc.mocks';
 import { describe, expect, it, vi } from 'vitest';
 import {
-  findOptimalScreenSharingLayers,
-  findOptimalVideoLayers,
+  PublishOption,
+  TrackType,
+  VideoQuality,
+} from '../../gen/video/sfu/models/models';
+import {
+  computeVideoLayers,
   getComputedMaxBitrate,
   OptimalVideoLayer,
   ridToVideoQuality,
   toSvcEncodings,
+  toVideoLayers,
 } from '../videoLayers';
-import { VideoQuality } from '../../gen/video/sfu/models/models';
 
 describe('videoLayers', () => {
-  it('should find optimal screen sharing layers', () => {
-    const track = new MediaStreamTrack();
-    vi.spyOn(track, 'getSettings').mockReturnValue({
-      width: 1920,
-      height: 1080,
-    });
-
-    const layers = findOptimalScreenSharingLayers(track);
-    expect(layers).toEqual([
-      {
-        active: true,
-        rid: 'q',
-        width: 1920,
-        height: 1080,
-        maxBitrate: 3000000,
-        scaleResolutionDownBy: 1,
-        maxFramerate: 30,
-      },
-    ]);
-  });
-
-  it('should use default max bitrate if none is provided in preferences', () => {
-    const track = new MediaStreamTrack();
-    vi.spyOn(track, 'getSettings').mockReturnValue({
-      width: 1920,
-      height: 1080,
-    });
-
-    const layers = findOptimalScreenSharingLayers(track, undefined, 192000);
-    expect(layers).toMatchObject([{ maxBitrate: 192000 }]);
-  });
-
   it('should find optimal video layers', () => {
     const track = new MediaStreamTrack();
     const width = 1920;
@@ -50,11 +22,14 @@ describe('videoLayers', () => {
     const targetBitrate = 3000000;
     vi.spyOn(track, 'getSettings').mockReturnValue({ width, height });
 
-    const layers = findOptimalVideoLayers(track, {
-      width,
-      height,
+    const publishOption: PublishOption = {
       bitrate: targetBitrate,
-    });
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      videoDimension: { width, height },
+      fps: 30,
+    };
+    const layers = computeVideoLayers(track, publishOption);
     expect(layers).toEqual([
       {
         active: true,
@@ -86,13 +61,31 @@ describe('videoLayers', () => {
     ]);
   });
 
+  it('should return undefined for audio track', () => {
+    const track = new MediaStreamTrack();
+    expect(
+      // @ts-expect-error - incomplete data
+      computeVideoLayers(track, { trackType: TrackType.AUDIO }),
+    ).toBeUndefined();
+    expect(
+      // @ts-expect-error - incomplete data
+      computeVideoLayers(track, { trackType: TrackType.SCREEN_SHARE_AUDIO }),
+    ).toBeUndefined();
+  });
+
   it('should use predefined bitrate values when track dimensions cant be determined', () => {
     const width = 0;
     const height = 0;
     const bitrate = 3000000;
     const track = new MediaStreamTrack();
     vi.spyOn(track, 'getSettings').mockReturnValue({ width, height });
-    const layers = findOptimalVideoLayers(track, { width, height, bitrate });
+    const layers = computeVideoLayers(track, {
+      bitrate,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      fps: 30,
+      videoDimension: { width, height },
+    });
     expect(layers).toEqual([
       {
         active: true,
@@ -111,7 +104,13 @@ describe('videoLayers', () => {
     const width = 320;
     const height = 240;
     vi.spyOn(track, 'getSettings').mockReturnValue({ width, height });
-    const layers = findOptimalVideoLayers(track);
+    const layers = computeVideoLayers(track, {
+      bitrate: 0,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      fps: 30,
+      videoDimension: { width, height },
+    });
     expect(layers.length).toBe(1);
     expect(layers[0].rid).toBe('q');
   });
@@ -121,7 +120,13 @@ describe('videoLayers', () => {
     const width = 640;
     const height = 480;
     vi.spyOn(track, 'getSettings').mockReturnValue({ width, height });
-    const layers = findOptimalVideoLayers(track);
+    const layers = computeVideoLayers(track, {
+      bitrate: 0,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      fps: 30,
+      videoDimension: { width, height },
+    });
     expect(layers.length).toBe(2);
     expect(layers[0].rid).toBe('q');
     expect(layers[1].rid).toBe('h');
@@ -132,7 +137,13 @@ describe('videoLayers', () => {
     const width = 1280;
     const height = 720;
     vi.spyOn(track, 'getSettings').mockReturnValue({ width, height });
-    const layers = findOptimalVideoLayers(track);
+    const layers = computeVideoLayers(track, {
+      bitrate: 0,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      fps: 30,
+      videoDimension: { width, height },
+    });
     expect(layers.length).toBe(3);
     expect(layers[0].rid).toBe('q');
     expect(layers[1].rid).toBe('h');
@@ -145,12 +156,15 @@ describe('videoLayers', () => {
       width: 1280,
       height: 720,
     });
-    const layers = findOptimalVideoLayers(track, undefined, 'vp9', {
-      preferredCodec: 'vp9',
-      scalabilityMode: 'L3T3',
+    const layers = computeVideoLayers(track, {
+      maxTemporalLayers: 3,
+      maxSpatialLayers: 3,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp9' },
+      videoDimension: { width: 1280, height: 720 },
     });
     expect(layers.length).toBe(3);
-    expect(layers[0].scalabilityMode).toBe('L3T3');
+    expect(layers[0].scalabilityMode).toBe('L3T3_KEY');
     expect(layers[0].rid).toBe('q');
     expect(layers[1].rid).toBe('h');
     expect(layers[2].rid).toBe('f');
@@ -163,7 +177,39 @@ describe('videoLayers', () => {
     expect(ridToVideoQuality('')).toBe(VideoQuality.HIGH);
   });
 
-  it('should map OptimalVideoLayer to SVC encodings', () => {
+  it('should map optimal video layers to SFU VideoLayers', () => {
+    const layers: Array<Partial<OptimalVideoLayer>> = [
+      { rid: 'f', width: 1920, height: 1080, maxBitrate: 3000000 },
+      { rid: 'h', width: 960, height: 540, maxBitrate: 750000 },
+      { rid: 'q', width: 480, height: 270, maxBitrate: 187500 },
+    ];
+
+    const videoLayers = toVideoLayers(layers as OptimalVideoLayer[]);
+    expect(videoLayers.length).toBe(3);
+    expect(videoLayers[0]).toEqual({
+      rid: 'f',
+      bitrate: 3000000,
+      fps: 0,
+      quality: VideoQuality.HIGH,
+      videoDimension: { width: 1920, height: 1080 },
+    });
+    expect(videoLayers[1]).toEqual({
+      rid: 'h',
+      bitrate: 750000,
+      fps: 0,
+      quality: VideoQuality.MID,
+      videoDimension: { width: 960, height: 540 },
+    });
+    expect(videoLayers[2]).toEqual({
+      rid: 'q',
+      bitrate: 187500,
+      fps: 0,
+      quality: VideoQuality.LOW_UNSPECIFIED,
+      videoDimension: { width: 480, height: 270 },
+    });
+  });
+
+  it('should map OptimalVideoLayer to SVC encodings (three layers)', () => {
     const layers: Array<Partial<OptimalVideoLayer>> = [
       { rid: 'f', width: 1920, height: 1080, maxBitrate: 3000000 },
       { rid: 'h', width: 960, height: 540, maxBitrate: 750000 },
@@ -180,10 +226,46 @@ describe('videoLayers', () => {
     });
   });
 
+  it('should map OptimalVideoLayer to SVC encodings (two layers)', () => {
+    const layers: Array<Partial<OptimalVideoLayer>> = [
+      { rid: 'h', width: 960, height: 540, maxBitrate: 750000 },
+      { rid: 'q', width: 480, height: 270, maxBitrate: 187500 },
+    ];
+
+    const svcLayers = toSvcEncodings(layers as OptimalVideoLayer[]);
+    expect(svcLayers.length).toBe(1);
+    expect(svcLayers[0]).toEqual({
+      rid: 'q',
+      width: 960,
+      height: 540,
+      maxBitrate: 750000,
+    });
+  });
+
+  it('should map OptimalVideoLayer to SVC encodings (two layers)', () => {
+    const layers: Array<Partial<OptimalVideoLayer>> = [
+      { rid: 'q', width: 480, height: 270, maxBitrate: 187500 },
+    ];
+
+    const svcLayers = toSvcEncodings(layers as OptimalVideoLayer[]);
+    expect(svcLayers.length).toBe(1);
+    expect(svcLayers[0]).toEqual({
+      rid: 'q',
+      width: 480,
+      height: 270,
+      maxBitrate: 187500,
+    });
+  });
+
   describe('getComputedMaxBitrate', () => {
     it('should scale target bitrate down if resolution is smaller than target resolution', () => {
       const targetResolution = { width: 1920, height: 1080, bitrate: 3000000 };
-      const scaledBitrate = getComputedMaxBitrate(targetResolution, 1280, 720);
+      const scaledBitrate = getComputedMaxBitrate(
+        targetResolution,
+        1280,
+        720,
+        3000000,
+      );
       expect(scaledBitrate).toBe(1333333);
     });
 
@@ -193,7 +275,12 @@ describe('videoLayers', () => {
       const targetBitrates = ['f', 'h', 'q'].map((rid) => {
         const width = targetResolution.width / downscaleFactor;
         const height = targetResolution.height / downscaleFactor;
-        const bitrate = getComputedMaxBitrate(targetResolution, width, height);
+        const bitrate = getComputedMaxBitrate(
+          targetResolution,
+          width,
+          height,
+          3000000,
+        );
         downscaleFactor *= 2;
         return {
           rid,
@@ -211,25 +298,45 @@ describe('videoLayers', () => {
 
     it('should not scale target bitrate if resolution is larger than target resolution', () => {
       const targetResolution = { width: 1280, height: 720, bitrate: 1000000 };
-      const scaledBitrate = getComputedMaxBitrate(targetResolution, 2560, 1440);
+      const scaledBitrate = getComputedMaxBitrate(
+        targetResolution,
+        2560,
+        1440,
+        1000000,
+      );
       expect(scaledBitrate).toBe(1000000);
     });
 
     it('should not scale target bitrate if resolution is equal to target resolution', () => {
       const targetResolution = { width: 1280, height: 720, bitrate: 1000000 };
-      const scaledBitrate = getComputedMaxBitrate(targetResolution, 1280, 720);
+      const scaledBitrate = getComputedMaxBitrate(
+        targetResolution,
+        1280,
+        720,
+        1000000,
+      );
       expect(scaledBitrate).toBe(1000000);
     });
 
     it('should handle 0 width and height', () => {
       const targetResolution = { width: 1280, height: 720, bitrate: 1000000 };
-      const scaledBitrate = getComputedMaxBitrate(targetResolution, 0, 0);
+      const scaledBitrate = getComputedMaxBitrate(
+        targetResolution,
+        0,
+        0,
+        1000000,
+      );
       expect(scaledBitrate).toBe(0);
     });
 
     it('should handle 4k target resolution', () => {
       const targetResolution = { width: 3840, height: 2160, bitrate: 15000000 };
-      const scaledBitrate = getComputedMaxBitrate(targetResolution, 1280, 720);
+      const scaledBitrate = getComputedMaxBitrate(
+        targetResolution,
+        1280,
+        720,
+        15000000,
+      );
       expect(scaledBitrate).toBe(1666667);
     });
   });

commit 69eee969ac4d52e3410d8e5e12e012b02a5eb1b7

fix: ensures that maxBitrate is an integer (#1657)
Fixes a regression introduced with #1527. 
We must ensure that the `maxBbitrate` value is an integer as otherwise,
floating point numbers fail to be serialized when `sfu.SetPublisher()`
RPC is invoked, leading to issues where the other participant couldn't
see the affected participant's video.

packages/client/src/rtc/videoLayers.ts
@@ -109,7 +109,8 @@ export const computeVideoLayers = (
       rid,
       width: Math.round(width / downscaleFactor),
       height: Math.round(height / downscaleFactor),
-      maxBitrate: maxBitrate / bitrateFactor || defaultBitratePerRid[rid],
+      maxBitrate:
+        Math.round(maxBitrate / bitrateFactor) || defaultBitratePerRid[rid],
       maxFramerate: fps,
     };
     if (svcCodec) {

packages/client/src/rtc/__tests__/videoLayers.test.ts
@@ -257,6 +257,23 @@ describe('videoLayers', () => {
     });
   });
 
+  it('should use integer for maxBitrate', () => {
+    const track = new MediaStreamTrack();
+    const layers = computeVideoLayers(track, {
+      bitrate: 2999777,
+      // @ts-expect-error - incomplete data
+      codec: { name: 'vp8' },
+      videoDimension: { width: 1920, height: 1080 },
+      fps: 30,
+    });
+    expect(layers).toBeDefined();
+    for (const layer of layers!) {
+      expect(Number.isInteger(layer.width)).toBe(true);
+      expect(Number.isInteger(layer.height)).toBe(true);
+      expect(Number.isInteger(layer.maxBitrate)).toBe(true);
+    }
+  });
+
   describe('getComputedMaxBitrate', () => {
     it('should scale target bitrate down if resolution is smaller than target resolution', () => {
       const targetResolution = { width: 1920, height: 1080, bitrate: 3000000 };