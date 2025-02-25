9b8dd2202038fb35ff6b46815722d31359bc4434
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-02-25 23:36:24 +0100

    roll back to using only react native babel preset

--- packages/react-native-sdk/babel.config.js
+++ packages/react-native-sdk/babel.config.js
@@ -1,15 +1,5 @@
 module.exports = function (api) {
-  const isTest = api.env('test');
-
-  if (isTest) {
-    return {
-      presets: [
-        'module:@react-native/babel-preset',
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-      ],
-    };
-  }
+  api.cache(true);
   return {
     presets: ['module:@react-native/babel-preset'],
   };
