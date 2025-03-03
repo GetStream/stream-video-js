005c51adff3d91c7456ee04a891305181a2c5e0a
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-03-03 21:26:08 +0100

    lint error

--- sample-apps/react-native/dogfood/ios/AppDelegate.swift
+++ sample-apps/react-native/dogfood/ios/AppDelegate.swift
@@ -19,92 +19,92 @@ import RNVoipPushNotification
 
 @main
 class AppDelegate: RCTAppDelegate, UNUserNotificationCenterDelegate, PKPushRegistryDelegate {
-
+  
   override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     return RCTLinkingManager.application(application, open: url, options: options)
   }
-
+  
   // Required for the register event.
   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     RNCPushNotificationIOS.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
   }
-
+  
   // Required for the notification event. You must call the completion handler after handling the remote notification.
   override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
     RNCPushNotificationIOS.didReceiveRemoteNotification(userInfo, fetchCompletionHandler: completionHandler)
   }
-
+  
   // Required for the registrationError event.
   override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
     RNCPushNotificationIOS.didFailToRegisterForRemoteNotificationsWithError(error)
   }
-
+  
   // Required for localNotification event
   func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
     RNCPushNotificationIOS.didReceive(response)
   }
-
+  
   override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
     return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
-
+  
   // --- Handle updated push credentials
   func pushRegistry(
-      _ registry: PKPushRegistry,
-      didUpdate credentials: PKPushCredentials,
-      for type: PKPushType
+    _ registry: PKPushRegistry,
+    didUpdate credentials: PKPushCredentials,
+    for type: PKPushType
   ) {
     RNVoipPushNotificationManager.didUpdate(credentials, forType: type.rawValue)
   }
-
+  
   // --- Handle incoming pushes
   func pushRegistry(
-      _ registry: PKPushRegistry,
-      didReceiveIncomingPushWith payload: PKPushPayload,
-      for type: PKPushType,
-      completion: @escaping () -> Void
+    _ registry: PKPushRegistry,
+    didReceiveIncomingPushWith payload: PKPushPayload,
+    for type: PKPushType,
+    completion: @escaping () -> Void
   ) {
-
+    
     guard let stream = payload.dictionaryPayload["stream"] as? [String: Any],
           let createdCallerName = stream["created_by_display_name"] as? String,
           let cid = stream["call_cid"] as? String else {
       completion() // Ensure completion handler is called even if parsing fails
       return
     }
-
+    
     let uuid = UUID().uuidString
-
+    
     StreamVideoReactNative.registerIncomingCall(cid, uuid: uuid)
-
+    
     // required if you want to call `completion()` on the js side
     RNVoipPushNotificationManager.addCompletionHandler(uuid, completionHandler: completion)
-
+    
     // Process the received push // fire 'notification' event to JS
     RNVoipPushNotificationManager.didReceiveIncomingPush(with: payload, forType: type.rawValue) // type is enum, use rawValue
-
+    
     RNCallKeep.reportNewIncomingCall(uuid,
-                                       handle: createdCallerName,
-                                       handleType: "generic",
-                                       hasVideo: true,
-                                       localizedCallerName: createdCallerName,
-                                       supportsHolding: true,
-                                       supportsDTMF: true,
-                                       supportsGrouping: true,
-                                       supportsUngrouping: true,
-                                       fromPushKit: true,
-                                       payload: stream,
-                                       withCompletionHandler: nil) // Completion handler is already handled above
+                                     handle: createdCallerName,
+                                     handleType: "generic",
+                                     hasVideo: true,
+                                     localizedCallerName: createdCallerName,
+                                     supportsHolding: true,
+                                     supportsDTMF: true,
+                                     supportsGrouping: true,
+                                     supportsUngrouping: true,
+                                     fromPushKit: true,
+                                     payload: stream,
+                                     withCompletionHandler: nil) // Completion handler is already handled above
   }
-
+  
   //Called when a notification is delivered to a foreground app.
   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
     completionHandler([.sound, .alert, .badge]) // Use array literal for options
   }
-
+  
   func provider(_ provider: CXProvider, didActivateAudioSession audioSession: AVAudioSession) {
     RTCAudioSession.sharedInstance().audioSessionDidActivate(AVAudioSession.sharedInstance()) // Use sharedInstance()
   }
-
+  
   func provider(_ provider: CXProvider, didDeactivateAudioSession audioSession: AVAudioSession) {
     RTCAudioSession.sharedInstance().audioSessionDidDeactivate(AVAudioSession.sharedInstance()) // Use sharedInstance()
   }
@@ -117,27 +117,27 @@ class AppDelegate: RCTAppDelegate, UNUserNotificationCenterDelegate, PKPushRegis
       "supportsVideo": true,
       "includesCallsInRecents": false,
     ])
-
+    
     RNVoipPushNotificationManager.voipRegistration()
     
     self.moduleName = "StreamReactNativeVideoSDKSample"
     self.dependencyProvider = RCTAppDependencyProvider()
-
+    
     let center = UNUserNotificationCenter.current()
     center.delegate = self
-
+    
     let options = WebRTCModuleOptions.sharedInstance()
     options.enableMultitaskingCameraAccess = true
     //  uncomment below to see native webrtc logs
     //  options.loggingSeverity = .info // Use enum value directly
-
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
- 
+  
   override func bundleURL() -> URL? {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
