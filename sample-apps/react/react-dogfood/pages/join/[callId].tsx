9b1aec3447dee611c0d900db44add6b6c89e2b8d
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-03-11 13:05:37 +0100

    chore: Upgrade to Next 15.2 (#1717)
    Upgrades our Pronto/Dogfooding app to Next 15.2
    
    ### Implementation notes
    - Upgrade NextJS
    - Upgrade Sentry
    - Upgrade some other accompanied dependencies
    - Remove the now obsolete `?id=` duplication for our native apps on the demo environment

--- sample-apps/react/react-dogfood/pages/join/[callId].tsx
+++ sample-apps/react/react-dogfood/pages/join/[callId].tsx
@@ -22,10 +22,7 @@ import {
 } from '../../lib/getServerSideCredentialsProps';
 import { useGleap } from '../../hooks/useGleap';
 import { useSettings } from '../../context/SettingsContext';
-import {
-  useAppEnvironment,
-  useIsDemoEnvironment,
-} from '../../context/AppEnvironmentContext';
+import { useAppEnvironment } from '../../context/AppEnvironmentContext';
 import { TourProvider } from '../../context/TourContext';
 import appTranslations from '../../translations';
 import { customSentryLogger } from '../../helpers/logger';
@@ -50,24 +47,6 @@ const CallRoom = (props: ServerSideCredentialsProps) => {
 
   // support for connecting to any application using an API key and user token
   const apiKeyOverride = !!router.query['api_key'];
-  const isProntoStaging = useAppEnvironment() === 'pronto-staging';
-  const isDemoEnvironment = useIsDemoEnvironment();
-  useEffect(() => {
-    if (!isDemoEnvironment) return;
-    // For backwards compatibility, we need to append `?id=${callId}` to the URL
-    // if it's not already there.
-    // Otherwise, deep links in the mobile apps won't work.
-    const id = router.query['id'] as string | undefined;
-    if (id !== callId) {
-      router
-        .replace({
-          pathname: router.pathname,
-          query: { ...router.query, id: callId },
-        })
-        .catch((err) => console.error('Failed to replace router', err));
-    }
-  }, [callId, isDemoEnvironment, router]);
-
   const { apiKey, userToken, user, gleapApiKey } = props;
 
   const environment = useAppEnvironment();
@@ -109,7 +88,7 @@ const CallRoom = (props: ServerSideCredentialsProps) => {
       options: {
         baseURL: process.env.NEXT_PUBLIC_STREAM_API_URL,
         logLevel: 'debug',
-        logger: customSentryLogger({ enableVerboseLogging: isProntoStaging }),
+        logger: customSentryLogger(),
         transformRequest: defaultRequestTransformers,
         transformResponse: defaultResponseTransformers,
       },
@@ -127,7 +106,7 @@ const CallRoom = (props: ServerSideCredentialsProps) => {
       // @ts-ignore - for debugging
       window.client = undefined;
     };
-  }, [apiKey, isProntoStaging, tokenProvider, user, userToken]);
+  }, [apiKey, tokenProvider, user, userToken]);
 
   const chatClient = useCreateStreamChatClient({
     apiKey,
