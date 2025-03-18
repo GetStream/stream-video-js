2b7cbf6eeeb07edccbb85adaab62c8c29128bf2e
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-03-18 16:05:35 +0100

    ringing screen implementation

--- sample-apps/react-native/ringing-tutorial/app/(app)/_layout.tsx
+++ sample-apps/react-native/ringing-tutorial/app/(app)/_layout.tsx
@@ -1,6 +1,8 @@
 import { Text } from 'react-native';
-import { Redirect, Stack } from 'expo-router';
+import { Redirect, router, Stack } from 'expo-router';
 import { useAuthentication } from '../../contexts/authentication-provider';
+import { useCalls } from '@stream-io/video-react-native-sdk';
+import { useEffect } from 'react';
 
 export default function AppLayout() {
   const { userWithToken, isLoading } = useAuthentication();
@@ -13,5 +15,18 @@ export default function AppLayout() {
     return <Redirect href="/login" />;
   }
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <RingingWrappedStack />;
 }
+
+const RingingWrappedStack = () => {
+  const calls = useCalls().filter((c) => c.ringing);
+
+  useEffect(() => {
+    // whenever there is a ringing call, redirect to the ringing screen
+    if (calls[0]) {
+      router.replace('/(app)/ringing');
+    }
+  }, [calls]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
