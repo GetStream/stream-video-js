6fcb33f3574adbf8f22cf016625fe53a11ed1169
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-03-21 15:28:01 +0100

    chore: RN Ringing App (#1719)
    ### Overview
    
    ### Implementation notes
    
    ---------
    
    Co-authored-by: Santhosh Vaiyapuri <santhoshvai@gmail.com>

---
+++ sample-apps/react-native/ringing-tutorial/app/login.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { router } from 'expo-router';
+import { Users } from '../constants/Users';
+import { useAuthentication } from '../contexts/authentication-provider';
+import { UserButton } from '../components/user-button';
+import { ActionButton } from '../components/action-button';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function LoginScreen() {
+  const [selectedUser, setSelectedUser] = React.useState<string | null>(null);
+  const { signIn } = useAuthentication();
+
+  const handleLogin = () => {
+    if (selectedUser === null) {
+      return;
+    }
+    signIn(selectedUser);
+    // Navigate to main app
+    router.replace('/');
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Login as:</Text>
+
+      <View style={styles.userButtonsContainer}>
+        {Users.map((user) => (
+          <UserButton
+            key={user.id}
+            userName={user.name}
+            selected={selectedUser === user.id}
+            onPress={() => setSelectedUser(user.id)}
+          />
+        ))}
+      </View>
+
+      <ActionButton
+        disabled={selectedUser === null}
+        onPress={handleLogin}
+        action="Login"
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  userButtonsContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+});
---
+++ sample-apps/react-native/ringing-tutorial/utils/setPushConfig.ts
@@ -0,0 +1,58 @@
+import {
+  StreamVideoClient,
+  StreamVideoRN,
+} from '@stream-io/video-react-native-sdk';
+import { AndroidImportance } from '@notifee/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Users } from '../constants/Users';
+
+const API_KEY = 'par8f5s3gn2j';
+
+export function setPushConfig() {
+  StreamVideoRN.setPushConfig({
+    isExpo: true,
+    ios: {
+      pushProviderName: 'expo-apn-video-ringingtutorial',
+    },
+    android: {
+      pushProviderName: 'expo-fcm-video',
+      callChannel: {
+        id: 'stream_call_notifications',
+        name: 'Call notifications',
+        importance: AndroidImportance.HIGH,
+        sound: 'default',
+      },
+      incomingCallChannel: {
+        id: 'stream_incoming_call',
+        name: 'Incoming call notifications',
+        importance: AndroidImportance.HIGH,
+      },
+      incomingCallNotificationTextGetters: {
+        getTitle: (createdUserName: string) =>
+          `Incoming call from ${createdUserName}`,
+        getBody: (_createdUserName: string) => 'Tap to open the call',
+      },
+    },
+    createStreamVideoClient,
+  });
+}
+
+/**
+ * Create a StreamVideoClient instance with the user details from mmkvStorage.
+ * This is used to create a video client for incoming calls in the background on a push notification.
+ */
+const createStreamVideoClient = async () => {
+  const userId = await AsyncStorage.getItem('@userid-key');
+  const userWithToken = Users.find((user) => user.id === userId);
+  if (!userWithToken) {
+    console.error(
+      'Push - createStreamVideoClient -- userWithToken is undefined',
+    );
+    return;
+  }
+  return StreamVideoClient.getOrCreateInstance({
+    apiKey: API_KEY,
+    tokenProvider: () => Promise.resolve(userWithToken.token),
+    user: { id: userWithToken.id, name: userWithToken.name },
+  });
+};
