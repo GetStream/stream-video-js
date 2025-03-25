1636a0e4061fef3ef38c210f38c82bac986a1b37
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-03-25 22:55:22 +0100

    fix: simplify mediaDevices patches

--- packages/client/src/rtc/stats/media.ts
+++ packages/client/src/rtc/stats/media.ts
@@ -4,17 +4,22 @@ import { dumpStream } from './utils';
 export const traceBuffer = new TraceBuffer();
 const trace = traceBuffer.trace;
 
-if (typeof navigator !== 'undefined') {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    console.log('patching navigator.mediaDevices.getUserMedia');
-    const origGetUserMedia = navigator.mediaDevices.getUserMedia.bind(
-      navigator.mediaDevices,
-    );
-    const gum: MediaDevices['getUserMedia'] = async function (constraints) {
-      const tag = 'navigator.mediaDevices';
+if (
+  typeof navigator !== 'undefined' &&
+  typeof navigator.mediaDevices !== 'undefined'
+) {
+  const tag = 'navigator.mediaDevices';
+  if (navigator.mediaDevices.getUserMedia) {
+    const origGetUserMedia = navigator.mediaDevices.getUserMedia;
+    navigator.mediaDevices.getUserMedia = async function patchedGetUserMedia(
+      constraints,
+    ) {
       trace(`${tag}.getUserMedia`, null, constraints);
       try {
-        const stream = await origGetUserMedia(constraints);
+        const stream = await origGetUserMedia.call(
+          navigator.mediaDevices,
+          constraints,
+        );
         trace(`${tag}.getUserMediaOnSuccess`, null, dumpStream(stream));
         return stream;
       } catch (err) {
@@ -22,25 +27,24 @@ if (typeof navigator !== 'undefined') {
         throw err;
       }
     };
-    navigator.mediaDevices.getUserMedia = gum.bind(navigator.mediaDevices);
   }
 
-  if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
-    const origGetDisplayMedia = navigator.mediaDevices.getDisplayMedia.bind(
-      navigator.mediaDevices,
-    );
-    const gdm: MediaDevices['getDisplayMedia'] = async function (constraints) {
-      const tag = 'navigator.mediaDevices';
-      trace(`${tag}.getDisplayMedia`, null, constraints);
-      try {
-        const stream = await origGetDisplayMedia(constraints);
-        trace(`${tag}.getDisplayMediaOnSuccess`, null, dumpStream(stream));
-        return stream;
-      } catch (err) {
-        trace(`${tag}.getDisplayMediaOnFailure`, null, (err as Error).name);
-        throw err;
-      }
-    };
-    navigator.mediaDevices.getDisplayMedia = gdm.bind(navigator.mediaDevices);
+  if (navigator.mediaDevices.getDisplayMedia) {
+    const origGetDisplayMedia = navigator.mediaDevices.getDisplayMedia;
+    navigator.mediaDevices.getDisplayMedia =
+      async function patchedGetDisplayMedia(constraints) {
+        trace(`${tag}.getDisplayMedia`, null, constraints);
+        try {
+          const stream = await origGetDisplayMedia.call(
+            navigator.mediaDevices,
+            constraints,
+          );
+          trace(`${tag}.getDisplayMediaOnSuccess`, null, dumpStream(stream));
+          return stream;
+        } catch (err) {
+          trace(`${tag}.getDisplayMediaOnFailure`, null, (err as Error).name);
+          throw err;
+        }
+      };
   }
 }
