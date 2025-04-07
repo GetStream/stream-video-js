3034ab0023a62258fc5fa05b7e437f02f01ae96d
Matvei Andrienko <m.y.andrienko@outlook.com>
2025-04-07 14:03:07 +0200

    fix: Document PiP support on Pronto (#1746)
    ### Overview
    
    Implements pop-out call view for Pronto using the Document
    Picture-in-Picture API in supported browsers.
    
    ![Screen Shot 2025-04-03 at 17 53 53
    PM](https://github.com/user-attachments/assets/2c55b679-5c34-46c5-855e-7128ed344229)
    
    ### Implementation notes
    
    The hook `usePipWindow` in react-dogfood app is responsible for managing
    PiP window state. The call is rendered into the window using a
    cross-document React portal. Stylesheets are copied over from the parent
    document.
    
    This hook also sets up the `enterpictureinpicture` action handler with
    the Media Session API. That way PiP activates automatically on tab
    switch. If you find this annoying, revoke "Automatic Picture-in-Picture"
    permission.
    
    The new `PipLayout` call layout is added. It renders as many
    participants as it can fit in single-column layout. The layout has two
    parts:
    
    1. `PipLayout.Pip` should be rendered in the PiP window. It renders
    participant video.
    2. `PipLayout.Host` should be rendered in the parent window. It renders
    participant audio. Participant audio might not play in PiP window
    because it doesn't have user interactions.
    
    ---------
    
    Co-authored-by: Oliver Lazoroski <oliver.lazoroski@gmail.com>

--- packages/react-sdk/src/core/components/CallLayout/index.ts
+++ packages/react-sdk/src/core/components/CallLayout/index.ts
@@ -1,6 +1,7 @@
 export * from './LivestreamLayout';
 export * from './PaginatedGridLayout';
 export * from './SpeakerLayout';
+export * from './PipLayout';
 export type {
   FilterableParticipant,
   ParticipantFilter,
