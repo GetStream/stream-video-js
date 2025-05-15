1a9dac3826f490296c8ba6d268229508799c49b6
Kristian Martinoski <martinoskikristian@gmail.com>
2025-05-15 20:16:07 +0200

    revert change

--- packages/react-sdk/src/components/CallParticipantsList/CallParticipantListHeader.tsx
+++ packages/react-sdk/src/components/CallParticipantsList/CallParticipantListHeader.tsx
@@ -15,7 +15,6 @@ export const CallParticipantListHeader = ({
   const { useParticipants, useAnonymousParticipantCount } = useCallStateHooks();
   const participants = useParticipants();
   const anonymousParticipantCount = useAnonymousParticipantCount();
-  const totalParticipantCount = participants.length + anonymousParticipantCount;
   const { t } = useI18n();
 
   return (
