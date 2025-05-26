df43e4147ff48e2317499328a4c240859cab2c7f
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-05-26 14:48:04 +0200

    wip. show on lock screen

--- packages/react-native-sdk/android/src/main/java/com/streamvideo/reactnative/StreamVideoReactNative.kt
+++ packages/react-native-sdk/android/src/main/java/com/streamvideo/reactnative/StreamVideoReactNative.kt
@@ -1,6 +1,11 @@
 package com.streamvideo.reactnative
 
+import android.app.Activity
+import android.app.KeyguardManager
 import android.content.res.Configuration
+import android.os.Build
+import android.view.WindowManager
+import androidx.core.content.getSystemService
 import java.util.concurrent.CopyOnWriteArrayList // For thread safety
 
 object StreamVideoReactNative {
@@ -35,6 +40,28 @@ object StreamVideoReactNative {
         // Iterate safely over the list
         pipListeners.forEach { listener ->
             listener(isInPictureInPictureMode, newConfig)
+        }
+    }
+
+    /**
+     * Enable the calling activity to be shown in the lockscreen and dismiss the keyguard to enable
+     * users to answer without unblocking.
+     */
+    @JvmStatic
+    fun setupCallActivity(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            activity.setShowWhenLocked(true)
+            activity.setTurnScreenOn(true)
+        } else {
+            activity.window.addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON,
+            )
+        }
+
+        val keyguardManager = activity.getSystemService<KeyguardManager>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && keyguardManager != null) {
+            keyguardManager.requestDismissKeyguard(activity, null)
         }
     }
 }
