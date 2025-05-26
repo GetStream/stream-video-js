249ce38170513e7838703482802b087bfbbac8cb
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-05-26 16:58:24 +0200

    Revert "Merge branch 'web-ring' into android-improvements"
    This reverts commit 71d16c53074880fb8eaade94fa0e8710e9aa8f1e, reversing
    changes made to df43e4147ff48e2317499328a4c240859cab2c7f.

--- sample-apps/react/stream-video-react-tutorial/src/App.tsx
+++ sample-apps/react/stream-video-react-tutorial/src/App.tsx
@@ -1,125 +1,76 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
+  Call,
   CallControls,
+  CallingState,
+  SpeakerLayout,
   StreamCall,
   StreamTheme,
   StreamVideo,
-  SpeakerLayout,
   StreamVideoClient,
-  useCalls,
-  CallingState,
-  useStreamVideoClient,
+  useCallStateHooks,
+  User,
 } from '@stream-io/video-react-sdk';
+
 import '@stream-io/video-react-sdk/dist/css/styles.css';
 import './style.css';
 
-// NOTE: This will generate a new call on every reload
-// Fork this CodeSandbox and set your own CallID if
-// you want to test with multiple users or multiple tabs opened
-const callId = () => 'csb-' + Math.random().toString(16).substring(2);
-const user_id = 'zita';
-const userCaller = { id: user_id };
+// For demo credentials, check out our video calling tutorial:
+// https://getstream.io/video/sdk/react/tutorial/video-calling/
+const apiKey = 'REPLACE_WITH_API_KEY';
+const token = 'REPLACE_WITH_TOKEN';
+const userId = 'REPLACE_WITH_USER_ID';
+const callId = 'REPLACE_WITH_CALL_ID';
 
-const apiKey = 'par8f5s3gn2j';
-const tokenProvider = async () => {
-  const { token } = await fetch(
-    'https://pronto.getstream.io/api/auth/create-token?' +
-      new URLSearchParams({
-        environment: 'pronto',
-        user_id: user_id,
-      }),
-  ).then((res) => res.json());
-  return token as string;
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
 };
 
-export default function CallerApp() {
-  const [client, setClient] = useState<StreamVideoClient>();
+// initialize the StreamVideoClient
+const client = new StreamVideoClient({ apiKey, user, token });
 
+export default function App() {
+  const [call, setCall] = useState<Call>();
   useEffect(() => {
-    const myClient = new StreamVideoClient({
-      apiKey,
-      user: userCaller,
-      tokenProvider,
-      options: { logLevel: 'debug' },
+    const myCall = client.call('default', callId);
+    myCall.join({ create: true }).catch((err) => {
+      console.error(`Failed to join the call`, err);
     });
-    setClient(myClient);
-    return () => {
-      myClient.disconnectUser();
-      setClient(undefined);
-    };
-  }, []);
 
-  if (!client) return null;
-
-  return (
-    <StreamVideo client={client}>
-      <CallPanel />
-    </StreamVideo>
-  );
-}
-
-const CallPanel = () => {
-  const client = useStreamVideoClient();
-  const call = useCalls().filter((c) => c.ringing)[0];
-  const [calleeId, setCalleeId] = useState('santhosh');
-
-  const initiateRingCall = useCallback(() => {
-    if (!client) return;
-    const myCall = client.call('default', callId());
-    myCall
-      .getOrCreate({
-        ring: true,
-        data: {
-          members: [
-            // include self
-            { user_id: user_id },
-            // include the userId of the callee
-            { user_id: calleeId },
-          ],
-        },
-      })
-      .catch((err) => {
-        console.error(`Failed to join the call`, err);
-      });
+    setCall(myCall);
 
     return () => {
-      if (myCall.state.callingState === CallingState.LEFT) return;
+      setCall(undefined);
       myCall.leave().catch((err) => {
         console.error(`Failed to leave the call`, err);
       });
     };
-  }, [client, calleeId]);
+  }, []);
 
-  function handleEnd() {
-    call?.endCall();
+  if (!call) return null;
+
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <UILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const UILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
   }
 
   return (
-    <StreamTheme className="my-theme-overrides">
-      {call && (
-        <StreamCall call={call}>
-          <label>callId: {call.cid}</label>
-          <SpeakerLayout />
-          <CallControls />
-          <button type="button" onClick={handleEnd}>
-            End call for all!
-          </button>
-        </StreamCall>
-      )}
-      {!call && (
-        <div className="ringContainer">
-          <label>My user_id: {user_id}</label>
-          <label>Callee user_id: </label>
-          <input
-            type="text"
-            value={calleeId}
-            onChange={(e) => {
-              setCalleeId(e.target.value);
-            }}
-          />
-          <button onClick={initiateRingCall}>Ring</button>
-        </div>
-      )}
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition="bottom" />
+      <CallControls />
     </StreamTheme>
   );
 };
