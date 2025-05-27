7897f4e1fa2324e9c83a072d9284777dee5f84c0
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-05-27 14:18:40 +0200

    feat: update i18next to its latest version

--- packages/react-bindings/src/i18n/types.ts
+++ packages/react-bindings/src/i18n/types.ts
@@ -1,17 +1,8 @@
-export type TranslationLanguage = 'en' | string;
-
-export type Namespace = string;
+export type TranslationLanguage = 'en' | (string & {});
 
 export type TranslationSheet = Record<string, string>;
 
-export type TranslationsForLanguage = Record<Namespace, TranslationSheet>;
-
 export type TranslationsMap = Record<TranslationLanguage, TranslationSheet>;
-
-export type TranslationsRegistry = Record<
-  TranslationLanguage | string,
-  TranslationsForLanguage
->;
 
 export type TranslatorFunction = (
   key: string,
