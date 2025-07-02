40e098d8a82a5ccf2076da69299bf18877dcbeec
Kristian <martinoskikristian@gmail.com>
2025-07-02 16:56:40 +0200

    fix(react-native): customise android notification buttons (#1842)
    ### 💡 Overview
    
    Adds customisation for the Decline and Accept notifications for Android.
    
    ### 📝 Implementation notes
    
    `setPushConfig` is updated so in `incomingCallNotificationTextGetters`
    new methods are added with which these values can be customised on
    client side.
    ```js
      getAcceptButtonTitle?: () => string;
      getDeclineButtonTitle?: () => string;
    ```
    
    🎫 Ticket: https://linear.app/stream/issue/RN-238
    
    📑 Docs: https://github.com/GetStream/docs-content/pull/421

--- packages/react-native-sdk/src/utils/push/android.ts
+++ packages/react-native-sdk/src/utils/push/android.ts
@@ -279,7 +279,8 @@ export const firebaseDataHandler = async (
       incomingCallChannel.sound = await getAndroidDefaultRingtoneUrl();
     }
     await notifee.createChannel(incomingCallChannel);
-    const { getTitle, getBody } = incomingCallNotificationTextGetters;
+    const { getTitle, getBody, getAcceptButtonTitle, getDeclineButtonTitle } =
+      incomingCallNotificationTextGetters;
     const createdUserName = data.created_by_display_name as string;
 
     const title = getTitle(createdUserName);
@@ -311,13 +312,13 @@ export const firebaseDataHandler = async (
         },
         actions: [
           {
-            title: 'Decline',
+            title: getDeclineButtonTitle?.() ?? 'Decline',
             pressAction: {
               id: DECLINE_CALL_ACTION_ID,
             },
           },
           {
-            title: 'Accept',
+            title: getAcceptButtonTitle?.() ?? 'Accept',
             pressAction: {
               id: ACCEPT_CALL_ACTION_ID,
               launchActivity: 'default', // open the app when the notification is pressed
