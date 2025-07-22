9b4a7ea7525aec934f601cf6d0811e67ca50f2b3
Kristian Martinoski <martinoskikristian@gmail.com>
2025-07-22 15:23:02 +0200

    add call reject with busy tone for react

--- packages/react-sdk/src/components/RingingCall/RingingCallControls.tsx
+++ packages/react-sdk/src/components/RingingCall/RingingCallControls.tsx
@@ -1,13 +1,41 @@
-import { CallingState } from '@stream-io/video-client';
+import { CallingState, getLogger } from '@stream-io/video-client';
 import { AcceptCallButton, CancelCallButton } from '../CallControls';
-import { useCall, useCallStateHooks } from '@stream-io/video-react-bindings';
+import {
+  useCall,
+  useCalls,
+  useCallStateHooks,
+} from '@stream-io/video-react-bindings';
 
 export const RingingCallControls = () => {
   const call = useCall();
+  const calls = useCalls();
   const { useCallCallingState } = useCallStateHooks();
   const callCallingState = useCallCallingState();
 
   if (!call) return null;
+
+  const joinedRingingCalls = calls.filter(
+    (c) => c.ringing && c.state.callingState === CallingState.JOINED,
+  );
+  const alreadyInAnotherRingingCall = joinedRingingCalls.length > 0;
+
+  // TODO: where to add config prop shouldRejectCallWhenBusy?
+  const isCalleeBusy = alreadyInAnotherRingingCall; // && shouldRejectCallWhenBusy;
+  const rejectReason = isCalleeBusy ? 'busy' : 'decline';
+
+  const ringingCallToReject = calls.filter(
+    (c) => c.ringing && c.state.callingState === CallingState.RINGING,
+  );
+
+  if (isCalleeBusy && ringingCallToReject?.length === 1) {
+    try {
+      ringingCallToReject[0]?.leave({ reject: true, reason: rejectReason });
+    } catch (error) {
+      const logger = getLogger(['RingingCallControls']);
+      logger('error', 'Error rejecting Call when busy', error);
+    }
+    return null;
+  }
 
   const buttonsDisabled = callCallingState !== CallingState.RINGING;
   return (
