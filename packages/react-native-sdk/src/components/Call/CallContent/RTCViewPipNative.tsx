35c5582255bda10c05eef8a359edaf5d8a70db47
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-08-05 14:06:03 +0200

    remove the

--- packages/react-native-sdk/src/components/Call/CallContent/RTCViewPipNative.tsx
+++ packages/react-native-sdk/src/components/Call/CallContent/RTCViewPipNative.tsx
@@ -19,12 +19,10 @@ const NativeComponent: HostComponent<RTCViewPipNativeProps> =
 
 export function onNativeCallClosed(reactTag: number) {
   getLogger(['RTCViewPipNative'])('debug', 'onNativeCallClosed');
-  UIManager.dispatchViewManagerCommand(
-    reactTag,
-    // @ts-expect-error - types issue
-    UIManager.getViewManagerConfig(COMPONENT_NAME).Commands.onCallClosed,
-    [],
-  );
+  const commandId =
+    UIManager.getViewManagerConfig(COMPONENT_NAME).Commands.onCallClosed;
+  if (!commandId) return;
+  UIManager.dispatchViewManagerCommand(reactTag, commandId, []);
 }
 
 export function onNativeDimensionsUpdated(
@@ -36,13 +34,11 @@ export function onNativeDimensionsUpdated(
     width,
     height,
   });
-  UIManager.dispatchViewManagerCommand(
-    reactTag,
-    // @ts-expect-error - types issue
+  const commandId =
     UIManager.getViewManagerConfig(COMPONENT_NAME).Commands
-      .setPreferredContentSize,
-    [width, height],
-  );
+      .setPreferredContentSize;
+  if (!commandId) return;
+  UIManager.dispatchViewManagerCommand(reactTag, commandId, [width, height]);
 }
 
 /** Wrapper for the native view
