3affa09b74fc62fb114a172aab94f5e9d9fcf08c
Anton Arnautov <arnautov.anton@gmail.com>
2025-09-18 13:00:42 +0200

    Initial POC

--- sample-apps/react/egress-composite/src/ConfigurationContext.tsx
+++ sample-apps/react/egress-composite/src/ConfigurationContext.tsx
@@ -7,6 +7,7 @@ import {
 } from '@stream-io/video-react-sdk';
 
 import { Layout, ScreenshareLayout } from './components/layouts';
+import { Filter } from '@stream-io/video-react-sdk';
 
 const DEFAULT_USER_ID = 'egress';
 const DEFAULT_CALL_TYPE = 'default';
@@ -35,6 +36,28 @@ export const objectFitMap: Record<ObjectFit, string> = {
   fit: 'contain',
   fill: 'cover',
 };
+
+export type ConditionValues = {
+  participantCount: number;
+  hasPinnedParticipant: boolean;
+};
+
+export type CustomActions = ({
+  condition: Filter<ConditionValues>;
+} & (
+  | {
+      action: 'layout_override';
+      layout: Layout;
+    }
+  | {
+      action: 'adjust_options';
+      options: Partial<ConfigurationValue['options']>;
+    }
+))[];
+
+export type ProcessedCustomActions = Array<
+  CustomActions[number] & { conditionMet: boolean }
+>;
 
 export type ConfigurationValue = {
   base_url?: string;
@@ -121,6 +144,8 @@ export type ConfigurationValue = {
       'center'
     >; // ✅
     'layout.spotlight.participants_bar_limit'?: 'dynamic' | number; // ✅
+
+    custom_actions?: CustomActions;
   };
 };
 
--- sample-apps/react/egress-composite/src/components/UIDispatcher.tsx
+++ sample-apps/react/egress-composite/src/components/UIDispatcher.tsx
@@ -1,23 +1,85 @@
-import { useCallStateHooks } from '@stream-io/video-react-sdk';
+import { isPinned, useCallStateHooks } from '@stream-io/video-react-sdk';
 
-import { useConfigurationContext } from '../ConfigurationContext';
+import {
+  ConditionValues,
+  ProcessedCustomActions,
+  useConfigurationContext,
+} from '../ConfigurationContext';
 import {
   DEFAULT_LAYOUT,
   DEFAULT_SCREENSHARE_LAYOUT,
+  Layout,
   layoutMap,
 } from './layouts';
 import { Spotlight } from './layouts/Spotlight';
+import { applyFilter } from '@stream-io/video-react-sdk';
+import { useMemo } from 'react';
+
+const useProcessCustomActions = () => {
+  const { options } = useConfigurationContext();
+  const { useParticipantCount, useParticipants } = useCallStateHooks();
+  const participantCount = useParticipantCount();
+  const participants = useParticipants();
+  const hasPinnedParticipant = participants.some(isPinned);
+
+  return useMemo(() => {
+    if (!options?.custom_actions) {
+      return;
+    }
+
+    const processed: ProcessedCustomActions = options.custom_actions.map(
+      (behavior) => {
+        const conditionMet = applyFilter<ConditionValues>(
+          { participantCount, hasPinnedParticipant },
+          behavior.condition,
+        );
+
+        return {
+          ...behavior,
+          conditionMet,
+        };
+      },
+    );
+
+    return processed;
+  }, [options, participantCount, hasPinnedParticipant]);
+};
 
 export const UIDispatcher = () => {
   const { layout, screenshare_layout } = useConfigurationContext();
   const { useHasOngoingScreenShare } = useCallStateHooks();
   const hasScreenShare = useHasOngoingScreenShare();
 
-  const DefaultView = layoutMap[layout ?? DEFAULT_LAYOUT]?.[0] ?? Spotlight;
+  const processedBehaviors = useProcessCustomActions();
+
+  const layoutOverride = useMemo<(typeof layoutMap)[Layout] | undefined>(() => {
+    if (!processedBehaviors) {
+      return;
+    }
+
+    // pick first matching layout override action
+    const switchLayoutBehavior = processedBehaviors.find(
+      (behavior) =>
+        behavior.action === 'layout_override' && behavior.conditionMet === true,
+    ) as
+      | Extract<ProcessedCustomActions[number], { action: 'layout_override' }>
+      | undefined;
+
+    return switchLayoutBehavior?.layout
+      ? layoutMap[switchLayoutBehavior.layout]
+      : undefined;
+  }, [processedBehaviors]);
+
+  const DefaultView =
+    layoutOverride?.[0] ??
+    layoutMap[layout ?? DEFAULT_LAYOUT]?.[0] ??
+    Spotlight;
   const ScreenShareView =
+    layoutOverride?.[1] ??
     layoutMap[
       screenshare_layout ?? layout ?? DEFAULT_SCREENSHARE_LAYOUT
-    ]?.[1] ?? Spotlight;
+    ]?.[1] ??
+    Spotlight;
 
   return hasScreenShare ? <ScreenShareView /> : <DefaultView />;
 };
