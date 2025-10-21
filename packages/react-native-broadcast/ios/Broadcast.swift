ebe3967346177e1d00e62409e6a5b5cbf61733e5
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-10-21 15:00:43 +0200

    simplify session creation, use reasonable bitrate

--- packages/react-native-broadcast/ios/Broadcast.swift
+++ packages/react-native-broadcast/ios/Broadcast.swift
@@ -11,12 +11,12 @@ public class BroadcastSwift: NSObject {
         let uri = "rtmps://ingress.stream-io-video.com:443/par8f5s3gn2j.default.RdcC9Qr4j7pzr62FZbo8Q/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL2phbmUiLCJ1c2VyX2lkIjoiamFuZSIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiZW52aXJvbm1lbnQiOiJwcm9udG8iLCJpYXQiOjE3NjA5Njc0MzMsImV4cCI6MTc2MTU3MjIzM30.pkBwlOMlo7wUJG4DSG7fk8QAxF912Y5UaErm4H6a59I"
 
         print("[RTMP] Broadcast starting")
-        
+
         Task {
             do {
                 let audioSourceService = AudioSourceService()
                 await audioSourceService.setUp()
-                
+
                 print("[RTMP] Audio source created")
 
                 let mixer = MediaMixer(captureSessionMode: .single)
@@ -25,37 +25,31 @@ public class BroadcastSwift: NSObject {
                     session.sessionPreset = .hd1280x720
                 }
                 await mixer.setMonitoringEnabled(true)
-                
+
                 var videoMixerSettings = await mixer.videoMixerSettings
                 videoMixerSettings.mode = .offscreen
                 await mixer.setVideoMixerSettings(videoMixerSettings)
-                
+
                 print("[RTMP] Mixer created")
 
                 let front = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
                 try? await mixer.attachVideo(front, track: 0) { unit in unit.isVideoMirrored = true }
-                
+
                 let mic = AVCaptureDevice.default(for: .audio)
                 try? await mixer.attachAudio(mic)
 
                 await mixer.startCapturing()
                 await mixer.startRunning()
-                
+
                 print("[RTMP] Mixer running")
-        
-                await SessionBuilderFactory.shared.register(RTMPSessionFactory())
-                let session = try await SessionBuilderFactory.shared.make(URL(string: uri))
-                    .setMode(.publish)
-                    .build()
-                guard let session else {
-                    print("[RTMP] session is null")
-                    return completion(-1, nil)
-                }
-                
+
+                let factory = RTMPSessionFactory.init()
+                let session = factory.make(URL(string: uri)!, mode: .publish, configuration: nil)
+
                 print("[RTMP] Session created")
 
                 await mixer.addOutput(session.stream)
-                
+
                 var audioSettings = await session.stream.audioSettings
                 audioSettings.format = .aac
                 audioSettings.bitRate = 48000
@@ -64,7 +58,7 @@ public class BroadcastSwift: NSObject {
                 var videoSettings = await session.stream.videoSettings
                 videoSettings.isLowLatencyRateControlEnabled = false
                 videoSettings.bitRateMode = .average
-                videoSettings.bitRate = 4200000
+                videoSettings.bitRate = 2500000
                 videoSettings.videoSize = CGSize(width: 720, height: 1280)
                 try await session.stream.setVideoSettings(videoSettings)
 
