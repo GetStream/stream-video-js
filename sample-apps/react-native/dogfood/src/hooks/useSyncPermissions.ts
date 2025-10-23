95445012f0e095fd462caaf855aa48f08b71797f
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2025-10-23 09:47:51 +0200

    chore: add cameraroll

--- sample-apps/react-native/dogfood/src/hooks/useSyncPermissions.ts
+++ sample-apps/react-native/dogfood/src/hooks/useSyncPermissions.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 import {
   PERMISSIONS,
   requestMultiple,
@@ -14,9 +14,58 @@ import {
  */
 export const useSyncPermissions = () => {
   useEffect(() => {
-    requestAndUpdatePermissions();
+    requestAndUpdatePermissions().then(() => {
+      hasCameraRollAndroidPermission();
+    });
   }, []);
 };
+
+async function hasCameraRollAndroidPermission() {
+  const getCheckPermissionPromise = () => {
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      return Promise.all([
+        PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+        ),
+        PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ),
+      ]).then(
+        ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+          hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+      );
+    } else {
+      return PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      );
+    }
+  };
+
+  const hasPermission = await getCheckPermissionPromise();
+  if (hasPermission) {
+    return true;
+  }
+  const getRequestPermissionPromise = () => {
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      return PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+      ]).then(
+        (statuses) =>
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+            PermissionsAndroid.RESULTS.GRANTED,
+      );
+    } else {
+      return PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      ).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+    }
+  };
+
+  return await getRequestPermissionPromise();
+}
 
 const requestAndUpdatePermissions = async () => {
   if (Platform.OS === 'ios') {
