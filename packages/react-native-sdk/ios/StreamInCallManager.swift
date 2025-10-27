f088449b1d4d8f5ba1b818e2c9dbbe5c664e3067
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-10-27 12:17:35 +0100

    trim empty spaces

--- packages/react-native-sdk/ios/StreamInCallManager.swift
+++ packages/react-native-sdk/ios/StreamInCallManager.swift
@@ -18,32 +18,31 @@ enum DefaultAudioDevice {
 
 @objc(StreamInCallManager)
 class StreamInCallManager: RCTEventEmitter {
-    
+
     private let audioSessionQueue = DispatchQueue(label: "io.getstream.rn.audioSessionQueue")
-    
+
     private var audioManagerActivated = false
     private var callAudioRole: CallAudioRole = .communicator
     private var defaultAudioDevice: DefaultAudioDevice = .speaker
     private var previousVolume: Float = 0.75
-    
+
     private struct AudioSessionState {
         let category: AVAudioSession.Category
         let mode: AVAudioSession.Mode
         let options: AVAudioSession.CategoryOptions
     }
-    
+
     private var previousAudioSessionState: AudioSessionState?
     
     override func invalidate() {
         stop()
         super.invalidate()
     }
-    
+
     override static func requiresMainQueueSetup() -> Bool {
         return false
     }
-    
-    
+
     @objc(setAudioRole:)
     func setAudioRole(audioRole: String) {
         audioSessionQueue.async { [self] in
@@ -54,7 +53,7 @@ class StreamInCallManager: RCTEventEmitter {
             self.callAudioRole = audioRole.lowercased() == "listener" ? .listener : .communicator
         }
     }
-    
+
     @objc(setDefaultAudioDeviceEndpointType:)
     func setDefaultAudioDeviceEndpointType(endpointType: String) {
         audioSessionQueue.async { [self] in
@@ -65,7 +64,7 @@ class StreamInCallManager: RCTEventEmitter {
             self.defaultAudioDevice = endpointType.lowercased() == "earpiece" ? .earpiece : .speaker
         }
     }
-    
+
     @objc
     func start() {
         audioSessionQueue.async { [self] in
@@ -82,7 +81,7 @@ class StreamInCallManager: RCTEventEmitter {
             audioManagerActivated = true
         }
     }
-    
+
     @objc
     func stop() {
         audioSessionQueue.async { [self] in
@@ -101,12 +100,12 @@ class StreamInCallManager: RCTEventEmitter {
             audioManagerActivated = false
         }
     }
-    
+
     private func configureAudioSession() {
         let intendedCategory: AVAudioSession.Category!
         let intendedMode: AVAudioSession.Mode!
         let intendedOptions: AVAudioSession.CategoryOptions!
-        
+
         if (callAudioRole == .listener) {
             // enables high quality audio playback but disables microphone
             intendedCategory = .playback
@@ -115,7 +114,7 @@ class StreamInCallManager: RCTEventEmitter {
         } else {
             intendedCategory = .playAndRecord
             intendedMode = .voiceChat
-            
+
             if (defaultAudioDevice == .speaker) {
                 // defaultToSpeaker will route to speaker if nothing else is connected
                 intendedOptions = [.allowBluetooth, .defaultToSpeaker]
@@ -124,7 +123,7 @@ class StreamInCallManager: RCTEventEmitter {
                 intendedOptions = [.allowBluetooth]
             }
         }
-        
+
         // START: set the config that webrtc must use when it takes control
         let rtcConfig = RTCAudioSessionConfiguration.webRTC()
         rtcConfig.category = intendedCategory.rawValue
@@ -132,14 +131,14 @@ class StreamInCallManager: RCTEventEmitter {
         rtcConfig.categoryOptions = intendedOptions
         RTCAudioSessionConfiguration.setWebRTC(rtcConfig)
         // END
-        
+
         // START: compare current audio session with intended, and update if different
         let session = RTCAudioSession.sharedInstance()
         let currentCategory = session.category
         let currentMode = session.mode
         let currentOptions = session.categoryOptions
         let currentIsActive = session.isActive
-        
+
         if currentCategory != intendedCategory.rawValue || currentMode != intendedMode.rawValue || currentOptions != intendedOptions || !currentIsActive {
             session.lockForConfiguration()
             do {
@@ -162,7 +161,7 @@ class StreamInCallManager: RCTEventEmitter {
         }
         // END
     }
-    
+
     @objc(showAudioRoutePicker)
     public func showAudioRoutePicker() {
         guard #available(iOS 11.0, tvOS 11.0, macOS 10.15, *) else {
@@ -177,7 +176,7 @@ class StreamInCallManager: RCTEventEmitter {
                 .sendActions(for: .touchUpInside)
         }
     }
-    
+
     @objc(setForceSpeakerphoneOn:)
     func setForceSpeakerphoneOn(enable: Bool) {
         let session = AVAudioSession.sharedInstance()
@@ -188,12 +187,12 @@ class StreamInCallManager: RCTEventEmitter {
             log("Error setting speakerphone: \(error)")
         }
     }
-    
+
     @objc(setMicrophoneMute:)
     func setMicrophoneMute(enable: Bool) {
         log("iOS does not support setMicrophoneMute()")
     }
-    
+
     @objc
     func logAudioState() {
         let session = AVAudioSession.sharedInstance()
@@ -209,17 +208,17 @@ class StreamInCallManager: RCTEventEmitter {
         """
         log(logString)
     }
-    
+
     @objc(muteAudioOutput)
     func muteAudioOutput() {
         DispatchQueue.main.async { [self] in
             let volumeView = MPVolumeView()
-            
+
             // Add to a temporary view hierarchy to make it functional
             if let window = getCurrentWindow() {
                 volumeView.frame = CGRect(x: -1000, y: -1000, width: 1, height: 1)
                 window.addSubview(volumeView)
-                
+
                 // Give it a moment to initialize
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                     if let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider {
@@ -230,24 +229,24 @@ class StreamInCallManager: RCTEventEmitter {
                     } else {
                         self.log("Could not find volume slider")
                     }
-                    
+
                     // Remove from view hierarchy after use
                     volumeView.removeFromSuperview()
                 }
             }
         }
     }
-    
+
     @objc(unmuteAudioOutput)
     func unmuteAudioOutput() {
         DispatchQueue.main.async { [self] in
             let volumeView = MPVolumeView()
-            
+
             // Add to a temporary view hierarchy to make it functional
             if let window = getCurrentWindow() {
                 volumeView.frame = CGRect(x: -1000, y: -1000, width: 1, height: 1)
                 window.addSubview(volumeView)
-                
+
                 // Give it a moment to initialize
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                     if let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider {
@@ -258,7 +257,7 @@ class StreamInCallManager: RCTEventEmitter {
                     } else {
                         self.log("Could not find volume slider")
                     }
-                    
+
                     // Remove from view hierarchy after use
                     volumeView.removeFromSuperview()
                 }
@@ -267,22 +266,22 @@ class StreamInCallManager: RCTEventEmitter {
     }
     
     // MARK: - RCTEventEmitter
-    
+
     override func supportedEvents() -> [String]! {
         // TODO: list events that can be sent to JS
         return []
     }
-    
+
     @objc
     override func addListener(_ eventName: String!) {
         super.addListener(eventName)
     }
-    
+
     @objc
     override func removeListeners(_ count: Double) {
         super.removeListeners(count)
     }
-    
+
     // MARK: - Helper Methods
     private func getCurrentWindow() -> UIWindow? {
         if #available(iOS 13.0, *) {
@@ -294,10 +293,10 @@ class StreamInCallManager: RCTEventEmitter {
             return UIApplication.shared.keyWindow
         }
     }
-    
+
     // MARK: - Logging Helper
     private func log(_ message: String) {
         NSLog("InCallManager: %@", message)
     }
-    
+
 }
