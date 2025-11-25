dfe2211a65becdd8627c08a5f7c10540737083a1
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2025-11-25 15:37:41 +0100

    chore(pronto): participant count indicator

--- sample-apps/react/react-dogfood/components/ActiveCallHeader.tsx
+++ sample-apps/react/react-dogfood/components/ActiveCallHeader.tsx
@@ -13,9 +13,7 @@ import clsx from 'clsx';
 import { CallHeaderTitle } from './CallHeaderTitle';
 import { ToggleSettingsTabModal } from './Settings/SettingsTabModal';
 import { ToggleDocumentationButton } from './ToggleDocumentationButton';
-
 import { LayoutSelectorProps } from './LayoutSelector';
-
 import { useIsDemoEnvironment } from '../context/AppEnvironmentContext';
 
 const LatencyIndicator = () => {
@@ -71,6 +69,19 @@ const RecordingIndicator = () => {
   return <div className="rd__header__recording-indicator">Recording...</div>;
 };
 
+const ParticipantCountIndicator = () => {
+  const { useParticipants, useParticipantCount } = useCallStateHooks();
+  const participants = useParticipants();
+  const participantCount = useParticipantCount();
+  const count = Math.max(participantCount, participants.length);
+  return (
+    <div className="rd__header__participant-count" title="Total participants">
+      <Icon icon="participants" />
+      {count}
+    </div>
+  );
+};
+
 export const ActiveCallHeader = ({
   onLeave,
   selectedLayout,
@@ -108,14 +119,13 @@ export const ActiveCallHeader = ({
               selectedLayout: selectedLayout,
               onMenuItemClick: onMenuItemClick,
             }}
-            tabModalProps={{
-              inMeeting: true,
-            }}
+            tabModalProps={{ inMeeting: true }}
           />
         </div>
 
         <div className="rd__call-header__controls-group">
           {isRecordingInProgress && <RecordingIndicator />}
+          <ParticipantCountIndicator />
           <Elapsed startedAt={session?.started_at} />
           <LatencyIndicator />
         </div>
