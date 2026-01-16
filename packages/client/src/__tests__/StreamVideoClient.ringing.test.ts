9c477b3adab23f1c766bccb87af8f983de8f6e78
jonadimovska <jona.dimovska@hotmail.com>
2026-01-16 14:14:51 +0100

    fix(react): resolve call state race condition when using join with ring - make test more deterministic

--- packages/client/src/__tests__/StreamVideoClient.ringing.test.ts
+++ packages/client/src/__tests__/StreamVideoClient.ringing.test.ts
@@ -225,23 +225,20 @@ describe('StreamVideoClient Ringing', () => {
     it('new call instance with doJoinRequest({ ring: true }) should replace old instance in store', async () => {
       const callId = crypto.randomUUID();
 
+      const call = oliverClient.call('default', callId, {
+        reuseInstance: true,
+      });
+      const callCreatedEvent = expectEvent(oliverClient, 'call.created');
       const serverCall = serverClient.video.call('default', callId);
-      const createPromise = serverCall.create({
+      await serverCall.create({
         data: {
           created_by_id: 'oliver',
           members: [{ user_id: 'oliver' }, { user_id: 'sacha' }],
         },
       });
 
-      // call.created event still hasn't arrived and the store is empty
-      expect(oliverClient.state.calls.length).toBe(0);
-      // .call creates the call as the store is empty but does not register the call in the store
-      const call = oliverClient.call('default', callId, {
-        reuseInstance: true,
-      });
-
       // the event arrives and since the store is empty registers a new IDLE call
-      await createPromise;
+      await callCreatedEvent;
 
       // join registers or updates the call
       await call.doJoinRequest({ ring: true });
