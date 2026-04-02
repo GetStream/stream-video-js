6eab56abd1ce4589fd2b3e775bba23fbe8a44743
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2026-04-02 14:58:38 +0200

    fix using index.js

--- sample-apps/react-native/ringing-tutorial/app/_layout.tsx
+++ sample-apps/react-native/ringing-tutorial/app/_layout.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Slot } from 'expo-router';
 import { AuthenticationProvider } from '../contexts/authentication-provider';
-import { setPushConfig } from '../utils/setPushConfig';
-import { setFirebaseListeners } from '../utils/setFirebaseListeners';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-// setPushConfig();
-// setFirebaseListeners();
 
 export default function Root() {
   return (
