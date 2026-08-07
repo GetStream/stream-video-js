c740cd9d7c96714fce983e6e4cd7864752f2d87e
Artem Grintsevich <greenfrvr@gmail.com>
2026-08-07 15:44:53 +0200

    chore: adjusted sdp munging for enabling stereo

--- packages/client/src/rtc/helpers/sdp.ts
+++ packages/client/src/rtc/helpers/sdp.ts
@@ -103,7 +103,7 @@ export const enableStereo = (offerSdp: string, answerSdp: string): string => {
     if (!opus) continue;
 
     for (const fmtp of media.fmtp) {
-      if (fmtp.payload === opus.payload && fmtp.config.includes('stereo=1')) {
+      if (fmtp.payload === opus.payload) {
         offeredStereoMids.add(media.mid!);
       }
     }
@@ -120,7 +120,7 @@ export const enableStereo = (offerSdp: string, answerSdp: string): string => {
     if (!opus) continue;
 
     for (const fmtp of media.fmtp) {
-      if (fmtp.payload === opus.payload && !fmtp.config.includes('stereo=1')) {
+      if (fmtp.payload === opus.payload) {
         fmtp.config += ';stereo=1';
       }
     }
