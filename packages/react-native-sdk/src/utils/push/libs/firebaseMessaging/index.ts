256632773d3af16a650fe4a86dd014343c6977d5
Artem Grintsevich <greenfrvr@gmail.com>
2026-08-24 13:05:54 +0200

    chore: adjusted firebase messaging imports

--- packages/react-native-sdk/src/utils/push/libs/firebaseMessaging/index.ts
+++ packages/react-native-sdk/src/utils/push/libs/firebaseMessaging/index.ts
@@ -1,12 +1,18 @@
-import { lib, type Type } from './lib';
-
-export type { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
-export type FirebaseMessagingType = Type;
+import { lib, type MessagingModule } from './lib';
 
 const INSTALLATION_INSTRUCTION =
   'Please see https://rnfirebase.io/messaging/usage#installation for installation instructions';
 
-export function getFirebaseMessagingLib(): FirebaseMessagingType {
+export type FirebaseRemoteMessage = NonNullable<
+  Awaited<ReturnType<MessagingModule['getInitialNotification']>>
+>;
+
+export type FirebaseMessagingCompat = {
+  getToken: () => Promise<string>;
+  onTokenRefresh: (listener: (token: string) => void) => () => void;
+};
+
+function getFirebaseMessagingModule() {
   if (!lib) {
     throw Error(
       '@react-native-firebase/messaging is not installed. ' +
@@ -14,4 +20,16 @@ export function getFirebaseMessagingLib(): FirebaseMessagingType {
     );
   }
   return lib;
+}
+
+export function getFirebaseMessagingLib(): () => FirebaseMessagingCompat {
+  const messagingModule = getFirebaseMessagingModule();
+  return () => {
+    const messaging = messagingModule.getMessaging();
+    return {
+      getToken: () => messagingModule.getToken(messaging),
+      onTokenRefresh: (listener) =>
+        messagingModule.onTokenRefresh(messaging, listener),
+    };
+  };
 }
