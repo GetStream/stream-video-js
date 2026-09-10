68f61736906189d9d51bdbd93af17f62c4159685
jonadimovska <jona.dimovska@hotmail.com>
2026-09-10 13:15:09 +0200

    feat(react-sdk): migrate participant tile to new token design system

--- packages/react-sdk/src/core/components/Video/BaseVideoPlaceholder.tsx
+++ packages/react-sdk/src/core/components/Video/BaseVideoPlaceholder.tsx
@@ -1,5 +1,6 @@
-import { ComponentProps, RefAttributes, forwardRef, useState } from 'react';
+import { ComponentProps, RefAttributes, forwardRef } from 'react';
 import type { StreamVideoParticipant } from '@stream-io/video-client';
+import { Avatar } from '../../../components';
 
 export type BaseVideoPlaceholderProps = {
   participant: StreamVideoParticipant;
@@ -10,40 +11,22 @@ export const BaseVideoPlaceholder = forwardRef<
   HTMLDivElement,
   BaseVideoPlaceholderProps
 >(function DefaultVideoPlaceholder({ participant, style, children }, ref) {
-  const [error, setError] = useState(false);
   const name = participant.name || participant.userId;
   return (
     <div className="str-video__video-placeholder" style={style} ref={ref}>
-      {(!participant.image || error) &&
-        (name ? (
-          <InitialsFallback name={name} />
-        ) : (
-          <div className="str-video__video-placeholder__no-video-label">
-            {children}
-          </div>
-        ))}
-      {participant.image && !error && (
-        <img
-          onError={() => setError(true)}
-          alt={name}
-          className="str-video__video-placeholder__avatar"
-          src={participant.image}
+      {(participant.image || name) && (
+        <Avatar
+          size="2xl"
+          showOutline
+          imageSrc={participant.image}
+          name={name}
         />
+      )}
+      {!participant.image && !name && (
+        <div className="str-video__video-placeholder__no-video-label">
+          {children}
+        </div>
       )}
     </div>
   );
 });
-
-const InitialsFallback = (props: { name: string }) => {
-  const { name } = props;
-  const initials = name
-    .split(' ')
-    .slice(0, 2)
-    .map((n) => n[0])
-    .join('');
-  return (
-    <div className="str-video__video-placeholder__initials-fallback">
-      {initials}
-    </div>
-  );
-};
