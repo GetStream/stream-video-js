2347ba185216af01cb99365afdec1f1925f35dce
jonadimovska <jona.dimovska@hotmail.com>
2026-09-25 14:36:01 +0200

    feat(client): integrate OpenAPI generated client - update flag name

--- packages/client/generate-openapi.sh
+++ packages/client/generate-openapi.sh
@@ -34,8 +34,7 @@ rm -rf "$OUTPUT_DIR"
     --language ts --spec "$SPEC_DIR/video-clientside-api.yaml" --output "$OUTPUT_DIR" \
     --opt response_dates_as_number=true \
     --opt typed_filters=true \
-    --opt separate_params=true \
-    --opt passthrough_response=true
+    --opt separate_path_params=true
 )
 
 echo "export * from './models';" >"$OUTPUT_DIR/index.ts"
